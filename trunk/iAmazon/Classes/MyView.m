1c9331e076b24f42b4bd1482d915fa60e896aec0
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-03-07 06:03:33 +0000

    Testing scm

--- trunk/iAmazon/Classes/MyView.m
+++ trunk/iAmazon/Classes/MyView.m
@@ -3,7 +3,7 @@
 //  iAmazon
 //
 //  Created by Cyrus Najmabadi on 3/6/08.
-//  Copyright __MyCompanyName__ 2008. All rights reserved.
+//  Copyright Metasyntactic.org 2008. All rights reserved.
 //
 
 #import "MyView.h"
