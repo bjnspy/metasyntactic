2bb0a6286bd04c4e911f2edf8b398d6c3cddfda1
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-03-07 05:58:10 +0000

    Adding iAmazon to source control

---
+++ trunk/iAmazon/Classes/MyView.m
@@ -0,0 +1,13 @@
+//
+//  MyView.m
+//  iAmazon
+//
+//  Created by Cyrus Najmabadi on 3/6/08.
+//  Copyright __MyCompanyName__ 2008. All rights reserved.
+//
+
+#import "MyView.h"
+
+@implementation MyView
+
+@end
