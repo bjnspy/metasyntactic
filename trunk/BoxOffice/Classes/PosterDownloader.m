33db1e540bd390c2ebf60c9fdebcff93cc306a57
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-05-04 00:17:46 +0000

    More work on poster downloading

--- trunk/BoxOffice/Classes/PosterDownloader.m
+++ trunk/BoxOffice/Classes/PosterDownloader.m
@@ -9,16 +9,21 @@
 #import "PosterDownloader.h"
 #import "XmlParser.h"
 #import "XmlElement.h"
+#import "DifferenceEngine.h"
 
 @implementation PosterDownloader
 
 @synthesize movie;
+@synthesize identifiers;
+@synthesize titleToPosterUrlMap;
 
 - (id) initWithMovie:(Movie*) movie_
 {
     if (self = [super init])
     {
         self.movie = movie_;
+        self.identifiers = [NSMutableArray array];
+        self.titleToPosterUrlMap = [NSMutableDictionary dictionary];
     }
     
     return self;
@@ -27,6 +32,8 @@
 - (void) dealloc
 {
     self.movie = nil;
+    self.identifiers = nil;
+    self.titleToPosterUrlMap = nil;
     [super dealloc];
 }
 
@@ -36,42 +43,39 @@
     return [downloader go];
 }
 
-- (NSData*) go
+- (void) sleep
 {
-    [self searchForItem];
-    return nil;
+    [NSThread sleepForTimeInterval:0.25];
 }
 
-- (void) searchForItem
+- (NSData*) go
 {
-    /*
-     POST /onca/soap?Service=AWSECommerceService HTTP/1.1
-     Content-Type: text/xml; charset=utf-8
-     SOAPAction: "http://soap.amazon.com"
-     Host: soap.amazon.com
-     Content-Length: 604
-     Expect: 100-continue
-     
-     <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"><s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><ItemSearch xmlns="http://webservices.amazon.com/AWSECommerceService/2008-04-07"><AWSAccessKeyId>1RRVC0BHDPKTXB98FCR2</AWSAccessKeyId><AssociateTag>cyrusnajma-20</AssociateTag><Request><SearchIndex>DVD</SearchIndex><Title>Dare Not Walk Alone: A War of Responsibility</Title></Request><Request><Keywords>Dare Not Walk Alone: A War of Responsibility</Keywords><SearchIndex>All</SearchIndex></Request></ItemSearch></s:Body></s:Envelope>
-     */
-    
+    [self searchForItems];
+    [self sleep];
+    [self lookupItems];
+    [self sleep];
+    return [self downloadBestPoster:[self findBestTitle]];
+}
+
+- (void) searchForItems
+{
     NSString* post =
     @"<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\">"
-        "<s:Body xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" "
-                "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">"
-            "<ItemSearch xmlns=\"http://webservices.amazon.com/AWSECommerceService/2008-04-07\">"
-                "<AWSAccessKeyId>1RRVC0BHDPKTXB98FCR2</AWSAccessKeyId>"
-                "<AssociateTag>cyrusnajma-20</AssociateTag>"
-                "<Request>"
-                    "<SearchIndex>DVD</SearchIndex>"
-                    "<Title>Juno</Title>"
-                "</Request>"
-                "<Request>"
-                    "<Keywords>Juno</Keywords>"
-                    "<SearchIndex>All</SearchIndex>"
-                "</Request>"
-            "</ItemSearch>"
-        "</s:Body>"
+    "<s:Body xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" "
+    "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">"
+    "<ItemSearch xmlns=\"http://webservices.amazon.com/AWSECommerceService/2008-04-07\">"
+    "<AWSAccessKeyId>1RRVC0BHDPKTXB98FCR2</AWSAccessKeyId>"
+    "<AssociateTag>cyrusnajma-20</AssociateTag>"
+    "<Request>"
+    "<SearchIndex>DVD</SearchIndex>"
+    "<Title>Juno</Title>"
+    "</Request>"
+    "<Request>"
+    "<Keywords>Juno</Keywords>"
+    "<SearchIndex>All</SearchIndex>"
+    "</Request>"
+    "</ItemSearch>"
+    "</s:Body>"
     "</s:Envelope>";
     NSData* postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
     
@@ -97,17 +101,141 @@
     }
     
     XmlElement* envelopeElement = [XmlParser parse:result];
-    XmlElement* bodyElement = [envelopeElement element:@"SOAP-ENV:body"];
+    XmlElement* bodyElement = [envelopeElement element:@"SOAP-ENV:Body"];
     XmlElement* responseElement = [bodyElement element:@"ItemSearchResponse"];
     XmlElement* itemsElement = [responseElement element:@"Items"];
     
-    NSMutableArray* identifiers = [NSMutableArray array];
-    
-    
-    NSString* desc = [envelopeElement description];
-    NSLog(@"%@", desc);
-    
-    [NSThread sleepForTimeInterval:0.25];
+    for (XmlElement* child in [itemsElement elements:@"Item"])
+    {
+        [identifiers addObject:[[child element:@"ASIN"] text]];
+    }
 }
+
+- (void) lookupItems
+{
+    if ([identifiers count] % 2 == 1)
+    {
+        [identifiers addObject:@""];
+    }
+    
+    for (NSInteger i = 0; i < [identifiers count]; i += 2)
+    {
+        [self lookupItem1:[identifiers objectAtIndex:i] item2:[identifiers objectAtIndex:(i + 1)]];
+        [self sleep];
+    }
+}
+
+- (void) lookupItem1:(NSString*) item1
+               item2:(NSString*) item2
+{
+    /*
+     POST /onca/soap?Service=AWSECommerceService HTTP/1.1
+     Content-Type: text/xml; charset=utf-8
+     SOAPAction: "http://soap.amazon.com"
+     Host: soap.amazon.com
+     Content-Length: 638
+     Expect: 100-continue
+     */
+    NSMutableString* post = 
+    @"<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\">"
+    "<s:Body xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\""
+    "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">"
+    "<ItemLookup xmlns=\"http://webservices.amazon.com/AWSECommerceService/2008-04-07\">"
+    "<AWSAccessKeyId>1RRVC0BHDPKTXB98FCR2</AWSAccessKeyId>"
+    "<AssociateTag>cyrusnajma-20</AssociateTag>"
+    "<Request>"
+    "<ItemId>B000YABYLA</ItemId>"
+    "<ResponseGroup>Images</ResponseGroup>"
+    "<ResponseGroup>ItemAttributes</ResponseGroup>"
+    "</Request>"
+    "<Request>"
+    "<ItemId>B0014CQNTK</ItemId>"
+    "<ResponseGroup>Images</ResponseGroup>"
+    "<ResponseGroup>ItemAttributes</ResponseGroup>"
+    "</Request>"
+    "</ItemLookup>"
+    "</s:Body>"
+    "</s:Envelope>";
+    
+    NSData* postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+    
+    NSMutableURLRequest* request = [[[NSMutableURLRequest alloc] init] autorelease];
+    [request setURL:[NSURL URLWithString:@"http://soap.amazon.com/onca/soap?Service=AWSECommerceService"]];
+    [request setHTTPMethod:@"POST"];
+    
+    [request setValue:@"text/xml" forHTTPHeaderField:@"Content-Type"];
+    [request setValue:@"http://soap.amazon.com" forHTTPHeaderField:@"Soapaction"];
+    [request setValue:@"soap.amazon.com" forHTTPHeaderField:@"Host"];
+    
+    [request setHTTPBody:postData];    
+    
+    NSURLResponse* response = nil;
+    NSError* error = nil;
+    NSData* result =
+    [NSURLConnection sendSynchronousRequest:request
+                          returningResponse:&response
+                                      error:&error];
+    if (error != nil || result == nil)
+    {
+        return;
+    }
+    
+    XmlElement* envelopeElement = [XmlParser parse:result];
+    XmlElement* bodyElement = [envelopeElement element:@"SOAP-ENV:Body"];
+    XmlElement* responseElement = [bodyElement element:@"ItemLookupResponse"];
+    XmlElement* itemsElement = [responseElement element:@"Items"];
+    
+    for (XmlElement* child in [itemsElement elements:@"Item"])
+    {
+        XmlElement* mediumImageElement = [child element:@"MediumImage"];
+        XmlElement* urlElement = [mediumImageElement element:@"URL"];
+        XmlElement* attributesElement = [child element:@"ItemAttributes"];
+        XmlElement* titleElement = [attributesElement element:@"Title"];
+        
+        NSString* posterUrl = [urlElement text];
+        NSString* title = [titleElement text];
+        
+        if (posterUrl != nil && title != nil)
+        {
+            [self.titleToPosterUrlMap setValue:posterUrl forKey:title];
+        }
+    }    
+}
+
+NSInteger titleSort(id t1, id t2, void *context)
+{
+    NSString* title1 = t1;
+    NSString* title2 = t2;
+    NSString* movieTitle = context;
+    
+    NSCharacterSet* nonAlphaNumerics = [[NSCharacterSet alphanumericCharacterSet] invertedSet];
+    NSString* trimmed1 = [title1 stringByTrimmingCharactersInSet:nonAlphaNumerics];
+    NSString* trimmed2 = [title2 stringByTrimmingCharactersInSet:nonAlphaNumerics];
+    
+    NSInteger editDistance1 = [[[[DifferenceEngine alloc] init] autorelease] editDistanceFrom:trimmed1 to:movieTitle];
+    NSInteger editDistance2 = [[[[DifferenceEngine alloc] init] autorelease] editDistanceFrom:trimmed2 to:movieTitle];
+    
+    return editDistance1 - editDistance2;
+}
+
+- (NSString*) findBestTitle
+{
+    NSArray* keys = [self.titleToPosterUrlMap allKeys];
+    NSArray* sortedKeys = [keys sortedArrayUsingFunction:titleSort context:self.movie.title];
+    
+    if ([sortedKeys count] == 0)
+    {
+        return nil;
+    }
+    
+    return [sortedKeys objectAtIndex:0];
+}
+
+- (NSData*) downloadBestPoster:(NSString*) title
+{
+    NSString* url = [self.titleToPosterUrlMap objectForKey:title];
+    return nil;
+}
+
 
 @end
