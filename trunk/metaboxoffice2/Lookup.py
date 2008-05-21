e3ed88ed6f1d6fbd2cecfd553e6376f1000b0c11
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-05-21 04:00:57 +0000

    adding search/lookup capbilities

---
+++ trunk/metaboxoffice2/Lookup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import wsgiref.handlers
+from xml.dom.minidom import getDOMImplementation
+
+from Movie import Movie
+from Person import Person 
+
+from google.appengine.ext import webapp
+from google.appengine.ext import db
+
+class LookupHandler(webapp.RequestHandler):
+  @classmethod
+  def encodePerson(cls, person, document):
+    personElement = document.createElement("person")
+    personElement.setAttribute("firstName", person.firstName)
+    personElement.setAttribute("lastName", person.lastName)
+    personElement.setAttribute("key", str(person.key()))
+
+    return personElement
+
+  @classmethod
+  def encodeMovie(cls, movie, document):
+    movieElement = document.createElement("movie")
+    movieElement.setAttribute("name", movie.name)
+    movieElement.setAttribute("year", str(movie.year))
+    movieElement.setAttribute("id", str(movie.key()))
+
+    return movieElement
