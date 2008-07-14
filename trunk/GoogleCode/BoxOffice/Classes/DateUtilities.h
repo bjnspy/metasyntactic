99bd2f54e41972b3f5a668753547604b6cfbf71f
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-07-14 03:34:30 +0000

    adding files

---
+++ trunk/GoogleCode/BoxOffice/Classes/DateUtilities.h
@@ -0,0 +1,15 @@
+//
+//  DateUtilies.h
+//  BoxOffice
+//
+//  Created by Cyrus Najmabadi on 7/13/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+@interface DateUtilities : NSObject {
+
+}
+
++ (NSString*) timeSinceNow:(NSDate*) date;
+
+@end
