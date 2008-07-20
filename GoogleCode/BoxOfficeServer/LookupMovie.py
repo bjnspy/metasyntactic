4f01dcbd9f241f91b1907457c19f6070d32fa66d
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-07-20 08:07:59 +0000

    movie review lookup to the server

--- GoogleCode/BoxOfficeServer/LookupMovie.py
+++ GoogleCode/BoxOfficeServer/LookupMovie.py
@@ -2,6 +2,7 @@
 
 import wsgiref.handlers
 from xml.dom.minidom import getDOMImplementation
+import zlib
 
 from Movie import Movie
 from Person import Person 
@@ -9,56 +10,109 @@ from Lookup import LookupHandler
 
 from google.appengine.ext import webapp
 from google.appengine.ext import db
+from google.appengine.api import memcache
+
+import imdb
 
 class LookupMovieHandler(LookupHandler):
   def get(self):
-    id = self.request.get("id")
-
-    movie = Movie.get(id)
-    if movie is None:
+    memcache.flush_all()    
+    q = self.request.get("q")
+    listings = self.get_listings_from_cache(q)
+    if listings is None:
       self.response.out.write("")
       return
 
+    self.response.out.write(zlib.decompress(listings))
+
+
+  def get_listings_from_cache(self, q):
+    listings = memcache.get("Movie_" + q)
+
+    if listings is None:
+      listings = self.get_listings_from_webservice(q)
+      memcache.Client().set("Movie_" + q, listings, time=3*24*60*60)
+
+    return listings
+    
+    
+  def encodeCharacter(self, c, document):
+    element = document.createElement("character")
+    element.setAttribute("name", c['name'])
+    if c.getID():
+        element.setAttribute("id", c.getID())
+    return element
+    
+  def encodePerson(self, p, document):
+    element = document.createElement("person")
+    element.setAttribute("name", p['long imdb canonical name'])
+    element.setAttribute("id", p.getID())
+    if p.currentRole:
+        if isinstance(p.currentRole, list):
+            for r in p.currentRole:
+                element.appendChild(self.encodeCharacter(r, document))
+        else:
+            element.appendChild(self.encodeCharacter(p.currentRole, document))
+            
+    return element
+
+  def encodePeople(self, movie, key, name, document, parent):
+    if movie.has_key(key):
+        element = document.createElement(name)
+        parent.appendChild(element)
+        for p in movie[key]:
+            if isinstance(p, imdb.Person.Person):
+                element.appendChild(self.encodePerson(p, document))
+
+
+  def get_listings_from_webservice(self, q):    
+    i = imdb.IMDb(accessSystem='http')
+    #movie = i.get_movie(q, info=imdb.Movie.Movie.default_info + ('trivia', 'goofs', 'quotes', 'crazy credits'))
+    movie = i.get_movie(q, info=imdb.Movie.Movie.default_info)
+    
+#    self.response.out.write(movie.keys())
+#    self.response.out.write(unicode(movie.summary(), "utf8"))
+    
     document = getDOMImplementation().createDocument(None, "result", None)
     resultElement = document.documentElement
-
-    movieElement = self.encodeMovie(movie, document)
-    resultElement.appendChild(movieElement)
-
-    directorsElement = document.createElement("directors")
-    resultElement.appendChild(directorsElement)
-
-    writersElement = document.createElement("writers")
-    resultElement.appendChild(writersElement)
-
-    castElement = document.createElement("cast")
-    resultElement.appendChild(castElement)
-
-    people = []
-    for key in movie.directors:
-      person = Person.get(key)
-      if not person is None:
-        people.append(person)
-
-    for person in people.sort():
-      directorsElement.appendChild(self.encodePerson(person, document))
-
-    people = []
-    for key in movie.writers:
-      person = Person.get(key)
-      if not person is None:
-        people.append(person)
-
-    for person in people.sort():
-      writersElement.appendChild(self.encodePerson(person, document))
-
-    people = []
-    for key in movie.cast:
-      person = Person.get(key)
-      if not person is None:
-        people.append(person)
-
-    for person in people.sort():
-      castElement.appendChild(self.encodePerson(person, document))
-
-    self.response.out.write(document.toxml())
+        
+    self.encodePeople(movie, 'producer', "producers", document, resultElement)
+    self.encodePeople(movie, 'writer', "writers", document, resultElement)
+    self.encodePeople(movie, 'director', "directors", document, resultElement)
+    self.encodePeople(movie, 'original music', "music", document, resultElement)
+    self.encodePeople(movie, 'cast', "cast", document, resultElement)
+            
+    if movie.has_key('cover url'):
+        element = document.createElement("imageUrl")
+        element.setAttribute("value", movie['cover url'])
+        resultElement.appendChild(element)
+    
+    if movie.has_key('genres'):
+        element = document.createElement("genres")
+        resultElement.appendChild(element)
+        for g in movie['genres']:
+            genreElement = document.createElement("genre")
+            genreElement.setAttribute("value", g)
+            element.appendChild(genreElement)
+        
+    if movie.has_key('plot outline'):
+        element = document.createElement("outline")
+        element.setAttribute("value", movie['plot outline'])
+        resultElement.appendChild(element)
+        
+    if movie.has_key('plot') and (len(movie['plot']) > 0):
+        element = document.createElement("plot")
+        element.setAttribute("value", movie['plot'][0])
+        resultElement.appendChild(element)
+        
+    if movie.has_key('votes'):
+        element = document.createElement("votes")
+        element.setAttribute("value", str(movie['votes']))
+        resultElement.appendChild(element)
+        
+    if movie.has_key('rating'):
+        element = document.createElement("rating")
+        element.setAttribute("value", str(movie['rating']))
+        resultElement.appendChild(element)
+        
+    return zlib.compress(document.toxml("utf-8"))
--- GoogleCode/BoxOfficeServer/Search.py
+++ GoogleCode/BoxOfficeServer/Search.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import wsgiref.handlers
+import zlib
 
 from xml.dom.minidom import getDOMImplementation
 
@@ -10,22 +11,54 @@ from Lookup import LookupHandler
 
 from google.appengine.ext import webapp
 from google.appengine.ext import db
+from google.appengine.api import memcache
+
+import imdb
 
 class SearchHandler(LookupHandler):
   def get(self):
     q = self.request.get("q")
+    listings = self.get_listings_from_cache(q)
+    if listings is None:
+      self.response.out.write("")
+      return
+
+    self.response.out.write(zlib.decompress(listings))
+
+
+  def get_listings_from_cache(self, q):
+    listings = memcache.get("Search_" + q)
+
+    if listings is None:
+      listings = self.get_listings_from_webservice(q)
+      memcache.Client().set("Search_" + q, listings, time=3*24*60*60)
+
+    return listings
+    
+
+  def get_listings_from_webservice(self, q):    
+    i = imdb.IMDb(accessSystem='http')
+    movies = i.search_movie(q)
+    people = i.search_person(q)
 
     document = getDOMImplementation().createDocument(None, "result", None)
     resultElement = document.documentElement
+
     moviesElement = document.createElement("movies")
-    peopleElement = document.createElement("people")
     resultElement.appendChild(moviesElement)
+    for m in movies:
+        movieElement = document.createElement("movie")
+        movieElement.setAttribute("name", m['canonical title'])
+        movieElement.setAttribute("year", m['year'])
+        movieElement.setAttribute("id", m.getID())
+        moviesElement.appendChild(movieElement);
+        
+    peopleElement = document.createElement("people")
     resultElement.appendChild(peopleElement)
-    
-    for person in Person.all().search(q).order('lastName').order('firstName'):
-      peopleElement.appendChild(LookupHandler.encodePerson(person, document))
-
-    for movie in Movie.all().search(q).order('-year').order('name'):
-      moviesElement.appendChild(LookupHandler.encodeMovie(movie, document))
-
-    self.response.out.write(document.toxml())
+    for p in people:
+        personElement = document.createElement("person")
+        personElement.setAttribute("name", p['long imdb canonical name']) #unicode(p['long imdb canonical name'], "iso-8859-1"))
+        personElement.setAttribute("id", p.getID())
+        peopleElement.appendChild(personElement)
+        
+    return zlib.compress(resultElement.toxml("utf8"))
