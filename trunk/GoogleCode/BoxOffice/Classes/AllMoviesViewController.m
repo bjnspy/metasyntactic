80bda430d45dc08432a2b6981d046fe9b21a8f0c
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-07-20 15:35:00 +0000

    use more specific 'back' button titles

--- trunk/GoogleCode/BoxOffice/Classes/AllMoviesViewController.m
+++ trunk/GoogleCode/BoxOffice/Classes/AllMoviesViewController.m
@@ -160,6 +160,8 @@
          @"#", @"A", @"B", @"C", @"D", @"E", @"F", @"G", @"H", 
          @"I", @"J", @"K", @"L", @"M", @"N", @"O", @"P", @"Q", 
          @"R", @"S", @"T", @"U", @"V", @"W", @"X", @"Y", @"Z", nil];
+        
+        self.title = NSLocalizedString(@"Movies", nil);
     }
     
     return self;
--- trunk/GoogleCode/BoxOffice/Classes/AllTheatersViewController.m
+++ trunk/GoogleCode/BoxOffice/Classes/AllTheatersViewController.m
@@ -181,6 +181,8 @@
              @"I", @"J", @"K", @"L", @"M", @"N", @"O", @"P", @"Q", 
              @"R", @"S", @"T", @"U", @"V", @"W", @"X", @"Y", @"Z", nil];
         }
+        
+        self.title = NSLocalizedString(@"Theaters", nil);
     }
     
     return self;
