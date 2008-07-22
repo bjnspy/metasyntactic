020c5fb21598a5dea85166f47fccd1e2398bb34c
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-07-22 06:11:36 +0000

    save reviews to disk, not to the preferences file

--- trunk/GoogleCode/BoxOffice/Classes/ReviewCache.m
+++ trunk/GoogleCode/BoxOffice/Classes/ReviewCache.m
@@ -19,12 +19,10 @@
 
 @synthesize model;
 @synthesize gate;
-@synthesize movieToReviewMap;
 
 - (void) dealloc {
     self.model = nil;
     self.gate = nil;
-    self.movieToReviewMap = nil;
     
     [super dealloc];
 }
@@ -33,26 +31,6 @@
     if (self = [super init]) {
         self.model = model_;
         self.gate = [[[NSLock alloc] init] autorelease];
-        self.movieToReviewMap = [NSMutableDictionary dictionary];
-        
-        NSDictionary* dict = [NSDictionary dictionaryWithContentsOfFile:[Application reviewsFile]];
-        if (dict != nil) {
-            for (NSString* movieId in dict) {
-                NSArray* dateAndEncodedReviews = [dict objectForKey:movieId];
-                
-                NSDate* date = [dateAndEncodedReviews objectAtIndex:0];
-                NSArray* encodedReviews = [dateAndEncodedReviews objectAtIndex:1];
-                
-                NSMutableArray* reviews = [NSMutableArray array];
-                for (NSDictionary* child in encodedReviews) {
-                    [reviews addObject:[Review reviewWithDictionary:child]];
-                }
-                
-                NSArray* dateAndReviews = [NSArray arrayWithObjects:date, reviews, nil];
-                [self.movieToReviewMap setObject:dateAndReviews
-                                          forKey:movieId];
-            }
-        }
     }
     
     return self;
@@ -62,16 +40,43 @@
     return [[[ReviewCache alloc] initWithModel:model] autorelease];
 }
 
+- (NSString*) reviewFilePath:(NSString*) title {
+    NSString* sanitizedTitle = [title stringByReplacingOccurrencesOfString:@"/" withString:@"-slash-"];
+    return [[[Application reviewsFolder] stringByAppendingPathComponent:sanitizedTitle] stringByAppendingPathExtension:@"plist"];
+}
+
+- (NSArray*) loadReviewFile:(NSString*) title {
+    NSArray* encodedReviews = [NSArray arrayWithContentsOfFile:[self reviewFilePath:title]];
+    
+    NSMutableArray* reviews = [NSMutableArray array];
+    for (NSDictionary* dict in encodedReviews) {
+        [reviews addObject:[Review reviewWithDictionary:dict]];
+    }
+    return reviews;
+}
+
+
+- (void) saveMovie:(NSString*) title reviews:(NSArray*) reviews {
+    NSMutableArray* encodedReviews = [NSMutableArray array];
+    for (Review* review in reviews) {
+        [encodedReviews addObject:[review dictionary]];
+    }
+    
+    [encodedReviews writeToFile:[self reviewFilePath:title] atomically:YES];
+}
+
 - (void) update:(NSDictionary*) supplementaryInformation ratingsProvider:(NSInteger) ratingsProvider {
     NSMutableDictionary* infoWithReviews = [NSMutableDictionary dictionary];
     NSMutableDictionary* infoWithoutReviews = [NSMutableDictionary dictionary];
     
     for (NSString* title in supplementaryInformation) {
-        NSArray* dateAndReviews = [self.movieToReviewMap objectForKey:title];
-        if (dateAndReviews == nil) {
+        NSError* error = nil;
+        NSDate* downloadDate = [[[NSFileManager defaultManager] attributesOfItemAtPath:[self reviewFilePath:title]
+                                                                                 error:&error] objectForKey:NSFileModificationDate];
+        
+        if (downloadDate == nil) {
             [infoWithoutReviews setObject:[supplementaryInformation objectForKey:title] forKey:title];
         } else {
-            NSDate* downloadDate = [dateAndReviews objectAtIndex:0];
             NSDate* now = [NSDate date];
                         
             NSDateComponents* downloadDateComponents = [[NSCalendar currentCalendar] components:NSDayCalendarUnit fromDate:downloadDate];
@@ -144,9 +149,7 @@
         return;
     }
     
-    NSArray* dateAndReviews = [NSArray arrayWithObjects:[NSDate date], reviews, nil];
-    
-    [self.movieToReviewMap setObject:dateAndReviews forKey:movieId];
+    [self saveMovie:movieId reviews:reviews];
 }
 
 - (void) downloadReviews:(NSDictionary*) supplementaryInformation
@@ -175,7 +178,6 @@
         NSInteger ratingsProvider = [[arguments lastObject] intValue];
         for (int i = 0; i < [arguments count] - 1; i++) {
             [self downloadReviews:[arguments objectAtIndex:i] ratingsProvider:ratingsProvider];
-            [self performSelectorOnMainThread:@selector(onComplete:) withObject:nil waitUntilDone:NO];
         }
         
         [autoreleasePool release];
@@ -184,39 +186,19 @@
 }
 
 - (NSArray*) reviewsForMovie:(NSString*) movieTitle {
-    NSArray* dateAndReviews = [self.movieToReviewMap objectForKey:movieTitle];
-    
-    NSArray* array = [dateAndReviews objectAtIndex:1];
+    NSArray* array = [self loadReviewFile:movieTitle];
     if (array == nil) {
         return [NSArray array];
     }
     return array;
 }
 
-- (void) onComplete:(id) nothing {
-    NSMutableDictionary* dict = [NSMutableDictionary dictionary];
-    for (NSString* movieId in self.movieToReviewMap) {
-        NSArray* dateAndReviews = [self.movieToReviewMap objectForKey:movieId];
-        
-        NSMutableArray* encodedReviews = [NSMutableArray array];
-        for (Review* review in [dateAndReviews objectAtIndex:1]) {
-            [encodedReviews addObject:[review dictionary]];
-        }
-        
-        NSArray* dateAndEncodedReviews = [NSArray arrayWithObjects:[dateAndReviews objectAtIndex:0], encodedReviews, nil];
-        [dict setObject:dateAndEncodedReviews forKey:movieId];
-    }
-    
-    [dict writeToFile:[Application reviewsFile] atomically:YES];
-}
-
 - (void) clear {
-    self.movieToReviewMap = [NSMutableDictionary dictionary];
-    [self onComplete:nil];
-}
-
-- (void) applicationWillTerminate {
-    [self onComplete:nil];
+    NSArray* contents = [[NSFileManager defaultManager] directoryContentsAtPath:[Application reviewsFolder]];
+    for (NSString* filePath in contents) {
+        NSError* error = nil;
+        [[NSFileManager defaultManager] removeItemAtPath:filePath error:&error];
+    }
 }
 
 @end
