bed3eb02b7441f8204f9df2a2a660b0f60970106
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-07-23 03:12:25 +0000

    support metacritic reviews

--- GoogleCode/BoxOfficeServer/LookupMovieListings.py
+++ GoogleCode/BoxOfficeServer/LookupMovieListings.py
@@ -19,8 +19,12 @@ from google.appengine.api import urlfetch
 
 class LookupMovieListingsHandler(webapp.RequestHandler):
   def get(self):
-#    memcache.flush_all()    
-    listings = self.get_listings_from_cache()
+    memcache.flush_all()
+    q = self.request.get("q")
+    if not (q == "RottenTomatoes" or q == "Metacritic"):
+        q = "RottenTomatoes"
+    
+    listings = self.get_listings_from_cache(q)
     if listings is None:
       self.response.out.write("")
       return
@@ -35,32 +39,117 @@ class LookupMovieListingsHandler(webapp.RequestHandler):
     return delta.seconds >= (3600 * 8)
     
 
-  def get_listings_from_cache(self):
-    listings = memcache.get("MovieListings")
+  def get_listings_from_cache(self, q):
+    listings = memcache.get("MovieListings_" + q)
 
     if listings is None or self.too_old(listings):
-      listings = self.get_listings_from_datastore()
-      memcache.Client().set("MovieListings", listings)
+      listings = self.get_listings_from_datastore(q)
+      memcache.Client().set("MovieListings_" + q, listings)
 
     return listings
     
 
-  def get_listings_from_datastore(self):
-    listings = MovieListings.get_by_key_name("MovieListings")
+  def get_listings_from_datastore(self, q):
+    listings = MovieListings.get_by_key_name("MovieListings_" + q)
 
     if listings is None or self.too_old(listings):
-      listings = self.get_listings_from_webservice()
+        listings = self.get_listings_from_webservice(q)
 
     return listings
 
 
-  def get_listings_from_webservice(self):
+  def get_listings_from_rotten_tomatoes_webservice(self, q):
     url = "http://i.rottentomatoes.com/syndication/tab/complete_movies.txt"
     content = urlfetch.fetch(url).content
     encoded = unicode(content, "iso-8859-1")
 
-    listings = MovieListings.get_or_insert("MovieListings")
+    listings = MovieListings.get_or_insert("MovieListings_" + q)
     listings.data = encoded
     listings.put()
 
     return listings
+
+
+  def get_listings_from_metacritic_webservice(self, q):
+    url = "http://www.metacritic.com/film/"
+    content = urlfetch.fetch(url).content
+    content = unicode(content, "iso-8859-1")
+  
+    section1Start = content.find("<div id=\"sortbyname1\"")
+    if section1Start < 0:
+        return None
+        
+    section1End = content.find("<div id=\"sortbyscore1\"", section1Start)
+    if section1End < 0:
+        return None
+  
+    section2Start = content.find("<div id=\"sortbyname2\"", section1End)
+    if section2Start < 0:
+        return None
+        
+    section2End = content.find("<div id=\"sortbyscore2\"", section2Start)
+    if section2End < 0:
+        return None
+        
+    reviews1 = self.extract_metacritic_reviews(content[section1Start:section1End])
+    reviews2 = self.extract_metacritic_reviews(content[section2Start:section2End])
+  
+    listings = MovieListings.get_or_insert("MovieListings_" + q)
+    listings.data = reviews1 + reviews2
+    listings.put()
+
+    return listings
+    
+    
+  def extract_metacritic_reviews(self, content):
+    sections = content.split("<SPAN")
+    result = ""
+    
+    for s in sections:
+        startBracketIndex = s.find(">")
+        if startBracketIndex < 0:
+            continue
+            
+        endBracketIndex = s.find("</SPAN>", startBracketIndex)
+        if endBracketIndex < 0:
+            continue
+        
+        hrefIndex = s.find("<A HREF=\"", endBracketIndex)
+        if hrefIndex < 0:
+            continue
+            
+        linkStart = hrefIndex + len("<A HREF=\"")
+        quoteIndex = s.find("\"", linkStart)
+        if quoteIndex < 0:
+            continue
+            
+        titleStart = s.find("<B>", quoteIndex)
+        title = ""
+        if titleStart > 0:
+            titleEnd = s.find("</B>", titleStart)
+            if titleEnd > 0:
+                title = s[titleStart + len("<B>"):titleEnd]
+        else:
+            titleStart = s.find(">", quoteIndex)
+            if titleStart > 0:
+                titleEnd = s.find("<", titleStart)
+                if titleEnd > 0:
+                    title = s[titleStart + 1:titleEnd]
+                    
+        title = title.replace("&#039;", "'")
+        score = s[startBracketIndex + 1:endBracketIndex]
+        link = "http://www.metacritic.com" + s[linkStart:quoteIndex]
+        
+        result += score + "\t" + link + "\t" + title + "\n"
+        
+    return result
+    
+
+  def get_listings_from_webservice(self, q):
+    if q == "Metacritic":
+        return self.get_listings_from_metacritic_webservice(q)
+        
+    if q == "RottenTomatoes":
+        return self.get_listings_from_rotten_tomatoes_webservice(q)
+        
+    return None
--- GoogleCode/BoxOfficeServer/LookupMovieReviews.py
+++ GoogleCode/BoxOfficeServer/LookupMovieReviews.py
@@ -6,6 +6,8 @@ import urllib
 import datetime
 import zlib
 import random
+import LookupRottenTomatoesReviews
+import LookupMetacriticReviews
 
 from xml.dom.minidom import getDOMImplementation, parseString
 
@@ -22,6 +24,7 @@ from google.appengine.api import urlfetch
 
 class LookupMovieReviewsHandler(webapp.RequestHandler):
   def get(self):
+#    memcache.flush_all()
     q = self.request.get("q")
 
     listings = self.get_listings_from_cache(q)
@@ -44,121 +47,15 @@ class LookupMovieReviewsHandler(webapp.RequestHandler):
 
 
   def get_listings_from_webservice(self, q):
-    content = urlfetch.fetch(q).content
+    if q.find("www.rottentomatoes.com") >= 0:
+        if q[-1] != "/":
+            q += "/"
+        q += "?critic=creamcrop"
+        content = urlfetch.fetch(q).content
+        return LookupRottenTomatoesReviews.LookupRottenTomatoesReviewsHandler().get_listings(content)
 
-    sections  = self.extract_review_sections(content)
-    result = ""
-    for s in sections:
-        review = self.extract_review(s)
-        if not review is None:
-            result += (review + "\n")
-    
-    return zlib.compress(result)
-    
-    
-  def extract_review(self, section):
-    text = self.extract_text(section)
-    if text is None:
-        return None
-    
-    positive = section.find("fresh.gif") >= 0
-    link = self.extract_link(section)
-    author = self.extract_author(section)
-    source = self.extract_source(section)
-    link = self.extract_link(section)
-    
-    text = self.replace_lines(text)
-    link = self.replace_lines(link)
-    author = self.replace_lines(author)
-    source = self.replace_lines(source)
-    
-    return text + "\t" + str(positive) + "\t" + link + "\t" + author + "\t" + source
-
-
-  def extract_critic_information(self, section, anchor):
-    anchorLocation = section.find(anchor)
-    if anchorLocation < 0:
-        return ""
+    if q.find("www.metacritic.com") >= 0:
+        content = urlfetch.fetch(q).content
+        return LookupMetacriticReviews.LookupMetacriticReviewsHandler().get_listings(content)
         
-    startTag = "\">"
-    startLocation = section.find(startTag, anchorLocation + len(anchor))
-    if startLocation < 0:
-        return ""
-        
-    endLocation = section.find("</a>", startLocation)
-    if endLocation < 0:
-        return ""
-        
-    result = section[(startLocation + len(startTag)):endLocation]
-    result = result.replace("&amp;", "&")
-    
-    return result;
-
-
-  def extract_author(self, section):
-    return self.extract_critic_information(section, "<div class=\"author\">")
-
-
-  def extract_source(self, section):
-    return self.extract_critic_information(section, "<div class=\"source\">")
-
-
-  def extract_link(self, section):
-    fullReviewLocation = section.find("Full Review")
-    if fullReviewLocation < 0:
-        return ""
-
-    hrefString = "href=\""
-    startLocation = section[:fullReviewLocation].rfind("href=\"")
-    if startLocation < 0:
-        return ""
-        
-    quoteLocation = section.find("\"", startLocation + len(hrefString))
-    if quoteLocation < 0:
-        return ""
-        
-    address = section[(startLocation + len(hrefString)):quoteLocation]
-    return "http://www.rottentomatoes.com" + address
-
-
-  def extract_text(self, section):
-    startLocation = section.find("<p>")
-    if startLocation < 0:
-        return None
-        
-    endLocation = section.find("</p>", startLocation)
-    if endLocation < 0:
-        return None
-    
-    result = section[(startLocation + 3):endLocation].strip()
-    result = result.replace("<I>", "")
-    result = result.replace("<i>", "")
-    result = result.replace("</i>", "")
-
-    if result.find("to read the article") >= 0:
-        return None
-        
-    return result
-    
-    
-  def replace_lines(self, string):
-    string = string.replace("\r\n", " ")
-    string = string.replace("\r", " ")
-    string = string.replace("\n", " ")
-    string = string.replace("\t", " ")
-    return string
-
-    
-  def extract_review_sections(self, content):
-    startLocation = content.find("<div id=\"Content_Reviews\"")
-    if startLocation < 0:
-      return []
-        
-    endLocation = content.find("<div class=\"main_reviews_column_nav\"", startLocation)
-    if endLocation < 0:
-      return []
-        
-    subsection = content[startLocation:endLocation]
-    sections = subsection.split("quoteBubble")
-    
-    return sections[1:]
+    return None
