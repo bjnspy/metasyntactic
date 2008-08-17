964f20c606a7007fdb222e1bb45403c184ae70cc
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-08-17 17:49:20 +0000

    adding classes

---
+++ GoogleCode/BoxOffice/Classes/AutoResizingCell.h
@@ -0,0 +1,18 @@
+//
+//  AutoResizingCell.h
+//  BoxOffice
+//
+//  Created by Cyrus Najmabadi on 8/17/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AutoResizingCell : UITableViewCell {
+    UILabel* label;
+}
+
+@property (retain) UILabel* label;
+
+@end
