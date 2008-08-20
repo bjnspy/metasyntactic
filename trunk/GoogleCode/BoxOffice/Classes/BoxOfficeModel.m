7dab16d3b25cd95fb98e0cea256fc14a98db96e4
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-08-20 17:27:33 +0000

    fixing up french translation

--- trunk/GoogleCode/BoxOffice/Classes/BoxOfficeModel.m
+++ trunk/GoogleCode/BoxOffice/Classes/BoxOfficeModel.m
@@ -309,10 +309,10 @@ static NSString* currentVersion = @"1.4.1";
 - (BOOL) useKilometers {
     // yeah... so the UK supposedly uses metric...
     // except they don't.  so we special case them to stick with 'miles' in the UI.
-    BOOL usesMetric = [[[NSLocale currentLocale] objectForKey:NSLocaleUsesMetricSystem] boolValue];
+    BOOL isMetric = [[[NSLocale currentLocale] objectForKey:NSLocaleUsesMetricSystem] boolValue];
     BOOL isUK = [@"GB" isEqual:[[NSLocale currentLocale] objectForKey:NSLocaleCountryCode]];
     
-    return usesMetric && !isUK;
+    return isMetric && !isUK;
 }
 
 
