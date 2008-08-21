183cbd4ffefa87ecab89ba24ed23de5e31e70a93
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-08-21 21:06:57 +0000

    improving danish

--- trunk/GoogleCode/BoxOffice/Classes/CreditsViewController.m
+++ trunk/GoogleCode/BoxOffice/Classes/CreditsViewController.m
@@ -78,7 +78,7 @@
     } else if (section == 5) {
         return 3;
     } else if (section == 6) {
-        return 5;
+        return 6;
     } else if (section == 7) {
         return 1;
     }
@@ -133,21 +133,24 @@
     NSString* language;
     NSString* person;
     if (row == 0) {
+        language = [[NSLocale currentLocale] displayNameForKey:NSLocaleIdentifier value:@"da"];
+        person = @"Allan Lund Jensen";
+    } else if (row == 1) {
         language = [[NSLocale currentLocale] displayNameForKey:NSLocaleIdentifier value:@"fr"];
         person = @"Jonathan Grenier";
-    } else if (row == 1) {
+    } else if (row == 2) {
         language = [[NSLocale currentLocale] displayNameForKey:NSLocaleIdentifier value:@"ja"];
         person = @"Leo Yamamoto";
-    } else if (row == 2) {
+    } else if (row == 3) {
         language = [[NSLocale currentLocale] displayNameForKey:NSLocaleIdentifier value:@"pt"];
         person = @"Pedro Pinhão";
-    } else if (row == 3) {
+    } else if (row == 4) {
         language = [[NSLocale currentLocale] displayNameForKey:NSLocaleIdentifier value:@"es"];
         person = @"Jorge Herskovic";
-    } else if (row == 4) {
+    } else if (row == 5) {
         language = [[NSLocale currentLocale] displayNameForKey:NSLocaleIdentifier value:@"sv"];
         person = @"André van Haren";
-    } else if (row == 5) {
+    } else if (row == 6) {
         language = [[NSLocale currentLocale] displayNameForKey:NSLocaleIdentifier value:@"tr"];
         person = @"Oğuz Taş";
     }
