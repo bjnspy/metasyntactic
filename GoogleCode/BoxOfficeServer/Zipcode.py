e75fe5db8179a818c524f246f7b5b3a3d442a8e5
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-08-28 03:51:18 +0000

    tagging 1.5

--- GoogleCode/BoxOfficeServer/Zipcode.py
+++
@@ -1,9 +0,0 @@
-from google.appengine.ext import db
-from google.appengine.ext import search
-
-class Zipcode(db.Model):
-    value = db.StringProperty()
-    latitude = db.FloatProperty()
-    longitude = db.FloatProperty()
-    city = db.StringProperty()
-    state = db.StringProperty()
