5e678559850512b4118f6e0dd8ad8b1288746d31
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-09-05 20:01:53 +0000

    tagging 1.6

--- NowPlayingServer/TrailerListings.py
+++
@@ -1,7 +0,0 @@
-from google.appengine.ext import db
-from google.appengine.ext import search
-
-class TrailerListings(db.Model):
-    data = db.TextProperty()
-    saveDate = db.DateTimeProperty(auto_now=True)
-
