14c1eed657af4e58f1a58940d1113affebcc98b7
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-09-05 16:49:31 +0000

    refresh automatically

--- trunk/NowPlaying/iPhone/Classes/MovieDetailsViewController.m
+++ trunk/NowPlaying/iPhone/Classes/MovieDetailsViewController.m
@@ -401,10 +401,10 @@
 
 
 - (void) didSelectShowHiddenTheaters {
-    [self.tableView deselectRowAtIndexPath:self.tableView.indexPathForSelectedRow animated:YES];
+    [self.tableView deselectRowAtIndexPath:self.tableView.indexPathForSelectedRow animated:NO];
 
     filterTheatersByDistance = NO;
-    [self performSelector:@selector(refresh) withObject:nil afterDelay:0.3];
+    [self refresh];
 }
 
 
