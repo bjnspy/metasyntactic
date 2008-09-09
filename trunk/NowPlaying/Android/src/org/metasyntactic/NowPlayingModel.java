e77af1ef1f2aa8e3f65bd457ef9589b315d4ec95
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-09-09 20:22:38 +0000

    fleshing out hte interface between the activity and hte service

--- trunk/NowPlaying/Android/src/org/metasyntactic/NowPlayingModel.java
+++ trunk/NowPlaying/Android/src/org/metasyntactic/NowPlayingModel.java
@@ -1,7 +1,13 @@
 package org.metasyntactic;
 
+import org.metasyntactic.data.Movie;
+import org.metasyntactic.data.Theater;
+
+import java.util.List;
+
 /** @author cyrusn@google.com (Cyrus Najmabadi) */
 public class NowPlayingModel {
+
   public void update() {
     new Thread(new Runnable() {
       public void run() {
@@ -11,6 +17,42 @@ public class NowPlayingModel {
   }
 
   private void updateBackgroundEntryPoint() {
-    
+
+  }
+
+  public int getSelectedTabIndex() {
+    return 0;
+  }
+
+  public void setSelectedTabIndex(int index) {
+  }
+
+  public int getAllMoviesSelecetedSortIndex() {
+    return 0;
+  }
+
+  public void setAllMoviesSelectedSortIndex(int index) {
+  }
+
+  public int getAllTheatersSelectedSortIndex() {
+    return 0;
+  }
+
+  public void setAllTheatersSelectedSortIndex() {
+  }
+
+  public int getUpcomingMovieSelectedSortIndex() {
+    return 0;
+  }
+
+  public void setUpcomingMovieSelectedSortIndex(int index) {
+  }
+
+  public List<Movie> getMovies() {
+    return null;
+  }
+
+  public List<Theater> getTheaters() {
+    return null;
   }
 }
