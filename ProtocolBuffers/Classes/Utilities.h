64627056559a894c8d250ea75f292004b10af92b
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-09-27 10:13:27 +0000

    extract bit twiddlers into their own functions

---
+++ ProtocolBuffers/Classes/Utilities.h
@@ -0,0 +1,18 @@
+//
+//  Utilities.h
+//  ProtocolBuffers
+//
+//  Created by Cyrus Najmabadi on 9/27/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+int64_t convertFloat64ToInt64(Float64 f);
+int32_t convertFloat32ToInt32(Float32 f);
+
+uint64_t convertInt64ToUInt64(int64_t i);
+int64_t  convertUInt64ToInt64(uint64_t u);
+uint32_t convertInt32ToUInt32(int32_t i);
+int64_t  convertUInt32ToInt32(uint32_t u);
+
+int32_t logicalRightShift32(int32_t value, int32_t spaces);
+int64_t logicalRightShift64(int64_t value, int32_t spaces);
