7d995f18e0ed8897040e8d65f63f2c14aac3dfb2
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-09-29 20:23:55 +0000

    make a library

---
+++ trunk/ProtocolBuffers/Classes/ProtocolBuffers.h
@@ -0,0 +1,61 @@
+// Protocol Buffers - Google's data interchange format
+// Copyright 2008 Google Inc.
+// http://code.google.com/p/protobuf/
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "ForwardDeclarations.h"
+
+#import "AbstractMessage.h"
+#import "AbstractMessage_Builder.h"
+#import "CodedInputStream.h"
+#import "CodedOutputStream.h"
+#import "Descriptor.h"
+#import "Descriptor.pb.h"
+#import "DescriptorPool.h"
+#import "DynamicMessage.h"
+#import "DynamicMessage_Builder.h"
+#import "EnumDescriptor.h"
+#import "EnumValueDescriptor.h"
+#import "ExtensionRegistry.h"
+#import "ExtensionRegistry_DescriptorIntPair.h"
+#import "ExtensionRegistry_ExtensionInfo.h"
+#import "Field.h"
+#import "FieldDescriptor.h"
+#import "FieldDescriptorType.h"
+#import "FieldSet.h"
+#import "Field_Builder.h"
+#import "FileDescriptor.h"
+#import "GeneratedMessage.h"
+#import "GeneratedMessage_Builder.h"
+#import "GeneratedMessage_FieldAccessor.h"
+#import "GeneratedMessage_FieldAccessorTable.h"
+#import "GeneratedMessage_RepeatedEnumFieldAccessor.h"
+#import "GeneratedMessage_RepeatedFieldAccessor.h"
+#import "GeneratedMessage_RepeatedMessageFieldAccessor.h"
+#import "GeneratedMessage_SingularEnumFieldAccessor.h"
+#import "GeneratedMessage_SingularFieldAccessor.h"
+#import "GeneratedMessage_SingularMessageFieldAccessor.h"
+#import "GenericDescriptor.h"
+#import "Message.h"
+#import "Message_Builder.h"
+#import "ObjectiveCType.h"
+#import "ProtocolBufferType.h"
+#import "ProtocolBuffers.h"
+#import "RpcChannel.h"
+#import "RpcController.h"
+#import "Service.h"
+#import "UnknownFieldSet.h"
+#import "UnknownFieldSet_Builder.h"
+#import "Utilities.h"
+#import "WireFormat.h"
