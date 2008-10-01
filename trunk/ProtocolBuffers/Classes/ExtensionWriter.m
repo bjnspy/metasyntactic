de090fefab7fdd36034d70c5b3f4592a9677fa16
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-10-01 19:41:08 +0000

    renaming files

---
+++ trunk/ProtocolBuffers/Classes/ExtensionWriter.m
@@ -0,0 +1,77 @@
+// Protocol Buffers - Google's data interchange format
+// Copyright 2008 Google Inc.
+// http://code.google.com/p/protobuf/
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "GeneratedMessage_ExtendableMessage_ExtensionWriter.h"
+
+#import "FieldSet.h"
+
+@implementation GeneratedMessage_ExtendableMessage_ExtensionWriter
+
+@synthesize extensions;
+@synthesize enumerator;
+@synthesize nextKey;
+@synthesize nextValue;
+
+- (void) dealloc {
+    self.extensions = nil;
+    self.enumerator = nil;
+    self.nextKey = nil;
+    self.nextValue = nil;
+
+    [super dealloc];
+}
+
+
+- (void) moveNext {
+    self.nextKey = enumerator.nextObject;
+    if (nextKey != nil) {
+        self.nextValue = [extensions.fields objectForKey:nextKey];
+    }
+}
+
+
+- (id) initWithExtensions:(FieldSet*) extensions_ {
+    if (self = [super init]) {
+        self.extensions = extensions_;
+        self.enumerator = extensions.fields.keyEnumerator;
+        
+        [self moveNext];
+    }
+    
+    return self;
+}
+
+
++ (GeneratedMessage_ExtendableMessage_ExtensionWriter*) writerWithExtensions:(FieldSet*) extensions {
+    return [[[GeneratedMessage_ExtendableMessage_ExtensionWriter alloc] initWithExtensions:extensions] autorelease];
+}
+
+
+/**
+ * Used by subclasses to serialize extensions.  Extension ranges may be
+ * interleaved with field numbers, but we must write them in canonical
+ * (sorted by field number) order.  ExtensionWriter helps us write
+ * individual ranges of extensions at once.
+ */
+- (void) writeUntil:(int32_t) end output:(CodedOutputStream*) output {
+    while (nextKey != nil && [nextValue getNumber] < end) {
+        [extensions writeField:nextKey value:nextValue output:output];
+        [self moveNext];
+    }
+}
+
+
+@end
---
+++ trunk/ProtocolBuffers/Classes/GeneratedExtension.m
@@ -0,0 +1,175 @@
+// Protocol Buffers - Google's data interchange format
+// Copyright 2008 Google Inc.
+// http://code.google.com/p/protobuf/
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "GeneratedMessage_GeneratedExtension.h"
+
+
+@implementation GeneratedMessage_GeneratedExtension
+
+@synthesize descriptor;
+@synthesize type;
+
+- (void) dealloc {
+    self.descriptor = nil;
+    self.type = nil;
+    
+    [super dealloc];
+}
+
+
+- (FieldDescriptor*) getDescriptor {
+    return descriptor;
+}
+
+#if 0
+public static final class GeneratedExtension<
+ContainingType extends Message, Type> {
+    // TODO(kenton):  Find ways to avoid using Java reflection within this
+    //   class.  Also try to avoid suppressing unchecked warnings.
+    
+    private GeneratedExtension(FieldDescriptor descriptor, Class type) {
+        if (!descriptor.isExtension()) {
+            throw new IllegalArgumentException(
+                                               "GeneratedExtension given a regular (non-extension) field.");
+        }
+        
+        this.descriptor = descriptor;
+        this.type = type;
+        
+        switch (descriptor.getJavaType()) {
+            case MESSAGE:
+                enumValueOf = null;
+                enumGetValueDescriptor = null;
+                messageDefaultInstance =
+                (Message)invokeOrDie(getMethodOrDie(type, "getDefaultInstance"),
+                                     null);
+                break;
+            case ENUM:
+                enumValueOf = getMethodOrDie(type, "valueOf",
+                                             EnumValueDescriptor.class);
+                enumGetValueDescriptor = getMethodOrDie(type, "getValueDescriptor");
+                messageDefaultInstance = null;
+                break;
+            default:
+                enumValueOf = null;
+                enumGetValueDescriptor = null;
+                messageDefaultInstance = null;
+                break;
+        }
+    }
+    
+    
+    
+    /**
+     * If the extension is an embedded message or group, returns the default
+     * instance of the message.
+     */
+    @SuppressWarnings("unchecked")
+    public Message getMessageDefaultInstance() {
+        return messageDefaultInstance;
+    }
+    
+    /**
+     * Convert from the type used by the reflection accessors to the type used
+     * by native accessors.  E.g., for enums, the reflection accessors use
+     * EnumValueDescriptors but the native accessors use the generated enum
+     * type.
+     */
+    @SuppressWarnings("unchecked")
+    private Object fromReflectionType(Object value) {
+        if (descriptor.isRepeated()) {
+            if (descriptor.getJavaType() == FieldDescriptor.JavaType.MESSAGE ||
+                descriptor.getJavaType() == FieldDescriptor.JavaType.ENUM) {
+                // Must convert the whole list.
+                List result = new ArrayList();
+                for (Object element : (List)value) {
+                    result.add(singularFromReflectionType(element));
+                }
+                return result;
+            } else {
+                return value;
+            }
+        } else {
+            return singularFromReflectionType(value);
+        }
+    }
+    
+    /**
+     * Like {@link #fromReflectionType(Object)}, but if the type is a repeated
+     * type, this converts a single element.
+     */
+    private Object singularFromReflectionType(Object value) {
+        switch (descriptor.getJavaType()) {
+            case MESSAGE:
+                if (type.isInstance(value)) {
+                    return value;
+                } else {
+                    // It seems the copy of the embedded message stored inside the
+                    // extended message is not of the exact type the user was
+                    // expecting.  This can happen if a user defines a
+                    // GeneratedExtension manually and gives it a different type.
+                    // This should not happen in normal use.  But, to be nice, we'll
+                    // copy the message to whatever type the caller was expecting.
+                    return messageDefaultInstance.newBuilderForType()
+                    .mergeFrom((Message)value).build();
+                }
+            case ENUM:
+                return invokeOrDie(enumValueOf, null, (EnumValueDescriptor)value);
+            default:
+                return value;
+        }
+    }
+    
+    /**
+     * Convert from the type used by the native accessors to the type used
+     * by reflection accessors.  E.g., for enums, the reflection accessors use
+     * EnumValueDescriptors but the native accessors use the generated enum
+     * type.
+     */
+    @SuppressWarnings("unchecked")
+    private Object toReflectionType(Object value) {
+        if (descriptor.isRepeated()) {
+            if (descriptor.getJavaType() == FieldDescriptor.JavaType.ENUM) {
+                // Must convert the whole list.
+                List result = new ArrayList();
+                for (Object element : (List)value) {
+                    result.add(singularToReflectionType(element));
+                }
+                return result;
+            } else {
+                return value;
+            }
+        } else {
+            return singularToReflectionType(value);
+        }
+    }
+    
+    /**
+     * Like {@link #toReflectionType(Object)}, but if the type is a repeated
+     * type, this converts a single element.
+     */
+    private Object singularToReflectionType(Object value) {
+        switch (descriptor.getJavaType()) {
+            case ENUM:
+                return invokeOrDie(enumGetValueDescriptor, value);
+            default:
+                return value;
+        }
+    }
+}
+#endif
+
+@end
