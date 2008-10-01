2f5e0b15ee67a125ecc1272dbd29ae6b637d1d4f
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-10-01 21:13:12 +0000

    moving files around

--- ProtocolBuffers/Classes/ExtensionInfo.m
+++ ProtocolBuffers/Classes/ExtensionInfo.m
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "ExtensionRegistry_ExtensionInfo.h"
+#import "ExtensionInfo.h"
 
 
 @implementation PBExtensionRegistry_ExtensionInfo
