893dc95f5663cfbc7f1ffebd87d5fc8a2aa3d8f9
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-10-01 19:41:08 +0000

    renaming files

---
+++ ProtocolBuffers/Classes/GeneratedExtension.h
@@ -0,0 +1,33 @@
+// Protocol Buffers - Google's data interchange format
+// Copyright 2008 Google Inc.
+// http://code.google.com/p/protobuf/
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+@interface GeneratedMessage_GeneratedExtension : NSObject {
+@private
+    FieldDescriptor* descriptor;
+    Class type;
+    /*
+     private final Method enumValueOf;
+     private final Method enumGetValueDescriptor;
+     private final Message messageDefaultInstance;
+     */
+}
+
+@property (retain) FieldDescriptor* descriptor;
+@property (retain) Class type;
+
+- (FieldDescriptor*) getDescriptor;
+
+@end
