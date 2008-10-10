34c6ff01bc02d3e6149d3a51e89e1fedfc073974
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-10-10 13:38:14 +0000

    adding tests

---
+++ ProtocolBuffers/Tests/DescriptorTests.h
@@ -0,0 +1,16 @@
+//
+//  DescriptorTests.h
+//  ProtocolBuffers
+//
+//  Created by Cyrus Najmabadi on 10/10/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface DescriptorTests : SenTestCase {
+
+}
+
+@end
