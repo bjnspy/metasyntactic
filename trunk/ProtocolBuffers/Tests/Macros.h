abeb88f684625009405cb9b26fe39aeae7d544e0
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-10-10 18:27:14 +0000

    adding tests

--- trunk/ProtocolBuffers/Tests/Macros.h
+++ trunk/ProtocolBuffers/Tests/Macros.h
@@ -11,3 +11,47 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+#define AssertTrue(expr, description, ...) \
+do { \
+    BOOL _evaluatedExpression = (expr); \
+    if (!_evaluatedExpression) { \
+        NSString *_expression = [NSString stringWithUTF8String: #expr]; \
+        @throw [NSException failureInCondition:_expression \
+                                        isTrue:NO \
+                                        inFile:[NSString stringWithUTF8String:__FILE__] \
+                                        atLine:__LINE__ \
+                               withDescription:STComposeString(description, ##__VA_ARGS__)]; \
+    } \
+} while (0)
+
+
+#define AssertEqualObjects(a1, a2, description, ...) \
+do { \
+    id a1value = (a1); \
+    id a2value = (a2); \
+    if (a1value == a2value) continue; \
+    if ( (@encode(__typeof__(a1value)) == @encode(id)) && \
+         (@encode(__typeof__(a2value)) == @encode(id)) && \
+         [(id)a1value isEqual:(id)a2value] ) continue; \
+    @throw [NSException failureInEqualityBetweenObject:a1value \
+                                             andObject:a2value \
+                                                inFile:[NSString stringWithUTF8String:__FILE__] \
+                                                atLine:__LINE__ \
+                                       withDescription:STComposeString(description, ##__VA_ARGS__)]; \
+} while(0)
+
+
+#define AssertFalse(expr, description, ...) \
+do { \
+    BOOL _evaluatedExpression = (expr); \
+    if (_evaluatedExpression) { \
+        NSString *_expression = [NSString stringWithUTF8String: #expr];\
+        @throw [NSException failureInCondition:_expression \
+                                        isTrue:YES \
+                                        inFile:[NSString stringWithUTF8String:__FILE__] \
+                                        atLine:__LINE__ \
+                               withDescription:STComposeString(description, ##__VA_ARGS__)]; \
+    } \
+} while (0)
+
