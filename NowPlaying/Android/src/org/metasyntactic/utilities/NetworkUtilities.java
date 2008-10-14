8f906d3c99914a5af72d3b859e90f118549b5a2f
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-10-14 01:42:23 +0000

    fleshing out the network helpers

--- NowPlaying/Android/src/org/metasyntactic/utilities/NetworkUtilities.java
+++ NowPlaying/Android/src/org/metasyntactic/utilities/NetworkUtilities.java
@@ -3,10 +3,13 @@ package org.metasyntactic.utilities;
 import org.metasyntactic.threading.PriorityMutex;
 import org.w3c.dom.Element;
 
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
 
 /** @author cyrusn@google.com (Cyrus Najmabadi) */
 public class NetworkUtilities {
@@ -15,6 +18,46 @@ public class NetworkUtilities {
 
   private NetworkUtilities() {
 
+  }
+
+
+  public static String downloadString(String url, boolean important) {
+    try {
+      return downloadString(new URL(url), important);
+    } catch (MalformedURLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+
+  private final static String[] charsets = new String[]{
+      "UTF-8",
+      "ISO-8859-1",
+  };
+
+
+  public static String downloadString(URL url, boolean important) {
+    byte[] bytes = download(url, important);
+
+    for (String charset : charsets) {
+      String result = decode(bytes, charset);
+      if (result != null) {
+        return result;
+      }
+    }
+
+    return null;
+  }
+
+
+  private static String decode(byte[] bytes, String charset) {
+    try {
+      Charset utfCharset = Charset.forName(charset);
+      CharsetDecoder decoder = utfCharset.newDecoder();
+      return decoder.decode(ByteBuffer.wrap(bytes)).toString();
+    } catch (CharacterCodingException e) {
+      return null;
+    }
   }
 
 
@@ -28,22 +71,39 @@ public class NetworkUtilities {
 
 
   private static Element downloadXml(URL url, boolean important) {
+    byte[] data = download(url, important);
+    return XmlUtilities.parseInputStream(new ByteArrayInputStream(data));
+  }
+
+
+  public static byte[] download(URL url, boolean important) {
     try {
       mutex.lock(important);
-      return downloadXmlWorker(url);
+      return downloadWorker(url);
     } finally {
       mutex.unlock(important);
     }
   }
 
 
-  private static Element downloadXmlWorker(URL url) {
+  private static byte[] downloadWorker(URL url) {
     try {
-      return XmlUtilities.parseInputStream(url.openStream());
-    } catch (ParserConfigurationException e) {
-      throw new RuntimeException(e);
+      ByteArrayOutputStream out = new ByteArrayOutputStream();
+      BufferedOutputStream bufferedOut = new BufferedOutputStream(out);
+      BufferedInputStream in = new BufferedInputStream(url.openStream());
+
+      byte[] bytes = new byte[1 << 16];
+      int length;
+      while ((length = in.read(bytes)) > 0) {
+        bufferedOut.write(bytes, 0, length);
+      }
+
+      bufferedOut.flush();
+      out.flush();
+
+      return out.toByteArray();
     } catch (IOException e) {
-      ExceptionUtilities.log(NetworkUtilities.class, "downloadXml", e);
+      ExceptionUtilities.log(NetworkUtilities.class, "downloadWorker", e);
       return null;
     }
   }
