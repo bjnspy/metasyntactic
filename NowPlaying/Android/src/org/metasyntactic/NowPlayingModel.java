c920215f690b581c8d31f9d92ecd335419ced04a
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-10-14 11:00:37 +0000

    adding copyright headers

--- NowPlaying/Android/src/org/metasyntactic/NowPlayingModel.java
+++ NowPlaying/Android/src/org/metasyntactic/NowPlayingModel.java
@@ -1,3 +1,17 @@
+//Copyright 2008 Cyrus Najmabadi
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
 package org.metasyntactic;
 
 import android.content.Context;
@@ -7,9 +21,9 @@ import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
 import org.metasyntactic.caches.UserLocationCache;
+import org.metasyntactic.caches.TrailerCache;
 import org.metasyntactic.caches.scores.ScoreType;
 import org.metasyntactic.caches.scores.ScoreCache;
-import org.metasyntactic.caches.trailers.TrailerCache;
 import org.metasyntactic.data.FavoriteTheater;
 import org.metasyntactic.data.Movie;
 import org.metasyntactic.data.Score;
