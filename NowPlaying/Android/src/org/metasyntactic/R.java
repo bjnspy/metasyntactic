83c98c3160db8aee133aac7c3895faadb9e9d7e5
mjoshi <mjoshi@google.com>
2008-10-23 09:45:34 +0000

    First iteration of movies UI

--- NowPlaying/Android/src/org/metasyntactic/R.java
+++ NowPlaying/Android/src/org/metasyntactic/R.java
@@ -14,14 +14,17 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int tab_host=0x7f050000;
+        public static final int tab_host=0x7f050002;
+        public static final int text1=0x7f050000;
+        public static final int text2=0x7f050001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int listitem=0x7f030000;
         public static final int tabs=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040002;
+        public static final int rated=0x7f040000;
+        public static final int unrated=0x7f040001;
     }
 }
