3cac2fff2157a4120e3b5d6bbc2829374adb5be8
mjoshi <mjoshi@google.com>
2008-10-27 21:13:47 +0000

    added theaters

--- NowPlaying/Android/src/org/metasyntactic/AllTheatersActivity.java
+++ NowPlaying/Android/src/org/metasyntactic/AllTheatersActivity.java
@@ -17,6 +17,7 @@ import android.widget.Toast;
 import org.metasyntactic.caches.scores.ScoreType;
 import org.metasyntactic.data.Movie;
 import org.metasyntactic.data.Score;
+import org.metasyntactic.data.Theater;
 import org.metasyntactic.utilities.MovieViewUtilities;
 import org.metasyntactic.views.NowPlayingPreferenceDialog;
 
@@ -24,10 +25,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 /** @author mjoshi@google.com (Megha Joshi) */
-public class AllMoviesActivity extends ListActivity {
+public class AllTheatersActivity extends ListActivity {
     private NowPlayingActivity activity;
-    private List<Movie> movies = new ArrayList<Movie>();
-    private static MoviesAdapter mAdapter;
+    private List<Theater> theaters = new ArrayList<Theater>();
+    private static TheatersAdapter mAdapter;
     private static Context mContext;
     
     public static final int MENU_SORT = 1;
@@ -40,22 +41,23 @@ public class AllMoviesActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         activity =  (NowPlayingActivity)getParent();
         mContext = this;
-
+        
+        theaters = activity.getController().getTheaters();
         // Set up Movies adapter
-        mAdapter = new MoviesAdapter(this);
+        mAdapter = new TheatersAdapter(this);
         setListAdapter(mAdapter);
     }
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
      
-        menu.add(0, MENU_SORT, 0, R.string.menu_movie_sort)
-                .setIcon(android.R.drawable.star_on);
+        menu.add(0, MENU_SORT, 0, R.string.menu_theater_sort)
+            .setIcon(android.R.drawable.star_on);
                 
-                menu.add(0, MENU_SETTINGS, 0, R.string.menu_settings)
-                .setIcon(android.R.drawable.ic_menu_preferences)
-                .setIntent(new Intent(this, SettingsActivity.class))
-                .setAlphabeticShortcut('s');
+        menu.add(0, MENU_SETTINGS, 0, R.string.menu_settings)
+            .setIcon(android.R.drawable.ic_menu_preferences)
+            .setIntent(new Intent(this, SettingsActivity.class))
+            .setAlphabeticShortcut('s');
        
         return super.onCreateOptionsMenu(menu);
     }
@@ -70,22 +72,22 @@ public class AllMoviesActivity extends ListActivity {
         if (item.getItemId() == MENU_SORT) {
             NowPlayingPreferenceDialog builder = new NowPlayingPreferenceDialog(this.activity)
                 
-                .setTitle(R.string.movies_select_sort_title)
-                .setKey(NowPlayingPreferenceDialog.Preference_keys.MOVIES_SORT)
-                .setEntries(R.array.entries_movies_sort_preference)
+                .setTitle(R.string.theaters_select_sort_title)
+                .setKey(NowPlayingPreferenceDialog.Preference_keys.THEATERS_SORT)
+                .setEntries(R.array.entries_theaters_sort_preference)
                 .show();              
           
             return true;
         }
         return false;
     }
-    class MoviesAdapter extends BaseAdapter {
+    class TheatersAdapter extends BaseAdapter {
         private Context mContext;
 
         private LayoutInflater mInflater;
 
 
-        public MoviesAdapter(Context context) {
+        public TheatersAdapter(Context context) {
             mContext = context;
             // Cache the LayoutInflate to avoid asking for a new one each time.
             mInflater = LayoutInflater.from(context);
@@ -105,17 +107,14 @@ public class AllMoviesActivity extends ListActivity {
         public View getView(int position, View convertView, ViewGroup viewGroup) {
             MovieViewHolder holder;
             if (convertView == null) {
-                convertView = mInflater.inflate(R.layout.movieview, null);
+                convertView = mInflater.inflate(R.layout.theaterview, null);
                 // Creates a MovieViewHolder and store references to the
                 // children
                 // views we want to bind data to.
                 holder = new MovieViewHolder();
-                holder.score = (TextView) convertView.findViewById(R.id.score);
                 holder.title = (TextView) convertView.findViewById(R.id.title);
-                holder.rating =
-                    (TextView) convertView.findViewById(R.id.rating);
-                holder.length =
-                    (TextView) convertView.findViewById(R.id.length);
+                holder.address = (TextView) convertView.findViewById(R.id.address);
+                
                 convertView.setTag(holder);
             } else {
                 // Get the MovieViewHolder back to get fast access to the child
@@ -124,50 +123,34 @@ public class AllMoviesActivity extends ListActivity {
             }
             // Bind the data efficiently with the holder.
             Resources res = mContext.getResources();
-            Movie movie = movies.get(position);
-            holder.title.setText(movie.getDisplayTitle());
-            holder.rating.setText(MovieViewUtilities.formatRatings(movie
-                .getRating(), res));
-            holder.length.setText(MovieViewUtilities.formatLength(movie
-                .getLength(), res));
-
-            // Get and set scores text and background image
-            Score score = ((NowPlayingActivity)activity).getController().getScore(movie);
-            int scoreValue = -1;
-            if (score != null) {
-                scoreValue = Integer.parseInt(score.getValue());
-             
-            } else
-            {
-              
-            }
-            ScoreType scoreType = ((NowPlayingActivity)activity).getController().getScoreType();
-
-            holder.score.setBackgroundDrawable(MovieViewUtilities
-                .formatScoreDrawable(scoreValue, scoreType, res));
-            holder.score.setText(String.valueOf(scoreValue));
+            Theater theater = theaters.get(position);
+            holder.title.setText(theater.getName());
+            holder.address.setText(theater.getAddress());
+           
             return convertView;
         }
 
         class MovieViewHolder {
-            TextView score;
+            TextView address;
+
             TextView title;
-            TextView rating;
-            TextView length;
         }
 
+
         public int getCount() {
-            return movies.size();
+            return theaters.size();
         }
-        
-        public void refreshMovies(List<Movie> new_movies) {
-            movies = new_movies;
+
+
+
+        public void refreshTheaters(List<Theater> new_theaters) {
+            theaters = new_theaters;
             notifyDataSetChanged();
         }
     }
 
-    public static void refresh(List<Movie> movies) {
-        
-        mAdapter.refreshMovies(movies);
+
+    public static void refresh(List<Theater> theaters) {
+        mAdapter.refreshTheaters(theaters);
     }
 }
--- NowPlaying/Android/src/org/metasyntactic/R.java
+++ NowPlaying/Android/src/org/metasyntactic/R.java
@@ -12,6 +12,9 @@ public final class R {
         /**  Used in movies sort preferences 
          */
         public static final int entries_movies_sort_preference=0x7f040000;
+        /**  Used in theaters sort preferences 
+         */
+        public static final int entries_theaters_sort_preference=0x7f040001;
     }
     public static final class attr {
     }
@@ -31,6 +34,7 @@ public final class R {
         public static final int upcoming=0x7f02000c;
     }
     public static final class id {
+        public static final int address=0x7f060004;
         public static final int length=0x7f060003;
         public static final int rating=0x7f060002;
         public static final int score=0x7f060000;
@@ -39,22 +43,25 @@ public final class R {
     public static final class layout {
         public static final int movieview=0x7f030000;
         public static final int tabs=0x7f030001;
+        public static final int theaterview=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f050009;
-        public static final int hours=0x7f05000c;
-        public static final int menu_settings=0x7f050005;
-        public static final int menu_sort=0x7f050004;
-        public static final int minutes=0x7f05000e;
+        public static final int app_name=0x7f05000b;
+        public static final int hours=0x7f05000e;
+        public static final int menu_movie_sort=0x7f050004;
+        public static final int menu_settings=0x7f050006;
+        public static final int menu_theater_sort=0x7f050005;
+        public static final int minutes=0x7f050010;
         public static final int moviesIconLabel=0x7f050000;
-        public static final int movies_select_sort_title=0x7f050006;
-        public static final int onehour=0x7f05000b;
-        public static final int oneminute=0x7f05000d;
-        public static final int rated=0x7f050007;
-        public static final int ratingslengthstr_separator=0x7f05000a;
+        public static final int movies_select_sort_title=0x7f050007;
+        public static final int onehour=0x7f05000d;
+        public static final int oneminute=0x7f05000f;
+        public static final int rated=0x7f050009;
+        public static final int ratingslengthstr_separator=0x7f05000c;
         public static final int settingsIconLabel=0x7f050003;
         public static final int theatersIconLabel=0x7f050001;
-        public static final int unrated=0x7f050008;
+        public static final int theaters_select_sort_title=0x7f050008;
+        public static final int unrated=0x7f05000a;
         public static final int upcomingIconLabel=0x7f050002;
     }
 }
