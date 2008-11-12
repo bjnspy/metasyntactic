ed64f75f444d14e02971b2f9599333609842b599
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-11-12 05:58:07 +0000

    making code build

--- NowPlaying/Android/src/org/metasyntactic/AllTheatersActivity.java
+++ NowPlaying/Android/src/org/metasyntactic/AllTheatersActivity.java
@@ -1,3 +1,4 @@
+
 package org.metasyntactic;
 
 import android.app.ListActivity;
@@ -27,262 +28,248 @@ import java.util.List;
 /** @author mjoshi@google.com (Megha Joshi) */
 
 public class AllTheatersActivity extends ListActivity implements INowPlaying {
-  public static final int MENU_SORT = 1;
-  public static final int MENU_SETTINGS = 2;
+	public static final int MENU_SORT = 1;
+	public static final int MENU_SETTINGS = 2;
 
+	private static NowPlayingControllerWrapper controller;
+	private static Pulser pulser;
+	private static TheatersAdapter adapter;
+	private static Context mContext;
 
-  private static NowPlayingControllerWrapper controller;
-  private static Pulser pulser;
-  private static TheatersAdapter adapter;
-  private static Context mContext;
+	private NowPlayingActivity activity;
 
-  private NowPlayingActivity activity;
+	private List<Theater> theaters = new ArrayList<Theater>();
 
-  private List<Theater> theaters = new ArrayList<Theater>();
+	// variable which controls the theater update thread
+	private ConditionVariable condition;
+	private ConditionVariable condition2;
+	private static Location userLocation;
 
-  // variable which controls the theater update thread
-  private ConditionVariable condition;
-  private ConditionVariable condition2;
-  private static Location userLocation;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    // TODO Auto-generated method stub
-    super.onCreate(savedInstanceState);
+		activity = (NowPlayingActivity) getParent();
+		mContext = this;
+		controller = activity.getController();
+		theaters = controller.getTheaters();
+		String userPostalCode = controller.getUserLocation();
+		Address address = null;
+		try {
+			address = new Geocoder(mContext).getFromLocationName(userPostalCode, 1).get(0);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
+		userLocation = new Location(address.getLatitude(), address.getLongitude(), null, null, null, null, null);
 
-        activity = (NowPlayingActivity) getParent();
-        mContext = this;
-        controller = activity.getController();
-        theaters = controller.getTheaters();
-        String userPostalCode = controller.getUserLocation();
-        Address address = null;
-        try {
-            address =
-                new Geocoder(mContext).getFromLocationName(
-                    userPostalCode, 1).get(0);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+		Collections.sort(theaters, THEATER_ORDER[controller.getAllTheatersSelectedSortIndex()]);
 
-        userLocation = new Location(address.getLatitude(), address.getLongitude(),
-            null, null, null, null, null);
-   
-        Collections.sort(theaters, THEATER_ORDER[controller
-            .getAllTheatersSelectedSortIndex()]);
-        
-             
-        // Set up Movies adapter
-        mAdapter = new TheatersAdapter(this);
-        setListAdapter(mAdapter);
+		// Set up Movies adapter
+		adapter = new TheatersAdapter(this);
+		setListAdapter(adapter);
+	}
 
-    }
+	@Override
+	protected void onPause() {
+		// stop the thread updating theaters
+		condition.open();
+		super.onPause();
+	}
 
+	@Override
+	protected void onResume() {
+		// Condition variables controlling the runnables updating
+		// the theaters view.
+		condition = new ConditionVariable(false);
+		if (theaters.size() > 0) {
+			condition2 = new ConditionVariable(true);
+		} else {
+			condition2 = new ConditionVariable(false);
+		}
+		// update the theaters UI every 15 seconds until all the theaters
+		// are loaded.
+		Runnable runnable1 = new Runnable() {
+			public void run() {
+				while (true) {
+					if (condition2.block(5 * 1000)) {
+						break;
+					}
+					refresh();
+				}
+			}
+		};
+		Thread thread2 = new Thread(null, runnable1);
+		thread2.start();
 
-  @Override
-  protected void onPause() {
-    // stop the thread updating theaters
-    condition.open();
-    super.onPause();
-  }
+		// update the theaters every 5 minutes after all theaters are loaded.
+		Runnable runnable = new Runnable() {
+			public void run() {
+				while (true) {
+					if (condition.block(5 * 60 * 1000)) {
+						break;
+					}
 
-  @Override
-  protected void onResume() {
-    // Condition variables controlling the runnables updating
-    // the theaters view.
-    condition = new ConditionVariable(false);
-    if (theaters.size() > 0) {
-      condition2 = new ConditionVariable(true);
-    } else {
-      condition2 = new ConditionVariable(false);
-    }
-    // update the theaters UI every 15 seconds until all the theaters
-    // are loaded.
-    Runnable runnable1 = new Runnable() {
-      public void run() {
-        while (true) {
-          if (condition2.block(5 * 1000)) {
-            break;
-          }
-          refresh();
-        }
-      }
-    };
-    Thread thread2 = new Thread(null, runnable1);
-    thread2.start();
+					refresh();
+				}
+			}
+		};
+		Thread thread = new Thread(null, runnable);
+		thread.start();
 
-    // update the theaters every 5 minutes after all theaters are loaded.
-    Runnable runnable = new Runnable() {
-      public void run() {
-        while (true) {
-          if (condition.block(5 * 60 * 1000)) {
-            break;
-          }
+		super.onResume();
+	}
 
-          refresh();
-        }
-      }
-    };
-    Thread thread = new Thread(null, runnable);
-    thread.start();
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 
-    super.onResume();
-  }
+		menu.add(0, MENU_SORT, 0, R.string.menu_theater_sort).setIcon(android.R.drawable.star_on);
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(0, MENU_SETTINGS, 0, R.string.settings).setIcon(android.R.drawable.ic_menu_preferences).setIntent(
+				new Intent(this, SettingsActivity.class)).setAlphabeticShortcut('s');
 
-    menu.add(0, MENU_SORT, 0, R.string.menu_theater_sort).setIcon(android.R.drawable.star_on);
+		return super.onCreateOptionsMenu(menu);
+	}
 
-    menu.add(0, MENU_SETTINGS, 0, R.string.settings).setIcon(android.R.drawable.ic_menu_preferences).setIntent(
-        new Intent(this, SettingsActivity.class))
-        .setAlphabeticShortcut('s');
+	public INowPlaying getNowPlayingActivityContext() {
+		return activity;
+	}
 
-    return super.onCreateOptionsMenu(menu);
-  }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == MENU_SORT) {
+			NowPlayingPreferenceDialog builder = new NowPlayingPreferenceDialog(this)
 
-  public INowPlaying getNowPlayingActivityContext() {
-    return activity;
-  }
+			.setTitle(R.string.theaters_select_sort_title).setKey(NowPlayingPreferenceDialog.Preference_keys.THEATERS_SORT)
+					.setEntries(R.array.entries_theaters_sort_preference).show();
 
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    if (item.getItemId() == MENU_SORT) {
-      NowPlayingPreferenceDialog builder = new NowPlayingPreferenceDialog(this)
+			return true;
+		}
+		return false;
+	}
 
-          .setTitle(R.string.theaters_select_sort_title).setKey(
-          NowPlayingPreferenceDialog.Preference_keys.THEATERS_SORT)
-          .setEntries(R.array.entries_theaters_sort_preference)
-          .show();
+	class TheatersAdapter extends BaseAdapter {
+		private final Context context;
+		private final LayoutInflater inflater;
 
-      return true;
-    }
-    return false;
-  }
+		public TheatersAdapter(Context context) {
+			this.context = context;
+			// Cache the LayoutInflate to avoid asking for a new one each time.
+			inflater = LayoutInflater.from(context);
+		}
 
-  class TheatersAdapter extends BaseAdapter {
-    private final Context context;
-    private final LayoutInflater inflater;
+		public Object getItem(int i) {
+			return i;
+		}
 
-    public TheatersAdapter(Context context) {
-      this.context = context;
-      // Cache the LayoutInflate to avoid asking for a new one each time.
-      inflater = LayoutInflater.from(context);
-    }
+		public View getView(int position, View convertView, ViewGroup viewGroup) {
+			NowPlayingControllerWrapper controller = activity.getController();
 
-    public Object getItem(int i) {
-      return i;
-    }
+			convertView = inflater.inflate(R.layout.theaterview, null);
 
-    public View getView(int position, View convertView, ViewGroup viewGroup) {
-      NowPlayingControllerWrapper controller = activity.getController();
+			// Creates a MovieViewHolder and store references to the
+			// children
+			// views we want to bind data to.
+			MovieViewHolder holder = new MovieViewHolder();
+			holder.divider = (ImageView) convertView.findViewById(R.id.divider1);
+			holder.title = (TextView) convertView.findViewById(R.id.title);
+			holder.address = (TextView) convertView.findViewById(R.id.address);
+			holder.header = (TextView) convertView.findViewById(R.id.header);
 
-      convertView = inflater.inflate(R.layout.theaterview, null);
+			// Bind the data efficiently with the holder.
+			Resources res = context.getResources();
+			Theater theater = theaters.get(position);
+			Address address = null;
+			try {
+				address = new Geocoder(context).getFromLocationName(controller.getUserLocation(), 1).get(0);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			String headerText = MovieViewUtilities.getTheaterHeader(theaters, position, controller
+					.getAllTheatersSelectedSortIndex(), address);
+			if (headerText != null) {
+				holder.header.setVisibility(1);
+				holder.header.setText(headerText);
+			} else {
+				holder.header.setVisibility(-1);
+				holder.header.setHeight(0);
+				holder.divider.setVisibility(-1);
+				holder.divider.setMaxHeight(0);
+			}
+			holder.title.setText(theater.getName());
+			holder.address.setText(theater.getAddress() + ", " + theater.getLocation().getCity());
+			return convertView;
+		}
 
-      // Creates a MovieViewHolder and store references to the
-      // children
-      // views we want to bind data to.
-      MovieViewHolder holder = new MovieViewHolder();
-      holder.divider = (ImageView) convertView.findViewById(R.id.divider1);
-      holder.title = (TextView) convertView.findViewById(R.id.title);
-      holder.address = (TextView) convertView.findViewById(R.id.address);
-      holder.header = (TextView) convertView.findViewById(R.id.header);
+		public int getCount() {
+			return theaters.size();
+		}
 
-      // Bind the data efficiently with the holder.
-      Resources res = context.getResources();
-      Theater theater = theaters.get(position);
-      Address address = null;
-      try {
-        address = new Geocoder(context).getFromLocationName(controller.getUserLocation(), 1).get(0);
-      } catch (IOException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      }
-      String headerText = MovieViewUtilities.getTheaterHeader(theaters, position, controller
-          .getAllTheatersSelectedSortIndex(), address);
-      if (headerText != null) {
-        holder.header.setVisibility(1);
-        holder.header.setText(headerText);
-      } else {
-        holder.header.setVisibility(-1);
-        holder.header.setHeight(0);
-        holder.divider.setVisibility(-1);
-        holder.divider.setMaxHeight(0);
-      }
-      holder.title.setText(theater.getName());
-      holder.address.setText(theater.getAddress() + ", " + theater.getLocation().getCity());
-      return convertView;
-    }
+		class MovieViewHolder {
+			TextView header;
+			TextView address;
+			TextView title;
+			ImageView divider;
+		}
 
-    public int getCount() {
-      return theaters.size();
-    }
+		public void refreshTheaters(List<Theater> new_theaters) {
+			theaters = new_theaters;
+			notifyDataSetChanged();
+		}
 
-    class MovieViewHolder {
-      TextView header;
-      TextView address;
-      TextView title;
-      ImageView divider;
-    }
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return position;
+		}
+	}
 
-    public void refreshTheaters(List<Theater> new_theaters) {
-      theaters = new_theaters;
-      notifyDataSetChanged();
-    }
+	public void refresh() {
+		if (ThreadingUtilities.isBackgroundThread()) {
+			Runnable runnable = new Runnable() {
+				public void run() {
+					refresh();
+				}
+			};
+			ThreadingUtilities.performOnMainThread(runnable);
+			return;
+		}
+		List<Theater> theaters = controller.getTheaters();
+		if (theaters.size() > 0) {
+			condition2.open();
+		}
+		Collections.sort(theaters, THEATER_ORDER[controller.getAllTheatersSelectedSortIndex()]);
+		adapter.refreshTheaters(theaters);
+	}
 
-    public long getItemId(int position) {
-      // TODO Auto-generated method stub
-      return position;
-    }
-  }
+	// Define comparators for theater listings sort.
+	private static final Comparator<Theater> TITLE_ORDER = new Comparator<Theater>() {
+		public int compare(Theater m1, Theater m2) {
+			return m1.getName().compareTo(m2.getName());
+		}
+	};
 
-  public void refresh() {
-    if (ThreadingUtilities.isBackgroundThread()) {
-      Runnable runnable = new Runnable() {
-        public void run() {
-          refresh();
-        }
-      };
-      ThreadingUtilities.performOnMainThread(runnable);
-      return;
-    }
-    List<Theater> theaters = controller.getTheaters();
-    if (theaters.size() > 0) {
-      condition2.open();
-    }
-    Collections.sort(theaters, THEATER_ORDER[controller
-        .getAllTheatersSelectedSortIndex()]);
-    adapter.refreshTheaters(theaters);
-  }
+	private static final Comparator<Theater> DISTANCE_ORDER = new Comparator<Theater>() {
+		public int compare(Theater m1, Theater m2) {
 
-  // Define comparators for theater listings sort.
-  private static final Comparator<Theater> TITLE_ORDER = new Comparator<Theater>() {
-    public int compare(Theater m1, Theater m2) {
-      return m1.getName().compareTo(m2.getName());
-    }
-  };
+			Double dist_m1 = userLocation.distanceTo(m1.getLocation());
+			Double dist_m2 = userLocation.distanceTo(m2.getLocation());
+			return dist_m1.compareTo(dist_m2);
+		}
+	};
+	// The order of items in this array should match the
+	// entries_theater_sort_preference array in res/values/arrays.xml
+	private static final Comparator[] THEATER_ORDER = { TITLE_ORDER, DISTANCE_ORDER, };
 
-  private static final Comparator<Theater> DISTANCE_ORDER = new Comparator<Theater>() {
-    public int compare(Theater m1, Theater m2) {
+	public Context getContext() {
+		// TODO Auto-generated method stub
+		return this;
+	}
 
-
-      Double dist_m1 = userLocation.distanceTo(m1.getLocation());
-      Double dist_m2 = userLocation.distanceTo(m2.getLocation());
-      return dist_m1.compareTo(dist_m2);
-    }
-  };
-  // The order of items in this array should match the
-  // entries_theater_sort_preference array in res/values/arrays.xml
-  private static final Comparator[] THEATER_ORDER = {TITLE_ORDER, DISTANCE_ORDER,};
-
-  public Context getContext() {
-    // TODO Auto-generated method stub
-    return this;
-  }
-
-  public NowPlayingControllerWrapper getController() {
-    // TODO Auto-generated method stub
-    return controller;
-  }
+	public NowPlayingControllerWrapper getController() {
+		// TODO Auto-generated method stub
+		return controller;
+	}
 }
