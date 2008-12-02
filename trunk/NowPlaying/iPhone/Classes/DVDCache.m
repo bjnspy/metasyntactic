27f0beba3e5fb07ef59d28b56edd246534330a68
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-12-02 22:31:49 +0000

    removing imports

--- trunk/NowPlaying/iPhone/Classes/DVDCache.m
+++ trunk/NowPlaying/iPhone/Classes/DVDCache.m
@@ -15,8 +15,6 @@
 #import "DVDCache.h"
 
 #import "Application.h"
-#import "LinkedSet.h"
-#import "Movie.h"
 #import "NetworkUtilities.h"
 #import "NowPlayingAppDelegate.h"
 #import "NowPlayingModel.h"
