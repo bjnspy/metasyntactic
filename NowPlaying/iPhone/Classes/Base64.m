ecc65ac1efd5c2e1f62b25135d3977b39105905c
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-12-14 03:39:47 +0000

    simplifying code

--- NowPlaying/iPhone/Classes/Base64.m
+++ NowPlaying/iPhone/Classes/Base64.m
@@ -17,25 +17,19 @@
     NSMutableData* out = [NSMutableData dataWithLength:((rawBytes.length + 2) / 3) * 4];
   
     for (NSInteger i = 0, index = 0; i < rawBytes.length; i += 3, index += 4) {
-        BOOL four_bytes = NO;
-        BOOL three_bytes = NO;
-        
-        NSInteger val = (0xFF & ((const uint8_t*)rawBytes.bytes)[i]);
-        val <<= 8;
-        if ((i + 1) < rawBytes.length) {
-            val |= (0xFF & ((const uint8_t*)rawBytes.bytes)[i + 1]);
-            three_bytes = YES;
-        }
-        val <<= 8;
-        if ((i + 2) < rawBytes.length) {
-            val |= (0xFF & ((const uint8_t*)rawBytes.bytes)[i + 2]);
-            four_bytes = YES;
+        NSInteger val = 0;
+        for (NSInteger j = i; j < (i + 3); j++) {
+            val <<= 8;
+            
+            if (j < rawBytes.length) {
+                val |= (0xFF & ((const uint8_t*)rawBytes.bytes)[j]);
+            }
         }
         
-        ((uint8_t*)out.mutableBytes)[index + 3] = four_bytes  ? table[(val >> 0) & 0x3F] : '=';
-        ((uint8_t*)out.mutableBytes)[index + 2] = three_bytes ? table[(val >> 6) & 0x3F] : '=';
-        ((uint8_t*)out.mutableBytes)[index + 1] =               table[(val >> 12) & 0x3F];
-        ((uint8_t*)out.mutableBytes)[index + 0] =               table[(val >> 18) & 0x3F];
+        ((uint8_t*)out.mutableBytes)[index + 3] = (i + 2) < rawBytes.length ? table[(val >> 0) & 0x3F] : '=';
+        ((uint8_t*)out.mutableBytes)[index + 2] = (i + 1) < rawBytes.length ? table[(val >> 6) & 0x3F] : '=';
+        ((uint8_t*)out.mutableBytes)[index + 1] =                             table[(val >> 12) & 0x3F];
+        ((uint8_t*)out.mutableBytes)[index + 0] =                             table[(val >> 18) & 0x3F];
     }
 
     return [[[NSString alloc] initWithData:out encoding:NSASCIIStringEncoding] autorelease]; 
