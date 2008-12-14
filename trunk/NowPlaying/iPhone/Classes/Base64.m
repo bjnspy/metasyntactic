40f67c2299866a208a278c83cc0502a93e071cb2
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-12-14 03:54:21 +0000

    simplifying code

--- trunk/NowPlaying/iPhone/Classes/Base64.m
+++ trunk/NowPlaying/iPhone/Classes/Base64.m
@@ -18,20 +18,20 @@
     uint8_t* output = (uint8_t*)data.mutableBytes;
     
     for (NSInteger i = 0; i < length; i += 3) {
-        NSInteger val = 0;
+        NSInteger value = 0;
         for (NSInteger j = i; j < (i + 3); j++) {
-            val <<= 8;
+            value <<= 8;
             
             if (j < length) {
-                val |= (0xFF & input[j]);
+                value |= (0xFF & input[j]);
             }
         }
         
         NSInteger index = 4 * i / 3;
-        output[index + 0] =                    table[(val >> 18) & 0x3F];
-        output[index + 1] =                    table[(val >> 12) & 0x3F];
-        output[index + 2] = (i + 1) < length ? table[(val >> 6)  & 0x3F] : '=';
-        output[index + 3] = (i + 2) < length ? table[(val >> 0)  & 0x3F] : '=';
+        output[index + 0] =                    table[(value >> 18) & 0x3F];
+        output[index + 1] =                    table[(value >> 12) & 0x3F];
+        output[index + 2] = (i + 1) < length ? table[(value >> 6)  & 0x3F] : '=';
+        output[index + 3] = (i + 2) < length ? table[(value >> 0)  & 0x3F] : '=';
     }
 
     return [[[NSString alloc] initWithData:data
