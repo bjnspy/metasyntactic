d5b21761bf29b75253adf3519bc21cc05c090455
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-12-14 03:42:25 +0000

    simplifying code

--- trunk/NowPlaying/iPhone/Classes/Base64.m
+++ trunk/NowPlaying/iPhone/Classes/Base64.m
@@ -14,25 +14,29 @@
 + (NSString*) encode:(NSData*) rawBytes {
     static char table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 
-    NSMutableData* out = [NSMutableData dataWithLength:((rawBytes.length + 2) / 3) * 4];
-  
+    NSMutableData* data = [NSMutableData dataWithLength:((rawBytes.length + 2) / 3) * 4];
+    
+    const uint8_t* in = (const uint8_t*)rawBytes.bytes;
+    uint8_t* out = (uint8_t*)data.mutableBytes;
+    
     for (NSInteger i = 0, index = 0; i < rawBytes.length; i += 3, index += 4) {
         NSInteger val = 0;
         for (NSInteger j = i; j < (i + 3); j++) {
             val <<= 8;
             
             if (j < rawBytes.length) {
-                val |= (0xFF & ((const uint8_t*)rawBytes.bytes)[j]);
+                val |= (0xFF & in[j]);
             }
         }
         
-        ((uint8_t*)out.mutableBytes)[index + 3] = (i + 2) < rawBytes.length ? table[(val >> 0) & 0x3F] : '=';
-        ((uint8_t*)out.mutableBytes)[index + 2] = (i + 1) < rawBytes.length ? table[(val >> 6) & 0x3F] : '=';
-        ((uint8_t*)out.mutableBytes)[index + 1] =                             table[(val >> 12) & 0x3F];
-        ((uint8_t*)out.mutableBytes)[index + 0] =                             table[(val >> 18) & 0x3F];
+        out[index + 3] = (i + 2) < rawBytes.length ? table[(val >> 0) & 0x3F] : '=';
+        out[index + 2] = (i + 1) < rawBytes.length ? table[(val >> 6) & 0x3F] : '=';
+        out[index + 1] =                             table[(val >> 12) & 0x3F];
+        out[index + 0] =                             table[(val >> 18) & 0x3F];
     }
 
-    return [[[NSString alloc] initWithData:out encoding:NSASCIIStringEncoding] autorelease]; 
+    return [[[NSString alloc] initWithData:data
+                                  encoding:NSASCIIStringEncoding] autorelease]; 
 }
 
 @end
