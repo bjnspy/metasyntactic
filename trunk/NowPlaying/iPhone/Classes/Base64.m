6da9075148e5d5ea3419cce707b337613cd0b308
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-12-14 03:49:37 +0000

    simplifying code

--- trunk/NowPlaying/iPhone/Classes/Base64.m
+++ trunk/NowPlaying/iPhone/Classes/Base64.m
@@ -11,32 +11,34 @@
 
 @implementation Base64
 
-+ (NSString*) encode:(NSData*) rawBytes {
++ (NSString*) encode:(const uint8_t*) input length:(NSInteger) length {
     static char table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 
-    NSMutableData* data = [NSMutableData dataWithLength:((rawBytes.length + 2) / 3) * 4];
+    NSMutableData* data = [NSMutableData dataWithLength:((length + 2) / 3) * 4];
+    uint8_t* output = (uint8_t*)data.mutableBytes;
     
-    const uint8_t* in = (const uint8_t*)rawBytes.bytes;
-    uint8_t* out = (uint8_t*)data.mutableBytes;
-    
-    for (NSInteger i = 0, index = 0; i < rawBytes.length; i += 3, index += 4) {
+    for (NSInteger i = 0, index = 0; i < length; i += 3, index += 4) {
         NSInteger val = 0;
         for (NSInteger j = i; j < (i + 3); j++) {
             val <<= 8;
             
-            if (j < rawBytes.length) {
-                val |= (0xFF & in[j]);
+            if (j < length) {
+                val |= (0xFF & input[j]);
             }
         }
         
-        out[index + 3] = (i + 2) < rawBytes.length ? table[(val >> 0) & 0x3F] : '=';
-        out[index + 2] = (i + 1) < rawBytes.length ? table[(val >> 6) & 0x3F] : '=';
-        out[index + 1] =                             table[(val >> 12) & 0x3F];
-        out[index + 0] =                             table[(val >> 18) & 0x3F];
+        output[index + 3] = (i + 2) < length ? table[(val >> 0) & 0x3F] : '=';
+        output[index + 2] = (i + 1) < length ? table[(val >> 6) & 0x3F] : '=';
+        output[index + 1] =                    table[(val >> 12) & 0x3F];
+        output[index + 0] =                    table[(val >> 18) & 0x3F];
     }
 
     return [[[NSString alloc] initWithData:data
                                   encoding:NSASCIIStringEncoding] autorelease]; 
+}
+
++ (NSString*) encode:(NSData*) rawBytes {
+    return [Base64 encode:(const uint8_t*)rawBytes.bytes length:rawBytes.length];
 }
 
 @end
