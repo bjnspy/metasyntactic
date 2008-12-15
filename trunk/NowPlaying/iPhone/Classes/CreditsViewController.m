14df253af4ef26bfd59b72b6a0ded87be16fd161
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-12-15 23:59:42 +0000

    localization

--- trunk/NowPlaying/iPhone/Classes/CreditsViewController.m
+++ trunk/NowPlaying/iPhone/Classes/CreditsViewController.m
@@ -76,6 +76,7 @@ NSComparisonResult compareLanguageCodes(id code1, id code2, void* context) {
         [dictionary setObject:@"J-P. Helisten"      forKey:@"fi"];
         [dictionary setObject:@"Jonathan Grenier"   forKey:@"fr"];
         [dictionary setObject:@"Dani Valevski"      forKey:@"he"];
+        [dictionary setObject:@"Megha Joshi"        forKey:@"hi"];
         [dictionary setObject:@"Horvath István"     forKey:@"hu"];
         [dictionary setObject:@"Santiago Navonne"   forKey:@"it"];
         [dictionary setObject:@"Leo Yamamoto"       forKey:@"ja"];
