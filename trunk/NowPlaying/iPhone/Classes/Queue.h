d6b5cdf13c30298ee4a89b360ab006b22abfe328
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-12-17 05:43:37 +0000

    download details

--- trunk/NowPlaying/iPhone/Classes/Queue.h
+++ trunk/NowPlaying/iPhone/Classes/Queue.h
@@ -12,18 +12,21 @@
     NSString* etag;
     NSArray* movies;
     NSArray* saved;
+    NSDictionary* identifierToDetails;
 }
 
 @property (readonly, copy) NSString* feedKey;
 @property (readonly, copy) NSString* etag;
 @property (readonly, retain) NSArray* movies;
 @property (readonly, retain) NSArray* saved;
+@property (readonly, retain) NSDictionary* identifierToDetails;
 
 + (Queue*) queueWithDictionary:(NSDictionary*) dictionary;
 + (Queue*) queueWithFeedKey:(NSString*) feedKey
                        etag:(NSString*) etag
                      movies:(NSArray*) movies
-                      saved:(NSArray*) saved;
+                      saved:(NSArray*) saved
+        identifierToDetails:(NSDictionary*) identifierToDetails;
 
 - (NSDictionary*) dictionary;
 
