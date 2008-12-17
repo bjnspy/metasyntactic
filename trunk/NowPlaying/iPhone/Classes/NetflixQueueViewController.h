604f4f3e1eb401110ed0d67f12077f51f71e402f
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-12-17 01:33:35 +0000

    moving file

--- trunk/NowPlaying/iPhone/Classes/NetflixQueueViewController.h
+++ trunk/NowPlaying/iPhone/Classes/NetflixQueueViewController.h
@@ -6,7 +6,7 @@
 //  Copyright 2008 __MyCompanyName__. All rights reserved.
 //
 
-#import "NetflixReorderQueueDelegate.h"
+#import "NetflixModifyQueueDelegate.h"
 #import "TappableImageViewDelegate.h"
 
 @interface NetflixQueueViewController : UITableViewController<TappableImageViewDelegate, NetflixModifyQueueDelegate> {
