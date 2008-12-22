39f3a903704f45a4bcc3e96da44b37a7e6edba45
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2008-12-22 05:31:25 +0000

    clean up how we migrate settings

--- trunk/NowPlaying/iPhone/Classes/NetflixSearchViewController.m
+++ trunk/NowPlaying/iPhone/Classes/NetflixSearchViewController.m
@@ -51,6 +51,7 @@
 
 - (id) initWithNavigationController:(AbstractNavigationController*) navigationController_ {
     if (self = [super initWithStyle:UITableViewStylePlain]) {
+        self.navigationController = navigationController_;
         self.searchEngine = [NetflixSearchEngine engineWithModel:self.model
                                                         delegate:self];
         self.activityIndicatorView = [[[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite] autorelease];
