66539ea589091b102233ea6c750493130696b157
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-01-06 02:49:37 +0000

    copyright

--- trunk/YourRights/Classes/GreatestHitsViewController.h
+++ trunk/YourRights/Classes/GreatestHitsViewController.h
@@ -12,9 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-@interface YourRightsNavigationController : UINavigationController {
-@private
-    SectionViewController* viewController;
+@interface GreatestHitsViewController : UITableViewController {
+
 }
 
 @end
