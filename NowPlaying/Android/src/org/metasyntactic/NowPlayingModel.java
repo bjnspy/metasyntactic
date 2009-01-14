eb22342c2b2d2bc73257fd0f0c4bebbe406a2fce
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-01-14 22:49:28 +0000

    fixing perf issue with java protocol buffers

--- NowPlaying/Android/src/org/metasyntactic/NowPlayingModel.java
+++ NowPlaying/Android/src/org/metasyntactic/NowPlayingModel.java
@@ -34,7 +34,7 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class NowPlayingModel {
-  private final static String VERSION = "9";
+  private final static String VERSION = "10";
   private final static String VERSION_KEY = "VERSION";
   private final static String USER_ADDRESS_KEY = "userAddress";
   private final static String SEARCH_DATE_KEY = "searchDate";
