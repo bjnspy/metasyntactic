7db151846c8160534ebc1272aa536b94d9472867
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-01-23 22:31:48 +0000

    adding pocketcards

---
+++ trunk/PocketCards/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PocketCards
+//
+//  Created by Cyrus Najmabadi on 1/23/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
