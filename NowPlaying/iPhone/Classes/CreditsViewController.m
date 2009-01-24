0e630df53b3d5efea1a2944a703bd33f459350ca
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-01-24 08:56:54 +0000

    include pricing details on the info page

--- NowPlaying/iPhone/Classes/CreditsViewController.m
+++ NowPlaying/iPhone/Classes/CreditsViewController.m
@@ -235,11 +235,11 @@ NSComparisonResult compareLanguageCodes(id code1, id code2, void* context) {
         }
     } else if (section == MyOtherApplicationsSection) {
         if (row == 0) {
-            cell.text = @"ComiXology";
+            cell.text = @"ComiXology ($3.99)";
         } else if (row == 1) {
-            cell.text = @"PocketFlix";
+            cell.text = @"PocketFlix ($1.99)";
         } else {
-            cell.text = @"Your Rights";
+            cell.text = @"Your Rights (free)";
         }
     } else if (section == GraphicsBySection) {
         cell.text = NSLocalizedString(@"Website", nil);
