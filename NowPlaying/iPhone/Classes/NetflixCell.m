966e968ddc3caf9d2f0625f8cf472276c1306f34
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-01-27 03:29:18 +0000

    porting features over from PocketFlix

--- NowPlaying/iPhone/Classes/NetflixCell.m
+++ NowPlaying/iPhone/Classes/NetflixCell.m
@@ -16,15 +16,13 @@
 
 #import "Application.h"
 #import "ColorCache.h"
-#import "DVD.h"
-#import "DVDCache.h"
-#import "DVDViewController.h"
 #import "DateUtilities.h"
 #import "GlobalActivityIndicator.h"
 #import "ImageCache.h"
 #import "Movie.h"
 #import "NetflixCache.h"
 #import "Model.h"
+#import "TappableImageView.h"
 
 @interface NetflixCell()
 @property (retain) UILabel* directorTitleLabel;
@@ -37,6 +35,9 @@
 @property (retain) UILabel* genreLabel;
 @property (retain) UILabel* ratedLabel;
 @property (retain) UILabel* netflixLabel;
+@property (retain) UILabel* availabilityLabel;
+@property (retain) UILabel* formatsLabel;
+@property (retain) TappableImageView* tappableArrow;
 @end
 
 
@@ -53,6 +54,10 @@
 @synthesize ratedLabel;
 @synthesize genreLabel;
 @synthesize netflixLabel;
+@synthesize availabilityLabel;
+@synthesize formatsLabel;
+
+@synthesize tappableArrow;
 
 - (void) dealloc {
     self.directorTitleLabel = nil;
@@ -60,40 +65,22 @@
     self.ratedTitleLabel = nil;
     self.genreTitleLabel = nil;
     self.netflixTitleLabel = nil;
-
+    
     self.directorLabel = nil;
     self.castLabel = nil;
     self.ratedLabel = nil;
     self.genreLabel = nil;
     self.netflixLabel = nil;
-
+    
+    self.availabilityLabel = nil;
+    self.formatsLabel = nil;
+    
+    tappableArrow.delegate = nil;
+    self.tappableArrow = nil;
+    
     [super dealloc];
 }
 
-
-- (UILabel*) createTitleLabel:(NSString*) title yPosition:(NSInteger) yPosition {
-    UILabel* label = [[[UILabel alloc] initWithFrame:CGRectZero] autorelease];
-
-    label.font = [UIFont systemFontOfSize:12];
-    label.textColor = [UIColor darkGrayColor];
-    label.text = title;
-    label.textAlignment = UITextAlignmentRight;
-    [label sizeToFit];
-    CGRect frame = label.frame;
-    frame.origin.y = yPosition;
-    label.frame = frame;
-
-    return label;
-}
-
-
-- (UILabel*) createValueLabel:(NSInteger) yPosition {
-    CGFloat height = directorTitleLabel.frame.size.height;
-    UILabel* label = [[[UILabel alloc] initWithFrame:CGRectMake(0, yPosition, 0, height)] autorelease];
-    label.font = [UIFont systemFontOfSize:12];
-    label.textColor = [UIColor darkGrayColor];
-    return label;
-}
 
 
 - (NSArray*) titleLabels {
@@ -112,12 +99,28 @@
             castLabel,
             genreLabel,
             ratedLabel,
-            netflixLabel, nil];
+            netflixLabel,
+            availabilityLabel,
+            formatsLabel,
+            nil];
 }
 
 
 - (NSArray*) allLabels {
     return [self.titleLabels arrayByAddingObjectsFromArray:self.valueLabels];
+}
+
+
+- (void) setupTappableArrow {
+    UIImage* image = [ImageCache upArrow];
+    TappableImageView* view = [[[TappableImageView alloc] initWithImage:image] autorelease];
+    view.contentMode = UIViewContentModeCenter;
+    
+    CGRect frame = view.frame;
+    frame.size.height += 80;
+    view.frame = frame;
+    
+    self.tappableArrow = view;
 }
 
 
@@ -127,41 +130,40 @@
     if (self = [super initWithFrame:frame
                     reuseIdentifier:reuseIdentifier
                               model:model_]) {
-        titleLabel.font = [UIFont boldSystemFontOfSize:18];
-        titleLabel.adjustsFontSizeToFitWidth = YES;
-        titleLabel.minimumFontSize = 14;
-
         self.directorTitleLabel = [self createTitleLabel:NSLocalizedString(@"Directors:", nil) yPosition:22];
-        self.directorLabel = [self createValueLabel:22];
-
+        self.directorLabel = [self createValueLabel:22 forTitle:directorTitleLabel];
+        
         self.castTitleLabel = [self createTitleLabel:NSLocalizedString(@"Cast:", nil) yPosition:37];
-        self.castLabel = [self createValueLabel:37];
-
+        self.castLabel = [self createValueLabel:37 forTitle:castTitleLabel];
+        
         self.genreTitleLabel = [self createTitleLabel:NSLocalizedString(@"Genre:", nil) yPosition:52];
-        self.genreLabel = [self createValueLabel:52];
-
+        self.genreLabel = [self createValueLabel:52 forTitle:genreTitleLabel];
+        
         self.ratedTitleLabel = [self createTitleLabel:NSLocalizedString(@"Rated:", nil) yPosition:67];
-        self.ratedLabel = [self createValueLabel:67];
-
+        self.ratedLabel = [self createValueLabel:67 forTitle:ratedTitleLabel];
+        
         self.netflixTitleLabel = [self createTitleLabel:NSLocalizedString(@"Netflix:", nil) yPosition:82];
-        self.netflixLabel = [self createValueLabel:81];
+        self.netflixLabel = [self createValueLabel:81 forTitle:netflixTitleLabel];
         netflixLabel.font = [UIFont systemFontOfSize:17];
-
+        
+        self.availabilityLabel = [self createValueLabel:67 forTitle:ratedTitleLabel];
+        self.formatsLabel = [self createValueLabel:81 forTitle:netflixTitleLabel];
+        
         titleWidth = 0;
         for (UILabel* label in self.titleLabels) {
             titleWidth = MAX(titleWidth, [label.text sizeWithFont:label.font].width);
         }
-
+        
         for (UILabel* label in self.titleLabels) {
             CGRect frame = label.frame;
-            frame.origin.x = (int)(imageView.frame.size.width + 7);
+            frame.origin.x = (int)(imageView.frame.size.width + 2);
             frame.size.width = titleWidth;
             label.frame = frame;
         }
-
-        [self.contentView addSubview:titleLabel];
+        
+        [self setupTappableArrow];
     }
-
+    
     return self;
 }
 
@@ -175,14 +177,14 @@
         userRating = NO;
         rating = [model.netflixCache netflixRatingForMovie:movie];
     }
-
+    
     if (rating.length == 0) {
         netflixLabel.text = @"";
         return;
     }
-
+    
     CGFloat score = [rating floatValue];
-
+    
     for (int i = 0; i < 5; i++) {
         CGFloat value = score - i;
         if (value <= 0) {
@@ -193,7 +195,7 @@
             [result appendString:[Application halfStarString]];
         }
     }
-
+    
     netflixLabel.text = result;
 }
 
@@ -209,32 +211,36 @@
 
 - (void) loadMovie:(id) owner {
     [self loadImage];
-
+    
     directorLabel.text  = [[model directorsForMovie:movie]  componentsJoinedByString:@", "];
     castLabel.text      = [[model castForMovie:movie]       componentsJoinedByString:@", "];
     genreLabel.text     = [[model genresForMovie:movie]     componentsJoinedByString:@", "];
-
+    formatsLabel.text   = [[[[model.netflixCache formatsForMovie:movie] sortedArrayUsingSelector:@selector(compare:)] componentsJoinedByString:@"/"] stringByReplacingOccurrencesOfString:@"/i" withString:@"/I"];
+    availabilityLabel.text = [model.netflixCache availabilityForMovie:movie];
+    
     NSString* rating;
     if (movie.isUnrated) {		
         rating = NSLocalizedString(@"Unrated", nil);		
     } else {		
         rating = movie.rating;		
     }
-
+    
     ratedLabel.text = rating;
-
+    
     if (movie.directors.count <= 1) {
         directorTitleLabel.text = NSLocalizedString(@"Director:", nil);
     } else {
         directorTitleLabel.text = NSLocalizedString(@"Directors:", nil);
     }
-
+    
     [self setNetflixLabel];
     [self setNetflixLabelColor];
-
+    
     for (UILabel* label in self.allLabels) {
         [self.contentView addSubview:label];
     }
+    
+    [self setNeedsLayout];
 }
 
 
@@ -246,19 +252,59 @@
 - (void) setSelected:(BOOL) selected
             animated:(BOOL) animated {
     [super setSelected:selected animated:animated];
-
-    if (!selected) {
+    
+    if (selected) {
+    } else {
         [self setNetflixLabelColor];
     }
 }
 
 
 - (void) onSetSameMovie:(Movie*) movie_
-                   owner:(id) owner  {
+                  owner:(id) owner  {
     [super onSetSameMovie:movie_ owner:owner];
-
+    
     [NSThread cancelPreviousPerformRequestsWithTarget:self selector:@selector(loadMovie:) object:owner];
     [self performSelector:@selector(loadMovie:) withObject:owner afterDelay:0];
+}
+
+
+- (void) layoutSubviews {
+    [super layoutSubviews];
+    
+    [availabilityLabel sizeToFit];
+    [formatsLabel sizeToFit];
+    
+    CGRect frame = self.frame;
+    
+    {
+        CGRect formatFrame = formatsLabel.frame;
+        formatFrame.origin.x = frame.size.width - formatFrame.size.width - 5;
+        formatsLabel.frame = formatFrame;
+    }
+    
+    {
+        CGRect availabilityFrame = availabilityLabel.frame;
+        availabilityFrame.origin.x = frame.size.width - availabilityFrame.size.width - 5;
+        availabilityLabel.frame = availabilityFrame;
+    }
+}
+
+
+- (void)setEditing:(BOOL)editing animated:(BOOL)animated {
+    [super setEditing:editing animated:animated];
+    
+    [UIView beginAnimations:nil context:NULL];
+    {
+        if (editing) {
+            availabilityLabel.alpha = 0;
+            formatsLabel.alpha = 0;
+        } else {
+            availabilityLabel.alpha = 1;
+            formatsLabel.alpha = 1;
+        }
+    }
+    [UIView commitAnimations];
 }
 
 @end
