e1ae1ec70974b8d98c69968b061b6a6e4d87bd07
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-02-15 21:15:40 +0000

    tagging PocketFlicks 1.4

---
+++ tags/PocketFlicks/1.4/iPhone/Classes/AbstractSearchEngine.m
@@ -0,0 +1,180 @@
+// Copyright 2008 Cyrus Najmabadi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "AbstractSearchEngine.h"
+
+#import "Movie.h"
+#import "Model.h"
+#import "SearchEngineDelegate.h"
+#import "SearchRequest.h"
+#import "SearchResult.h"
+#import "Utilities.h"
+
+@interface AbstractSearchEngine()
+@property (assign) id<SearchEngineDelegate> delegate;
+@property NSInteger currentRequestId;
+@property (retain) Model* model;
+@property (retain) SearchRequest* currentlyExecutingRequest;
+@property (retain) SearchRequest* nextSearchRequest;
+@property (retain) NSCondition* gate;
+@end
+
+
+@implementation AbstractSearchEngine
+
+@synthesize delegate;
+@synthesize currentRequestId;
+@synthesize model;
+@synthesize currentlyExecutingRequest;
+@synthesize nextSearchRequest;
+@synthesize gate;
+
+
+- (void) dealloc {
+    self.delegate = nil;
+    self.currentRequestId = 0;
+    self.model = nil;
+    self.currentlyExecutingRequest = nil;
+    self.nextSearchRequest = nil;
+    self.gate = nil;
+
+    [super dealloc];
+}
+
+
+- (id) initWithModel:(Model*) model_
+            delegate:(id<SearchEngineDelegate>) delegate_ {
+    if (self = [super init]) {
+        self.model = model_;
+        self.currentRequestId = 0;
+        self.delegate = delegate_;
+        self.gate = [[[NSCondition alloc] init] autorelease];
+
+        [self performSelectorInBackground:@selector(searchThreadEntryPoint) withObject:nil];
+    }
+
+    return self;
+}
+
+
+- (BOOL) abortEarly {
+    BOOL result;
+
+    [gate lock];
+    {
+        result = currentlyExecutingRequest.requestId != currentRequestId;
+    }
+    [gate unlock];
+
+    return result;
+}
+
+
+- (void) search {
+    @throw [NSException exceptionWithName:@"ImproperSubclassing" reason:@"" userInfo:nil];
+}
+
+
+- (void) searchLoop {
+    while (true) {
+        NSAutoreleasePool* autoreleasePool= [[NSAutoreleasePool alloc] init];
+        {
+            [gate lock];
+            {
+                while (nextSearchRequest == nil) {
+                    [gate wait];
+                }
+
+                self.currentlyExecutingRequest = nextSearchRequest;
+                self.nextSearchRequest = nil;
+            }
+            [gate unlock];
+
+            [self search];
+
+            self.currentlyExecutingRequest = nil;
+        }
+        [autoreleasePool release];
+    }
+}
+
+
+- (void) searchThreadEntryPoint {
+    NSAutoreleasePool* autoreleasePool= [[NSAutoreleasePool alloc] init];
+    {
+        [NSThread setThreadPriority:0.0];
+
+        [self searchLoop];
+    }
+    [autoreleasePool release];
+}
+
+
+- (void) submitRequest:(NSString*) string {
+    [gate lock];
+    {
+        currentRequestId++;
+        self.nextSearchRequest = [SearchRequest requestWithId:currentRequestId value:string model:model];
+
+        [gate broadcast];
+    }
+    [gate unlock];
+}
+
+
+- (void) reportResult:(SearchResult*) result {
+    NSAssert([NSThread isMainThread], nil);
+
+    BOOL abort = NO;
+    [gate lock];
+    {
+        if (result.requestId != currentRequestId) {
+            abort = YES;
+        }
+    }
+    [gate unlock];
+
+    if (abort) {
+        return;
+    }
+
+    [delegate reportResult:result];
+}
+
+
+- (void) reportError:(NSString*) error movies:(NSArray*) movies {
+    [self reportError:error movies:movies people:nil];
+}
+
+
+- (void) reportError:(NSString*) error movies:(NSArray*) movies people:(NSArray*) people {
+    SearchResult* result = [SearchResult resultWithId:currentlyExecutingRequest.requestId
+                                                value:currentlyExecutingRequest.value
+                                                error:error
+                                               movies:movies
+                                               people:people];
+    [self performSelectorOnMainThread:@selector(reportResult:) withObject:result waitUntilDone:NO];
+}
+
+
+- (void) invalidateExistingRequests {
+    [gate lock];
+    {
+        currentRequestId++;
+        self.nextSearchRequest = nil;
+    }
+    [gate unlock];
+}
+
+@end
---
+++ tags/PocketFlicks/1.4/iPhone/Classes/SearchResult.h
@@ -0,0 +1,41 @@
+// Copyright 2008 Cyrus Najmabadi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+@interface SearchResult : NSObject {
+@private
+    NSInteger requestId;
+    NSString* value;
+    NSString* error;
+    NSArray* movies;
+    NSArray* people;
+}
+
+@property (readonly) NSInteger requestId;
+@property (readonly, copy) NSString* value;
+@property (readonly, copy) NSString* error;
+@property (readonly, retain) NSArray* movies;
+@property (readonly, retain) NSArray* people;
+
++ (SearchResult*) resultWithId:(NSInteger) requestId
+                         value:(NSString*) value
+                         error:(NSString*) error
+                        movies:(NSArray*) movies;
+
++ (SearchResult*) resultWithId:(NSInteger) requestId
+                         value:(NSString*) value
+                         error:(NSString*) error
+                        movies:(NSArray*) movies
+                        people:(NSArray*) people;
+
+@end
