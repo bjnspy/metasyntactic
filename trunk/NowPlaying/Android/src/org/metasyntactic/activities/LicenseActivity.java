8f6f5d9255308cd371cef20ce4402be1e4faabe7
mjoshi@google.com <mjoshi@google.com@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-02-20 18:53:54 +0000

    Adding LicenseActivity to controller

--- trunk/NowPlaying/Android/src/org/metasyntactic/activities/LicenseActivity.java
+++ trunk/NowPlaying/Android/src/org/metasyntactic/activities/LicenseActivity.java
@@ -1,16 +1,34 @@
 package org.metasyntactic.activities;
 
 import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebView;
+
+import org.metasyntactic.NowPlayingControllerWrapper;
+import org.metasyntactic.utilities.LogUtilities;
 
 public class LicenseActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.license);
+    NowPlayingControllerWrapper.addActivity(this);
     WebView license = (WebView) findViewById(R.id.license);
     license.loadUrl("file:///android_asset/License.html");
+  }
+
+  @Override
+  protected void onDestroy() {
+    LogUtilities.i(getClass().getSimpleName(), "onDestroy");
+    NowPlayingControllerWrapper.removeActivity(this);
+    super.onDestroy();
+  }
+
+  @Override
+  public Object onRetainNonConfigurationInstance() {
+    LogUtilities.i(getClass().getSimpleName(), "onRetainNonConfigurationInstance");
+    final Object result = new Object();
+    NowPlayingControllerWrapper.onRetainNonConfigurationInstance(this, result);
+    return result;
   }
 }
