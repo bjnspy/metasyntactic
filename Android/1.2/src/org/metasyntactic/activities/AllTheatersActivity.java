9062487e7b93ad940fbdaf7748fd1dbff79124bf
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-02-23 22:36:21 +0000

    ci before tag

---
+++ Android/1.2/src/org/metasyntactic/activities/AllTheatersActivity.java
@@ -0,0 +1,332 @@
+package org.metasyntactic.activities;
+
+import android.app.ListActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.metasyntactic.NowPlayingApplication;
+import org.metasyntactic.INowPlaying;
+import org.metasyntactic.NowPlayingControllerWrapper;
+import org.metasyntactic.data.Location;
+import org.metasyntactic.data.Theater;
+import org.metasyntactic.threading.ThreadingUtilities;
+import org.metasyntactic.utilities.MovieViewUtilities;
+import org.metasyntactic.views.FastScrollView;
+import org.metasyntactic.views.NowPlayingPreferenceDialog;
+import org.metasyntactic.utilities.LogUtilities;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author mjoshi@google.com (Megha Joshi)
+ */
+public class AllTheatersActivity extends ListActivity implements INowPlaying {
+  private int lastPosition;
+  private int lastSection;
+  private TheatersAdapter adapter;
+  private List<Theater> theaters = new ArrayList<Theater>();
+  private final Map<Integer, Integer> alphaTheaterSectionsMap = new HashMap<Integer, Integer>();
+  private final Map<Integer, Integer> alphaTheaterPositionsMap = new HashMap<Integer, Integer>();
+  private final Map<Integer, Integer> distanceTheaterSectionsMap = new HashMap<Integer, Integer>();
+  private final Map<Integer, Integer> distanceTheaterPositionsMap = new HashMap<Integer, Integer>();
+  private String[] distance;
+  private String[] alphabet;
+  private Location userLocation;
+  private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+    @Override public void onReceive(final Context context, final Intent intent) {
+      refresh();
+    }
+  };
+  // Define comparators for theater listings sort.
+  private final Comparator<Theater> TITLE_ORDER = new Comparator<Theater>() {
+    public int compare(final Theater m1, final Theater m2) {
+      return m1.getName().compareTo(m2.getName());
+    }
+  };
+  private final Comparator<Theater> DISTANCE_ORDER = new Comparator<Theater>() {
+    public int compare(final Theater m1, final Theater m2) {
+      final Double dist_m1 = userLocation.distanceTo(m1.getLocation());
+      final Double dist_m2 = userLocation.distanceTo(m2.getLocation());
+      return dist_m1.compareTo(dist_m2);
+    }
+  };
+  // The order of items in this array should match the
+  // entries_theater_sort_preference array in res/values/arrays.xml
+  @SuppressWarnings("unchecked") private final List<Comparator<Theater>> THEATER_ORDER = Arrays.asList(TITLE_ORDER, DISTANCE_ORDER);
+
+  @Override protected void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    LogUtilities.i(getClass().getSimpleName(), "onCreate");
+    NowPlayingControllerWrapper.addActivity(this);
+    getAlphabet(this);
+    getDistances();
+    setupView();
+  }
+
+  @Override protected void onDestroy() {
+    LogUtilities.i(getClass().getSimpleName(), "onDestroy");
+    NowPlayingControllerWrapper.removeActivity(this);
+    super.onDestroy();
+  }
+
+  @Override protected void onPause() {
+    LogUtilities.i(getClass().getSimpleName(), "onPause");
+    unregisterReceiver(broadcastReceiver);
+    super.onPause();
+  }
+
+  @Override protected void onResume() {
+    super.onResume();
+    LogUtilities.i(getClass().getSimpleName(), "onResume");
+    registerReceiver(broadcastReceiver, new IntentFilter(NowPlayingApplication.NOW_PLAYING_CHANGED_INTENT));
+  }
+
+  @Override public Object onRetainNonConfigurationInstance() {
+    LogUtilities.i(getClass().getSimpleName(), "onRetainNonConfigurationInstance");
+    final Object result = new Object();
+    NowPlayingControllerWrapper.onRetainNonConfigurationInstance(this, result);
+    return result;
+  }
+
+  private void setupView() {
+    theaters = new ArrayList<Theater>(NowPlayingControllerWrapper.getTheaters());
+    setContentView(R.layout.theaterlist);
+    final ListView list = getListView();
+    userLocation = NowPlayingControllerWrapper.getLocationForAddress(NowPlayingControllerWrapper.getUserLocation());
+    Collections.sort(theaters, THEATER_ORDER.get(NowPlayingControllerWrapper.getAllTheatersSelectedSortIndex()));
+    populateAlphaTheaterSectionsAndPositions();
+    populateDistanceTheaterSectionsAndPositions();
+    // Set up Movies adapter
+    adapter = new TheatersAdapter();
+    list.setAdapter(adapter);
+  }
+
+  private void populateAlphaTheaterSectionsAndPositions() {
+    int i = 0;
+    String prevLetter = null;
+    final List<String> alphabets = Arrays.asList(alphabet);
+    for (final Theater theater : theaters) {
+      final String firstLetter = theater.getName().substring(0, 1);
+      alphaTheaterSectionsMap.put(i, alphabets.indexOf(firstLetter));
+      if (prevLetter == null || !firstLetter.equals(prevLetter)) {
+        alphaTheaterPositionsMap.put(alphabets.indexOf(firstLetter), i);
+      }
+      prevLetter = firstLetter;
+      i++;
+    }
+    for (i = 0; i < alphabets.size(); i++) {
+      if (alphaTheaterPositionsMap.get(i) == null) {
+        if (i == 0) {
+          alphaTheaterPositionsMap.put(i, 0);
+        } else {
+          alphaTheaterPositionsMap.put(i, alphaTheaterPositionsMap.get(i - 1));
+        }
+      }
+    }
+  }
+
+  private void getDistances() {
+    distance = new String[6];
+    distance[0] = getResources().getString(R.string.less_than_number_string_away, 2, getResources().getString(R.string.miles));
+    distance[1] = getResources().getString(R.string.less_than_number_string_away, 5, getResources().getString(R.string.miles));
+    distance[2] = getResources().getString(R.string.less_than_number_string_away, 10, getResources().getString(R.string.miles));
+    distance[3] = getResources().getString(R.string.less_than_number_string_away, 25, getResources().getString(R.string.miles));
+    distance[4] = getResources().getString(R.string.less_than_number_string_away, 50, getResources().getString(R.string.miles));
+    distance[5] = getResources().getString(R.string.less_than_number_string_away, 100, "miles");
+  }
+
+  private void populateDistanceTheaterSectionsAndPositions() {
+    int i = 0;
+    int prevLevel = 0;
+    final List<String> distances = Arrays.asList(distance);
+    for (final Theater theater : theaters) {
+      final double distance = userLocation.distanceTo(theater.getLocation());
+      final int distanceLevel = MovieViewUtilities.getDistanceLevel(distance);
+      distanceTheaterSectionsMap.put(i, distanceLevel);
+      if (prevLevel == 0 || distanceLevel != prevLevel) {
+        distanceTheaterPositionsMap.put(distanceLevel, i);
+      }
+      prevLevel = distanceLevel;
+      i++;
+    }
+    for (i = 0; i < distances.size(); i++) {
+      if (distanceTheaterPositionsMap.get(i) == null) {
+        if (i == 0) {
+          distanceTheaterPositionsMap.put(i, 0);
+        } else {
+          distanceTheaterPositionsMap.put(i, distanceTheaterPositionsMap.get(i - 1));
+        }
+      }
+    }
+  }
+
+  @Override public boolean onCreateOptionsMenu(final Menu menu) {
+    menu.add(0, MovieViewUtilities.MENU_MOVIES, 0, R.string.menu_movies).setIcon(R.drawable.ic_menu_home).setIntent(
+        new Intent(this, AllTheatersActivity.class));
+    menu.add(0, MovieViewUtilities.MENU_SORT, 0, R.string.sort_theaters).setIcon(R.drawable.ic_menu_switch);
+    menu.add(0, MovieViewUtilities.MENU_SETTINGS, 0, R.string.settings).setIcon(android.R.drawable.ic_menu_preferences).setIntent(
+        new Intent(this, SettingsActivity.class).putExtra("from_menu","yes"));
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override public boolean onOptionsItemSelected(final MenuItem item) {
+    if (item.getItemId() == MovieViewUtilities.MENU_SORT) {
+      final NowPlayingPreferenceDialog builder = new NowPlayingPreferenceDialog(this).setKey(NowPlayingPreferenceDialog.PreferenceKeys.THEATERS_SORT)
+      .setEntries(R.array.entries_theaters_sort_preference).setPositiveButton(android.R.string.ok).setNegativeButton(android.R.string.cancel);
+      builder.setTitle(R.string.sort_theaters);
+      builder.show();
+    }
+    if (item.getItemId() == MovieViewUtilities.MENU_MOVIES) {
+      startActivity(new Intent(this, AllTheatersActivity.class));
+    }
+    if (item.getItemId() == MovieViewUtilities.MENU_SETTINGS) {
+      startActivity(new Intent(this, SettingsActivity.class).putExtra("from_menu","yes"));
+    }
+    return true;
+  }
+
+  private void getAlphabet(final Context context) {
+    final String alphabetString = context.getResources().getString(R.string.alphabet);
+    alphabet = new String[alphabetString.length()];
+    for (int i = 0; i < alphabet.length; i++) {
+      alphabet[i] = String.valueOf(alphabetString.charAt(i));
+    }
+  }
+
+  private class TheatersAdapter extends BaseAdapter implements FastScrollView.SectionIndexer {
+    private final LayoutInflater inflater;
+
+    private TheatersAdapter() {
+      // Cache the LayoutInflate to avoid asking for a new one each time.
+      inflater = LayoutInflater.from(AllTheatersActivity.this);
+    }
+
+    public Object getItem(final int i) {
+      return i;
+    }
+
+    public View getView(final int position, View convertView, final ViewGroup viewGroup) {
+      convertView = inflater.inflate(R.layout.theaterview, null);
+      // Creates a MovieViewHolder and store references to the
+      // children
+      // views we want to bind data to.
+      final MovieViewHolder holder = new MovieViewHolder((TextView) convertView.findViewById(R.id.address), (TextView) convertView
+          .findViewById(R.id.title));
+      // Bind the data efficiently with the holder.
+      final Theater theater = theaters.get(position);
+      holder.title.setText(theater.getName());
+      holder.address.setText(theater.getAddress() + ", " + theater.getLocation().getCity());
+      return convertView;
+    }
+
+    public int getCount() {
+      return theaters.size();
+    }
+
+    private class MovieViewHolder {
+      private final TextView address;
+      private final TextView title;
+
+      private MovieViewHolder(final TextView address, final TextView title) {
+        this.address = address;
+        this.title = title;
+      }
+    }
+
+    public void refreshTheaters(final List<Theater> new_theaters) {
+      theaters = new_theaters;
+      notifyDataSetChanged();
+    }
+
+    public long getItemId(final int position) {
+      return position;
+    }
+
+    public int getPositionForSection(final int section) {
+      Integer position = null;
+      if (NowPlayingControllerWrapper.getAllTheatersSelectedSortIndex() == 0) {
+        position = alphaTheaterPositionsMap.get(section);
+      }
+      if (NowPlayingControllerWrapper.getAllTheatersSelectedSortIndex() == 1) {
+        position = distanceTheaterPositionsMap.get(section);
+      }
+      if (position != null) {
+        lastPosition = position;
+      }
+      return lastPosition;
+    }
+
+    public int getSectionForPosition(final int position) {
+      Integer section = null;
+      if (NowPlayingControllerWrapper.getAllTheatersSelectedSortIndex() == 0) {
+        section = alphaTheaterSectionsMap.get(position);
+      }
+      if (NowPlayingControllerWrapper.getAllTheatersSelectedSortIndex() == 1) {
+        section = distanceTheaterSectionsMap.get(position);
+      }
+      if (section != null) {
+        lastSection = section;
+      }
+      return lastSection;
+    }
+
+    public Object[] getSections() {
+      if (NowPlayingControllerWrapper.getAllTheatersSelectedSortIndex() == 0) {
+        return alphabet;
+      }
+      if (NowPlayingControllerWrapper.getAllTheatersSelectedSortIndex() == 1) {
+        return distance;
+      }
+      return null;
+    }
+  }
+
+  public Context getContext() {
+    return this;
+  }
+
+  public void refresh() {
+    if (ThreadingUtilities.isBackgroundThread()) {
+      final Runnable runnable = new Runnable() {
+        public void run() {
+          refresh();
+        }
+      };
+      ThreadingUtilities.performOnMainThread(runnable);
+      return;
+    }
+    final List<Theater> localTheaters = NowPlayingControllerWrapper.getTheaters();
+    Collections.sort(localTheaters, THEATER_ORDER.get(NowPlayingControllerWrapper.getAllTheatersSelectedSortIndex()));
+    populateAlphaTheaterSectionsAndPositions();
+    populateDistanceTheaterSectionsAndPositions();
+    FastScrollView.getSections();
+    adapter.refreshTheaters(localTheaters);
+  }
+
+  @Override protected void onListItemClick(final ListView listView, final View view, final int position, final long id) {
+    final Theater theater = theaters.get(position);
+    final Intent intent = new Intent();
+    intent.setClass(this, TheaterDetailsActivity.class);
+    intent.putExtra("theater", (Parcelable) theater);
+    startActivity(intent);
+    super.onListItemClick(listView, view, position, id);
+  }
+}
---
+++ Android/1.2/src/org/metasyntactic/activities/ShowtimesDetailsActivity.java
@@ -0,0 +1,243 @@
+package org.metasyntactic.activities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.metasyntactic.NowPlayingControllerWrapper;
+import org.metasyntactic.data.Movie;
+import org.metasyntactic.data.Performance;
+import org.metasyntactic.data.Theater;
+import org.metasyntactic.utilities.LogUtilities;
+import org.metasyntactic.utilities.MovieViewUtilities;
+import org.metasyntactic.utilities.StringUtilities;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class ShowtimesDetailsActivity extends ListActivity {
+  private Theater theater;
+  private Movie movie;
+  private final List<TheaterDetailItem> detailItems = new ArrayList<TheaterDetailItem>();
+  private List<Performance> performances = new ArrayList<Performance>();
+  private final List<String> showtimes = new ArrayList<String>();
+  private final List<String> showtimes_url = new ArrayList<String>();
+
+  private enum TheaterDetailItemType {
+    NAME_SHOWTIMES, ADDRESS, PHONE
+  }
+
+  @Override protected void onListItemClick(final ListView listView, final View view, final int position, final long id) {
+    final Intent intent = detailItems.get(position).getIntent();
+    if (intent != null) {
+      startActivity(intent);
+    }
+    super.onListItemClick(listView, view, position, id);
+  }
+
+  @Override public void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    LogUtilities.i(getClass().getSimpleName(), "onCreate");
+    NowPlayingControllerWrapper.addActivity(this);
+    setContentView(R.layout.showtimedetails);
+    movie = getIntent().getExtras().getParcelable("movie");
+    theater = getIntent().getExtras().getParcelable("theater");
+    performances = NowPlayingControllerWrapper.getPerformancesForMovieAtTheater(movie, theater);
+    for (final Performance per : performances) {
+      if (per != null && !StringUtilities.isNullOrEmpty(per.getUrl())) {
+        showtimes.add(per.getTime());
+        showtimes_url.add(per.getUrl());
+      }
+    }
+    populateTheaterDetailItem();
+    final TheaterAdapter theaterAdapter = new TheaterAdapter();
+    setListAdapter(theaterAdapter);
+    final TextView theaterTxt = (TextView) findViewById(R.id.theater);
+    theaterTxt.setText(theater.getName());
+    final Button orderTickets = (Button) findViewById(R.id.showtimes);
+    orderTickets.setOnClickListener(new OnClickListener() {
+      public void onClick(final View v) {
+        showDialog(1);
+      }
+    });
+    if (!showtimes.isEmpty()) {
+      findViewById(R.id.bottom_bar).setVisibility(View.VISIBLE);
+    }
+  }
+
+  @Override protected void onResume() {
+    super.onResume();
+    LogUtilities.i(getClass().getSimpleName(), "onResume");
+  }
+
+  @Override protected void onPause() {
+    super.onPause();
+    LogUtilities.i(getClass().getSimpleName(), "onPause");
+  }
+
+  @Override protected void onDestroy() {
+    LogUtilities.i(getClass().getSimpleName(), "onDestroy");
+    NowPlayingControllerWrapper.removeActivity(this);
+    super.onDestroy();
+  }
+
+  @Override public Object onRetainNonConfigurationInstance() {
+    LogUtilities.i(getClass().getSimpleName(), "onRetainNonConfigurationInstance");
+    final Object result = new Object();
+    NowPlayingControllerWrapper.onRetainNonConfigurationInstance(this, result);
+    return result;
+  }
+
+  private void populateTheaterDetailItem() {
+    // Add name_showtimes type
+    TheaterDetailItem entry1 = new TheaterDetailItem();
+    entry1.setType(TheaterDetailItemType.NAME_SHOWTIMES);
+    detailItems.add(entry1);
+    // Add address type
+    entry1 = new TheaterDetailItem();
+    entry1.setType(TheaterDetailItemType.ADDRESS);
+    detailItems.add(entry1);
+    // Add phone type
+    entry1 = new TheaterDetailItem();
+    entry1.setType(TheaterDetailItemType.PHONE);
+    detailItems.add(entry1);
+  }
+
+  private class TheaterAdapter extends BaseAdapter {
+    private final LayoutInflater inflater;
+
+    private TheaterAdapter() {
+      // Cache the LayoutInflate to avoid asking for a new one each time.
+      inflater = LayoutInflater.from(ShowtimesDetailsActivity.this);
+    }
+
+    public View getView(final int position, View convertView, final ViewGroup viewGroup) {
+      convertView = inflater.inflate(R.layout.showtimes_item, null);
+      final Resources res = getResources();
+      final TheaterDetailsViewHolder holder = new TheaterDetailsViewHolder((TextView) convertView.findViewById(R.id.label), (ImageView) convertView
+          .findViewById(R.id.icon), (TextView) convertView.findViewById(R.id.data));
+      final int theaterIndex = position / TheaterDetailItemType.values().length;
+      switch (detailItems.get(position).getType()) {
+      case NAME_SHOWTIMES:
+        holder.label.setText(res.getString(R.string.showtimes_for, movie.getCanonicalTitle(), theater.getName()));
+        holder.icon.setImageDrawable(getResources().getDrawable(R.drawable.sym_action_email));
+        String performance = "";
+        for (final Performance per : performances) {
+          performance += per.getTime() + ", ";
+        }
+        performance = performance.substring(0, performance.length() - 2);
+        holder.data.setText(performance);
+        final String addr = "user@example.com";
+        final Intent intent1 = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + addr));
+        intent1.putExtra("subject", res.getString(R.string.showtimes_for, movie.getDisplayTitle(), theater.getName()));
+        intent1.putExtra("body", performance);
+        detailItems.get(position).setIntent(intent1);
+        break;
+      case PHONE:
+        holder.data.setText(theater.getPhoneNumber());
+        holder.icon.setImageDrawable(getResources().getDrawable(R.drawable.sym_action_call));
+        holder.label.setText(res.getString(R.string.call));
+        final Intent intent2 = new Intent("android.intent.action.DIAL", Uri.parse("tel:" + theater.getPhoneNumber()));
+        detailItems.get(position).setIntent(intent2);
+        break;
+      case ADDRESS:
+        final String address = theater.getAddress() + ", "
+        + theater.getLocation().getCity();
+        holder.data.setText(address);
+        holder.icon.setImageDrawable(getResources().getDrawable(R.drawable.sym_action_map));
+        holder.label.setText(res.getString(R.string.location));
+        final Intent intent3 = new Intent("android.intent.action.VIEW", Uri.parse("geo:0,0?q=" + address));
+        detailItems.get(position).setIntent(intent3);
+      }
+      return convertView;
+    }
+
+    public int getCount() {
+      return detailItems.size();
+    }
+
+    private class TheaterDetailsViewHolder {
+      private final TextView label;
+      private final ImageView icon;
+      private final TextView data;
+
+      private TheaterDetailsViewHolder(final TextView label, final ImageView icon, final TextView data) {
+        this.label = label;
+        this.icon = icon;
+        this.data = data;
+      }
+    }
+
+    public long getEntryId(final int position) {
+      return position;
+    }
+
+    public Object getItem(final int position) {
+      return detailItems.get(position);
+    }
+
+    public long getItemId(final int position) {
+      return position;
+    }
+
+    public void refresh() {
+      notifyDataSetChanged();
+    }
+  }
+
+  private static class TheaterDetailItem {
+    private TheaterDetailItemType type;
+    private Intent intent;
+
+    public Intent getIntent() {
+      return intent;
+    }
+
+    public void setIntent(final Intent intent) {
+      this.intent = intent;
+    }
+
+    public TheaterDetailItemType getType() {
+      return type;
+    }
+
+    public void setType(final TheaterDetailItemType type) {
+      this.type = type;
+    }
+  }
+
+  @Override public boolean onCreateOptionsMenu(final Menu menu) {
+    menu.add(0, MovieViewUtilities.MENU_MOVIES, 0, R.string.menu_movies).setIcon(R.drawable.ic_menu_home).setIntent(
+        new Intent(this, NowPlayingActivity.class));
+    menu.add(0, MovieViewUtilities.MENU_SETTINGS, 0, R.string.settings).setIcon(android.R.drawable.ic_menu_preferences).setIntent(
+        new Intent(this, SettingsActivity.class).putExtra("from_menu","yes"));
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override protected Dialog onCreateDialog(final int id) {
+    final String[] criteria = new String[showtimes.size()];
+    showtimes.toArray(criteria);
+    return new AlertDialog.Builder(this).setTitle(R.string.order_tickets).setItems(criteria, new DialogInterface.OnClickListener() {
+      public void onClick(final DialogInterface dialog, final int which) {
+        final String order_url = showtimes_url.get(which);
+        final Intent intent = new Intent("android.intent.action.VIEW", Uri.parse(order_url));
+        startActivity(intent);
+      }
+    }).create();
+  }
+}
