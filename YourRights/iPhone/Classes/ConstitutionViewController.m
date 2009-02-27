3c33a5ff8dcfad53cc247687e0b64320353d166d
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-02-27 22:32:38 +0000

    Adding loc file

--- YourRights/iPhone/Classes/ConstitutionViewController.m
+++ YourRights/iPhone/Classes/ConstitutionViewController.m
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "UnitedStatesConstitutionViewController.h"
+#import "ConstitutionViewController.h"
 
 #import "Amendment.h"
 #import "Article.h"
