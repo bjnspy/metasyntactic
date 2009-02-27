775f7f4ea22bf18c7fd671d99235d0af6d103765
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-02-27 22:48:54 +0000

    moving text

---
+++ YourRights/iPhone/Classes/UnitedStatesConstitution.h
@@ -0,0 +1,18 @@
+//
+//  UnitedStatesConstitution.h
+//  YourRights
+//
+//  Created by Cyrus Najmabadi on 2/27/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface UnitedStatesConstitution : NSObject {
+
+}
+
++ (Constitution*) unitedStatesConstitution;
+
+@end
