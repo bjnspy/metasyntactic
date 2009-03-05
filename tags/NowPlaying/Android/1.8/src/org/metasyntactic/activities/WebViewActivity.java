6fcff17989d42fba131c05617991fe45d8e3594a
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-03-05 20:42:09 +0000

    tagging 1.8

---
+++ tags/NowPlaying/Android/1.8/src/org/metasyntactic/activities/WebViewActivity.java
@@ -0,0 +1,40 @@
+package org.metasyntactic.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import org.metasyntactic.NowPlayingControllerWrapper;
+import org.metasyntactic.utilities.LogUtilities;
+
+/**
+ * @author mjoshi@google.com (Megha Joshi)
+ */
+public class WebViewActivity extends Activity {
+  @Override
+  protected void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.license);
+    NowPlayingControllerWrapper.addActivity(this);
+    final WebView webView = (WebView)findViewById(R.id.license);
+    if ("license".equals(getIntent().getExtras().getString("type"))) {
+      webView.loadUrl("file:///android_asset/License.html");
+    } else {
+      webView.loadUrl("file:///android_asset/credits.html");
+    }
+  }
+
+  @Override
+  protected void onDestroy() {
+    LogUtilities.i(getClass().getSimpleName(), "onDestroy");
+    NowPlayingControllerWrapper.removeActivity(this);
+    super.onDestroy();
+  }
+
+  @Override
+  public Object onRetainNonConfigurationInstance() {
+    LogUtilities.i(getClass().getSimpleName(), "onRetainNonConfigurationInstance");
+    final Object result = new Object();
+    NowPlayingControllerWrapper.onRetainNonConfigurationInstance(this, result);
+    return result;
+  }
+}
