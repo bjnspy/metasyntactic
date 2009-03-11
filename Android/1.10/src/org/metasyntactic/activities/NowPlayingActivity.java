36a132196c783fb25949cdcfd45f29b1e0f03b19
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-03-11 03:51:04 +0000

    tagging 1.10

---
+++ Android/1.10/src/org/metasyntactic/activities/NowPlayingActivity.java
@@ -0,0 +1,330 @@
+package org.metasyntactic.activities;
+
+import static org.apache.commons.collections.CollectionUtils.isEmpty;
+import static org.metasyntactic.utilities.StringUtilities.isNullOrEmpty;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import org.metasyntactic.NowPlayingApplication;
+import org.metasyntactic.NowPlayingControllerWrapper;
+import org.metasyntactic.data.Movie;
+import org.metasyntactic.providers.DataProvider;
+import org.metasyntactic.utilities.FileUtilities;
+import org.metasyntactic.utilities.LogUtilities;
+import org.metasyntactic.utilities.MovieViewUtilities;
+import org.metasyntactic.views.CustomGridView;
+import org.metasyntactic.views.NowPlayingPreferenceDialog;
+
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+
+/**
+ * @author mjoshi@google.com (Megha Joshi)
+ */
+public class NowPlayingActivity extends MoviesActivity {
+  private TextView progressUpdate;
+  private final BroadcastReceiver progressBroadcastReceiver = new BroadcastReceiver() {
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+      progressUpdate.setText(intent.getStringExtra("message"));
+    }
+  };
+  private final BroadcastReceiver dataBroadcastReceiver = new BroadcastReceiver() {
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+      // the data provider finished downloading. set up our view accordingly.
+      setupView();
+    }
+  };
+
+  private void showNoInformationFoundDialog() {
+    new AlertDialog.Builder(this).setMessage(R.string.no_information).setPositiveButton(
+        android.R.string.ok, new DialogInterface.OnClickListener() {
+          public void onClick(final DialogInterface dialog, final int whichButton) {
+          }
+        }).show();
+  }
+
+  private void setupView() {
+    // we're currently in 'downloading' mode. We need to deal with a few
+    // cases. First, we deal with the case where a user has returned to
+    // this activity, and now there are movies available. In that case, we
+    // just display them.
+    refresh();
+    if (isEmpty(movies)) {
+      // Ok. so we have no movies. THat means one of two things. Either
+      // we're trying to download the movies, or we tried and failed to
+      // download them. In the former case just wait. We'll get a
+      // notification when they're done. In the latter case, let the user
+      // know.
+      if (!isNullOrEmpty(NowPlayingControllerWrapper.getUserLocation())
+          && NowPlayingControllerWrapper.getDataProviderState() == DataProvider.State.Finished) {
+        showNoInformationFoundDialog();
+      }
+    } else {
+      setupMovieGrid();
+    }
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    LogUtilities.i(getClass().getSimpleName(), "onResume");
+    if (FileUtilities.isSDCardAccessible()) {
+      scrolling = false;
+      registerReceiver(dataBroadcastReceiver, new IntentFilter(
+          NowPlayingApplication.NOW_PLAYING_LOCAL_DATA_DOWNLOADED));
+      registerReceiver(progressBroadcastReceiver, new IntentFilter(
+          NowPlayingApplication.NOW_PLAYING_LOCAL_DATA_DOWNLOAD_PROGRESS));
+      if (isGridSetup) {
+        grid.setVisibility(View.VISIBLE);
+        postersAdapter.notifyDataSetChanged();
+      } else {
+        setupView();
+      }
+    }
+  }
+
+  @Override
+  protected void onPause() {
+    LogUtilities.i(getClass().getSimpleName(), "onPause");
+    if (FileUtilities.isSDCardAccessible()) {
+      unregisterReceiver(dataBroadcastReceiver);
+      unregisterReceiver(progressBroadcastReceiver);
+    }
+    super.onPause();
+  }
+
+  @Override
+  protected void onDestroy() {
+    LogUtilities.i(getClass().getSimpleName(), "onDestroy");
+    if (FileUtilities.isSDCardAccessible()) {
+      NowPlayingControllerWrapper.removeActivity(this);
+      clearBitmaps();
+    }
+    super.onDestroy();
+  }
+
+  @Override
+  public Object onRetainNonConfigurationInstance() {
+    LogUtilities.i(getClass().getSimpleName(), "onRetainNonConfigurationInstance");
+    final Object result = search;
+    NowPlayingControllerWrapper.onRetainNonConfigurationInstance(this, result);
+    return result;
+  }
+
+  /**
+   * Updates display of the list of movies.
+   */
+  @Override public void refresh() {
+    if (search == null) {
+      movies = new ArrayList<Movie>(NowPlayingControllerWrapper.getMovies());
+    }
+    // sort movies according to the default sort preference.
+    final Comparator<Movie> comparator = MOVIE_ORDER.get(NowPlayingControllerWrapper
+        .getAllMoviesSelectedSortIndex());
+    Collections.sort(movies, comparator);
+    super.refresh();
+  }
+
+  @Override public Context getContext() {
+    return this;
+  }
+
+  @Override
+  public void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    LogUtilities.i(getClass().getSimpleName(), "onCreate");
+    broadcastReceiver = new BroadcastReceiver() {
+      @Override public void onReceive(final Context context, final Intent intent) {
+        refresh();
+      }
+    };
+    scrollStatebroadcastReceiver = new BroadcastReceiver() {
+      @Override public void onReceive(final Context context, final Intent intent) {
+        if (NowPlayingApplication.SCROLLING_INTENT.equals(intent.getAction())) {
+          scrolling = true;
+        } else if (NowPlayingApplication.NOT_SCROLLING_INTENT.equals(intent.getAction())) {
+          scrolling = false;
+          postersAdapter.notifyDataSetChanged();
+        }
+      }
+    };
+    search = (String) getLastNonConfigurationInstance();
+    // check for sdcard mounted properly
+    if (FileUtilities.isSDCardAccessible()) {
+      // Request the progress bar to be shown in the title
+      requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+      setContentView(R.layout.progressbar_1);
+      progressUpdate = (TextView) findViewById(R.id.progress_update);
+      NowPlayingControllerWrapper.addActivity(this);
+      getUserLocation();
+      refresh();
+    } else {
+      new AlertDialog.Builder(this).setTitle(R.string.insert_sdcard).setPositiveButton(
+          android.R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(final DialogInterface dialog, final int whichButton) {
+              finish();
+            }
+          }).show();
+    }
+  }
+
+  @Override
+  protected void onNewIntent(final Intent intent) {
+    LogUtilities.i(getClass().getSimpleName(), "onNewIntent");
+    super.onNewIntent(intent);
+    refresh();
+  }
+
+  private void setupMovieGrid() {
+    if (isGridSetup) {
+      return;
+    }
+    isGridSetup = true;
+    setContentView(R.layout.moviegrid_anim);
+    bottomBar = (RelativeLayout) findViewById(R.id.bottom_bar);
+    if (search == null) {
+      bottomBar.setVisibility(View.GONE);
+    }
+    final View allMovies = findViewById(R.id.all_movies);
+    allMovies.setOnClickListener(new OnClickListener() {
+      public void onClick(final View arg0) {
+        final Intent intent = new Intent().setClass(NowPlayingActivity.this,
+            NowPlayingActivity.class);
+        startActivity(intent);
+        bottomBar.setVisibility(View.GONE);
+      }
+    });
+    grid = (CustomGridView) findViewById(R.id.grid);
+    grid.setOnItemClickListener(new OnItemClickListener() {
+      public void onItemClick(final AdapterView<?> parent, final View view, final int position,
+          final long id) {
+        selectedMovie = movies.get(position);
+        setupRotationAnimation(view);
+      }
+    });
+    populateSections();
+    postersAdapter = new NowPlayingPostersAdapter();
+    grid.setAdapter(postersAdapter);
+    intent = new Intent().setClass(this, MovieDetailsActivity.class);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(final Menu menu) {
+    menu.add(0, MovieViewUtilities.MENU_SEARCH, 0, R.string.search).setIcon(
+        android.R.drawable.ic_menu_search);
+    menu.add(0, MovieViewUtilities.MENU_SORT, 0, R.string.sort_movies).setIcon(
+        R.drawable.ic_menu_switch);
+    menu.add(0, MovieViewUtilities.MENU_THEATER, 0, R.string.theaters).setIcon(
+        R.drawable.ic_menu_allfriends);
+    menu.add(0, MovieViewUtilities.MENU_UPCOMING, 0, R.string.upcoming)
+    .setIcon(R.drawable.upcoming);
+    menu.add(0, MovieViewUtilities.MENU_SEND_FEEDBACK, 0, R.string.send_feedback).setIcon(
+        android.R.drawable.ic_menu_send);
+    menu.add(0, MovieViewUtilities.MENU_SETTINGS, 0, R.string.settings).setIcon(
+        android.R.drawable.ic_menu_preferences).setIntent(
+            new Intent(this, SettingsActivity.class).putExtra("from_menu", "yes"))
+            .setAlphabeticShortcut('s');
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(final MenuItem item) {
+    if (item.getItemId() == MovieViewUtilities.MENU_SORT) {
+      final NowPlayingPreferenceDialog builder = new NowPlayingPreferenceDialog(this).setKey(
+          NowPlayingPreferenceDialog.PreferenceKeys.MOVIES_SORT).setEntries(
+              R.array.entries_movies_sort_preference).setPositiveButton(android.R.string.ok)
+              .setNegativeButton(android.R.string.cancel);
+      builder.setTitle(R.string.sort_movies);
+      builder.show();
+      return true;
+    }
+    if (item.getItemId() == MovieViewUtilities.MENU_THEATER) {
+      final Intent localIntent = new Intent();
+      localIntent.setClass(this, AllTheatersActivity.class);
+      startActivity(localIntent);
+      return true;
+    }
+    if (item.getItemId() == MovieViewUtilities.MENU_UPCOMING) {
+      final Intent localIntent = new Intent();
+      localIntent.setClass(this, UpcomingMoviesActivity.class);
+      startActivity(localIntent);
+      return true;
+    }
+    if (item.getItemId() == MovieViewUtilities.MENU_SEARCH) {
+      final Intent localIntent = new Intent();
+      localIntent.setClass(this, SearchMovieActivity.class);
+      localIntent.putExtra("activity", "NowPlayingActivity");
+      startActivity(localIntent);
+      return true;
+    }
+    if (item.getItemId() == MovieViewUtilities.MENU_SEND_FEEDBACK) {
+      final Resources res = getResources();
+      final String address = "cyrus.najmabadi@gmail.com, mjoshi@google.com";
+      final Intent localIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + address));
+      localIntent.putExtra("subject", res.getString(R.string.feedback));
+      final String body = getUserSettings();
+      localIntent.putExtra("body", body);
+      startActivity(localIntent);
+      return true;
+    }
+    return false;
+  }
+
+  private String getUserSettings() {
+    String body = "\n\n\n\n";
+    body += NowPlayingApplication.getNameAndVersion(getResources());
+    body += "\nAuto-Update Location: " + NowPlayingControllerWrapper.isAutoUpdateEnabled();
+    body += "\nLocation: " + NowPlayingControllerWrapper.getUserLocation();
+    body += "\nSearch Distance: " + NowPlayingControllerWrapper.getSearchDistance();
+    body += "\nReviews: " + NowPlayingControllerWrapper.getScoreType();
+    return body;
+  }
+
+  @Override protected void populateSections() {
+    super.populateSections();
+    if (NowPlayingControllerWrapper.getAllMoviesSelectedSortIndex() == 0) {
+      populateAlphaMovieSectionsAndPositions();
+    } else if (NowPlayingControllerWrapper.getAllMoviesSelectedSortIndex() == 2) {
+      populateScoreMovieSectionsAndPositions();
+    }
+  }
+
+  protected class NowPlayingPostersAdapter extends PostersAdapter {
+    protected NowPlayingPostersAdapter() {
+      super();
+    }
+
+    @Override
+    public Object[] getSections() {
+      // fast scroll is implemented only for alphabetic & score sort for release
+      // 1.
+      if (NowPlayingControllerWrapper.getAllMoviesSelectedSortIndex() == 0) {
+        return actualSections.toArray();
+      }
+      if (NowPlayingControllerWrapper.getAllMoviesSelectedSortIndex() == 2) {
+        return actualSections.toArray();
+      }
+      return null;
+    }
+  }
+}
---
+++ Android/1.10/src/org/metasyntactic/activities/UpcomingMovieDetailsActivity.java
@@ -0,0 +1,360 @@
+package org.metasyntactic.activities;
+
+import static org.apache.commons.collections.CollectionUtils.isEmpty;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.metasyntactic.NowPlayingApplication;
+import org.metasyntactic.NowPlayingControllerWrapper;
+import org.metasyntactic.data.Movie;
+import org.metasyntactic.data.Review;
+import org.metasyntactic.utilities.LogUtilities;
+import org.metasyntactic.utilities.MovieViewUtilities;
+import org.metasyntactic.utilities.StringUtilities;
+
+import android.app.ListActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+/**
+ * @author mjoshi@google.com (Megha Joshi)
+ */
+public class UpcomingMovieDetailsActivity extends ListActivity {
+  /**
+   * Called when the activity is first created.
+   */
+  private List<MovieDetailEntry> movieDetailEntries = new ArrayList<MovieDetailEntry>();
+  private Movie movie;
+  private MovieAdapter movieAdapter;
+
+  private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+    @Override public void onReceive(final Context context, final Intent intent) {
+      movieAdapter.notifyDataSetChanged();
+    }
+  };
+
+  @Override
+  public void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    LogUtilities.i(getClass().getSimpleName(), "onCreate");
+    NowPlayingControllerWrapper.addActivity(this);
+    setContentView(R.layout.upcomingmoviedetails);
+    final Bundle extras = getIntent().getExtras();
+    movie = extras.getParcelable("movie");
+    NowPlayingControllerWrapper.prioritizeMovie(movie);
+    final Resources res = getResources();
+    final TextView title = (TextView)findViewById(R.id.title);
+    title.setText(movie.getDisplayTitle());
+    // Get and set scores text and background image
+    final TextView ratingLengthLabel = (TextView)findViewById(R.id.ratingLength);
+    final CharSequence rating = MovieViewUtilities.formatRatings(movie.getRating(), res);
+    final CharSequence length = MovieViewUtilities.formatLength(movie.getLength(), res);
+    ratingLengthLabel.setText(rating + ". " + length);
+    populateMovieDetailEntries();
+    movieAdapter = new MovieAdapter();
+    setListAdapter(movieAdapter);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    LogUtilities.i(getClass().getSimpleName(), "onResume");
+
+    registerReceiver(broadcastReceiver, new IntentFilter(NowPlayingApplication.NOW_PLAYING_CHANGED_INTENT));
+  }
+
+  @Override
+  protected void onPause() {
+    LogUtilities.i(getClass().getSimpleName(), "onPause");
+
+    unregisterReceiver(broadcastReceiver);
+
+    super.onPause();
+  }
+
+  @Override
+  protected void onDestroy() {
+    LogUtilities.i(getClass().getSimpleName(), "onDestroy");
+    NowPlayingControllerWrapper.removeActivity(this);
+    super.onDestroy();
+  }
+
+  @Override
+  public Object onRetainNonConfigurationInstance() {
+    LogUtilities.i(getClass().getSimpleName(), "onRetainNonConfigurationInstance");
+    final Object result = movieDetailEntries;
+    NowPlayingControllerWrapper.onRetainNonConfigurationInstance(this, result);
+    return result;
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override public List<MovieDetailEntry> getLastNonConfigurationInstance() {
+    return (List<MovieDetailEntry>)super.getLastNonConfigurationInstance();
+  }
+
+  private void populateMovieDetailEntries() {
+    movieDetailEntries = getLastNonConfigurationInstance();
+
+    if (isEmpty(movieDetailEntries)) {
+      movieDetailEntries = new ArrayList<MovieDetailEntry>();
+      final Resources res = getResources();
+      // Add title and Synopsis
+      {
+        final MovieDetailEntry entry = new MovieDetailEntry(res.getString(R.string.synopsis), null, MovieDetailItemType.POSTER_SYNOPSIS, null,
+            false);
+        movieDetailEntries.add(entry);
+      }
+      // Add release Date
+      final Date releaseDate = movie.getReleaseDate();
+      if (releaseDate != null) {
+        final String releaseDateString = DateFormat.getDateInstance(DateFormat.LONG).format(releaseDate);
+        final MovieDetailEntry entry = new MovieDetailEntry(res.getString(R.string.release_date_colon), releaseDateString, MovieDetailItemType.DATA,
+            null, false);
+        movieDetailEntries.add(entry);
+      }
+      {
+        // Add cast
+        final MovieDetailEntry entry = new MovieDetailEntry(res.getString(R.string.cast_colon),
+            MovieViewUtilities.formatListToString(movie.getCast()), MovieDetailItemType.DATA, null, false);
+        movieDetailEntries.add(entry);
+      }
+      // Add director
+      final List<String> directors = movie.getDirectors();
+      if (directors != null && !directors.isEmpty()) {
+        final MovieDetailEntry entry = new MovieDetailEntry(res.getString(R.string.director_colon), MovieViewUtilities.formatListToString(directors),
+            MovieDetailItemType.DATA, null, false);
+        movieDetailEntries.add(entry);
+      }
+      {
+        // Add header
+        final MovieDetailEntry entry = new MovieDetailEntry(res.getString(R.string.more_options), null, MovieDetailItemType.HEADER, null, false);
+        movieDetailEntries.add(entry);
+      }
+      // Add trailer
+      final String trailer_url = NowPlayingControllerWrapper.getTrailer(movie);
+      if (!StringUtilities.isNullOrEmpty(trailer_url) && trailer_url.startsWith("http")) {
+        final Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.parse(trailer_url), "video/*");
+        final MovieDetailEntry entry = new MovieDetailEntry(res.getString(R.string.play_trailer), null, MovieDetailItemType.ACTION, intent, true);
+        movieDetailEntries.add(entry);
+      }
+      // Add reviews
+      // doing this as the getReviews() throws NPE instead null return.
+      ArrayList<Review> reviews = new ArrayList<Review>();
+      if (NowPlayingControllerWrapper.getScore(movie) != null) {
+        reviews = new ArrayList<Review>(NowPlayingControllerWrapper.getReviews(movie));
+      }
+      if (!reviews.isEmpty()) {
+        final Intent intent = new Intent();
+        intent.putParcelableArrayListExtra("reviews", reviews);
+        intent.setClass(this, AllReviewsActivity.class);
+        final MovieDetailEntry entry = new MovieDetailEntry(res.getString(R.string.read_reviews), null, MovieDetailItemType.ACTION, intent, true);
+        movieDetailEntries.add(entry);
+      }
+
+      // Add website links
+      final Map<String,String> nameToUrl = new LinkedHashMap<String, String>();
+      nameToUrl.put("IMDb", NowPlayingControllerWrapper.getIMDbAddress(movie));
+      nameToUrl.put("Wikipedia", NowPlayingControllerWrapper.getWikipediaAddress(movie));
+      nameToUrl.put("Amazon", NowPlayingControllerWrapper.getAmazonAddress(movie));
+
+      for (final Map.Entry<String,String> entry : nameToUrl.entrySet()) {
+        final String url = entry.getValue();
+        final String name = entry.getKey();
+        if (!StringUtilities.isNullOrEmpty(url) && url.startsWith("http")) {
+          final Intent intent = new Intent("android.intent.action.VIEW", Uri.parse(url));
+          final MovieDetailEntry movieDetails = new MovieDetailEntry(name, null, MovieDetailItemType.ACTION, intent, true);
+          movieDetailEntries.add(movieDetails);
+        }
+      }
+    }
+  }
+
+  private class MovieAdapter extends BaseAdapter {
+    @Override
+    public boolean areAllItemsEnabled() {
+      return false;
+    }
+
+    @Override
+    public boolean isEnabled(final int position) {
+      return movieDetailEntries.get(position).isSelectable();
+    }
+
+    private final LayoutInflater inflater;
+
+    private MovieAdapter() {
+      // Cache the LayoutInflate to avoid asking for a new one each time.
+      inflater = LayoutInflater.from(UpcomingMovieDetailsActivity.this);
+    }
+
+    public Object getEntry(final int i) {
+      return i;
+    }
+
+    public View getView(final int position, View convertView, final ViewGroup viewGroup) {
+      final MovieDetailEntry entry = movieDetailEntries.get(position);
+      switch (entry.type) {
+      case POSTER_SYNOPSIS:
+        convertView = setupPosterAndSynopsisView();
+        break;
+      case DATA:
+        convertView = inflater.inflate(R.layout.moviedetails_item, null);
+        // Creates a MovieViewHolder and store references to the
+        // children views we want to bind data to.
+        final MovieViewHolder holder = new MovieViewHolder((TextView)convertView.findViewById(R.id.name),
+            (TextView)convertView.findViewById(R.id.value));
+        holder.name.setText(entry.name);
+        holder.value.setText(entry.value);
+        break;
+      case HEADER:
+        convertView = inflater.inflate(R.layout.headerview, null);
+        final TextView headerView = (TextView)convertView.findViewById(R.id.name);
+        headerView.setText(entry.name);
+        break;
+      case ACTION:
+        convertView = inflater.inflate(R.layout.dataview, null);
+        final TextView view = (TextView)convertView.findViewById(R.id.name);
+        view.setText(entry.name);
+        break;
+      }
+      return convertView;
+    }
+
+    private View setupPosterAndSynopsisView() {
+      final View convertView = inflater.inflate(R.layout.moviepostersynopsis, null);
+      final ImageView posterImage = (ImageView)convertView.findViewById(R.id.poster);
+      final TextView text1 = (TextView)convertView.findViewById(R.id.value1);
+      final TextView text2 = (TextView)convertView.findViewById(R.id.value2);
+      final byte[] bytes = NowPlayingControllerWrapper.getPoster(movie);
+      if (bytes.length > 0) {
+        posterImage.setImageBitmap(BitmapFactory.decodeByteArray(bytes, 0, bytes.length));
+        posterImage.setBackgroundResource(R.drawable.image_frame);
+      }
+
+      String synopsis = NowPlayingControllerWrapper.getSynopsis(movie);
+      if (StringUtilities.isNullOrEmpty(synopsis)) {
+        synopsis = getResources().getString(R.string.no_synopsis_available_dot);
+      }
+
+      final TextPaint paint = text1.getPaint();
+      final int orientation = getResources().getConfiguration().orientation;
+      final int textViewWidth;
+      if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        // textViewWidth = screenWidth - posterWidth - paddingLeft -
+        // paddingRight - spaceBetweenPosterAndTextView
+        textViewWidth = 480 - 126 - 5 - 5 - 5;
+      } else {
+        textViewWidth = 320 - 126 - 5 - 5 - 5;
+      }
+      final Layout layout = new StaticLayout(synopsis, paint, textViewWidth, Layout.Alignment.ALIGN_NORMAL, 1, 0, false);
+      // height of poster is 182px
+      final int line = layout.getLineForVertical(182);
+      final int off = layout.getLineStart(line + 1);
+      final String desc1_text = synopsis.substring(0, off);
+      final String desc2_text = synopsis.substring(off, synopsis.length());
+      text1.setText(desc1_text);
+      text2.setText(desc2_text);
+
+      return convertView;
+    }
+
+    public int getCount() {
+      return movieDetailEntries.size();
+    }
+
+    private class MovieViewHolder {
+      private final TextView name;
+      private final TextView value;
+
+      private MovieViewHolder(final TextView name, final TextView value) {
+        this.name = name;
+        this.value = value;
+      }
+    }
+
+    public long getEntryId(final int position) {
+      return position;
+    }
+
+    public Object getItem(final int position) {
+      return movieDetailEntries.get(position);
+    }
+
+    public long getItemId(final int position) {
+      return position;
+    }
+
+    public void refresh() {
+      notifyDataSetChanged();
+    }
+  }
+
+  private static class MovieDetailEntry {
+    private final String name;
+    private final String value;
+    private final MovieDetailItemType type;
+    private final Intent intent;
+    private final boolean selectable;
+
+    private MovieDetailEntry(final String name, final String value, final MovieDetailItemType type, final Intent intent, final boolean selectable) {
+      this.name = name;
+      this.value = value;
+      this.type = type;
+      this.intent = intent;
+      this.selectable = selectable;
+    }
+
+    public boolean isSelectable() {
+      return selectable;
+    }
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(final Menu menu) {
+    menu.add(0, MovieViewUtilities.MENU_MOVIES, 0, R.string.menu_movies).setIcon(R.drawable.ic_menu_home)
+    .setIntent(new Intent(this, NowPlayingActivity.class));
+    menu.add(0, MovieViewUtilities.MENU_SETTINGS, 0, R.string.settings).setIcon(android.R.drawable.ic_menu_preferences)
+    .setIntent(new Intent(this, SettingsActivity.class).putExtra("from_menu", "yes"));
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  private enum MovieDetailItemType {
+    POSTER_SYNOPSIS, DATA, ACTION, HEADER
+  }
+
+  @Override
+  protected void onListItemClick(final ListView listView, final View view, final int position, final long id) {
+
+    final MovieDetailEntry entry = movieDetailEntries.get(position);
+    final Intent intent = entry.intent;
+    if (intent != null) {
+      startActivity(intent);
+    }
+
+    super.onListItemClick(listView, view, position, id);
+  }
+}
