0a921f8dbfd9dae5a5c2e8d9ef1005fdf7cd3800
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-03-23 22:28:21 +0000

    removing code

--- iPhoneShared/Classes/LinkedSet.h
+++ iPhoneShared/Classes/LinkedSet.h
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/*
 @interface LinkedSet : NSObject {
 @private
     NSLock* gate;
@@ -37,3 +37,4 @@
 - (NSInteger) count;
 
 @end
+*/
