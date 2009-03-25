4d378f6479cf81f3954bf7da91a8e20b53f8aa46
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-03-25 16:59:28 +0000

    access field directly

--- trunk/NowPlaying/iPhone/Classes/ApplicationTabBarController.h
+++ trunk/NowPlaying/iPhone/Classes/ApplicationTabBarController.h
@@ -14,11 +14,11 @@
 
 @interface ApplicationTabBarController : UITabBarController<UITabBarControllerDelegate> {
 @private
-    MoviesNavigationController* moviesNavigationController_;
-    TheatersNavigationController* theatersNavigationController_;
-    UpcomingMoviesNavigationController* upcomingMoviesNavigationController_;
-    DVDNavigationController* dvdNavigationController_;
-    NetflixNavigationController* netflixNavigationController_;
+    MoviesNavigationController* moviesNavigationController;
+    TheatersNavigationController* theatersNavigationController;
+    UpcomingMoviesNavigationController* upcomingMoviesNavigationController;
+    DVDNavigationController* dvdNavigationController;
+    NetflixNavigationController* netflixNavigationController;
 }
 
 + (ApplicationTabBarController*) controller;
