4bac9fe653f254a568fe34ec2eab0dd177b1488e
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-05-02 03:02:20 +0000

    tagging 3.7

---
+++ iPhone/3.7/Classes/AbstractNavigationController.m
@@ -0,0 +1,307 @@
+// Copyright 2008 Cyrus Najmabadi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "AbstractNavigationController.h"
+
+#import "Controller.h"
+#import "Model.h"
+#import "Movie.h"
+#import "MovieDetailsViewController.h"
+#import "PostersViewController.h"
+#import "ReviewsViewController.h"
+#import "SearchViewController.h"
+#import "SettingsViewController.h"
+#import "Theater.h"
+#import "TheaterDetailsViewController.h"
+#import "TicketsViewController.h"
+#import "WebViewController.h"
+
+#ifndef IPHONE_OS_VERSION_3
+#endif
+
+@interface AbstractNavigationController()
+@property (retain) PostersViewController* postersViewController;
+@property BOOL visible;
+#ifndef IPHONE_OS_VERSION_3
+@property BOOL isViewLoaded;
+@property (retain) SearchViewController* searchViewController;
+#endif
+@end
+
+
+@implementation AbstractNavigationController
+
+@synthesize postersViewController;
+@synthesize visible;
+#ifndef IPHONE_OS_VERSION_3
+@synthesize isViewLoaded;
+@synthesize searchViewController;
+#endif
+
+- (void) dealloc {
+    self.postersViewController = nil;
+    self.visible = NO;
+
+#ifndef IPHONE_OS_VERSION_3
+    self.isViewLoaded = NO;
+    self.searchViewController = nil;
+#endif
+
+    [super dealloc];
+}
+
+
+- (void) loadView {
+    [super loadView];
+
+#ifndef IPHONE_OS_VERSION_3
+    self.isViewLoaded = YES;
+#endif
+
+    self.view.autoresizesSubviews = YES;
+}
+
+
+- (void) refreshWithSelector:(SEL) selector {
+    if (!self.isViewLoaded) {
+        return;
+    }
+
+    if (self.modalViewController != nil) {
+        if ([self.modalViewController respondsToSelector:selector]) {
+            [self.modalViewController performSelector:selector];
+        }
+    }
+
+    if (!visible) {
+        return;
+    }
+
+    for (id controller in self.viewControllers) {
+        if ([controller respondsToSelector:selector]) {
+            [controller performSelector:selector];
+        }
+    }
+}
+
+
+- (void) majorRefresh {
+    [self refreshWithSelector:@selector(majorRefresh)];
+}
+
+
+- (void) minorRefresh {
+    [self refreshWithSelector:@selector(minorRefresh)];
+}
+
+
+- (void) viewDidAppear:(BOOL) animated {
+    [super viewDidAppear:animated];
+    self.visible = YES;
+}
+
+
+- (void) viewDidDisappear:(BOOL) animated {
+    [super viewDidDisappear:animated];
+    self.visible = NO;
+}
+
+
+- (void) didReceiveMemoryWarning {
+    if (visible || postersViewController != nil) {
+        return;
+    }
+
+    [self popToRootViewControllerAnimated:NO];
+    [super didReceiveMemoryWarning];
+}
+
+
+- (Model*) model {
+    return [Model model];
+}
+
+
+- (Controller*) controller {
+    return [Controller controller];
+}
+
+
+- (Movie*) movieForTitle:(NSString*) canonicalTitle {
+    for (Movie* movie in self.model.movies) {
+        if ([movie.canonicalTitle isEqual:canonicalTitle]) {
+            return movie;
+        }
+    }
+
+    return nil;
+}
+
+
+- (Theater*) theaterForName:(NSString*) name {
+    for (Theater* theater in self.model.theaters) {
+        if ([theater.name isEqual:name]) {
+            return theater;
+        }
+    }
+
+    return nil;
+}
+
+
+- (void) navigateToLastViewedPage {
+    NSArray* types = self.model.navigationStackTypes;
+    NSArray* values = self.model.navigationStackValues;
+
+    for (int i = 0; i < types.count; i++) {
+        NSInteger type = [[types objectAtIndex:i] intValue];
+        id value = [values objectAtIndex:i];
+
+        if (type == MovieDetails) {
+            Movie* movie = [self movieForTitle:value];
+            [self pushMovieDetails:movie animated:NO];
+        } else if (type == TheaterDetails) {
+            Theater* theater = [self theaterForName:value];
+            [self pushTheaterDetails:theater animated:NO];
+        } else if (type == Reviews) {
+            Movie* movie = [self movieForTitle:value];
+            [self pushReviews:movie animated:NO];
+        } else if (type == Tickets) {
+            Movie* movie = [self movieForTitle:[value objectAtIndex:0]];
+            Theater* theater = [self theaterForName:[value objectAtIndex:1]];
+            NSString* title = [value objectAtIndex:2];
+
+            [self pushTicketsView:movie theater:theater title:title animated:NO];
+        }
+    }
+}
+
+
+- (void) pushReviews:(Movie*) movie animated:(BOOL) animated {
+    ReviewsViewController* controller = [[[ReviewsViewController alloc] initWithMovie:movie] autorelease];
+
+    [self pushViewController:controller animated:animated];
+}
+
+
+- (void) pushMovieDetails:(Movie*) movie
+                 animated:(BOOL) animated {
+    if (movie == nil) {
+        return;
+    }
+
+    UIViewController* viewController = [[[MovieDetailsViewController alloc] initWithMovie:movie] autorelease];
+    [self pushViewController:viewController animated:animated];
+}
+
+
+- (void) pushTheaterDetails:(Theater*) theater animated:(BOOL) animated {
+    if (theater == nil) {
+        return;
+    }
+
+    UIViewController* viewController = [[[TheaterDetailsViewController alloc] initWithTheater:theater] autorelease];
+    [self pushViewController:viewController animated:animated];
+}
+
+
+- (void) pushTicketsView:(Movie*) movie
+                 theater:(Theater*) theater
+                   title:(NSString*) title
+                animated:(BOOL) animated {
+    if (movie == nil || theater == nil) {
+        return;
+    }
+
+    UIViewController* viewController = [[[TicketsViewController alloc] initWithTheater:theater
+                                                                                 movie:movie
+                                                                                 title:title] autorelease];
+
+    [self pushViewController:viewController animated:animated];
+}
+
+
+- (BOOL) shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation) interfaceOrientation {
+    [super shouldAutorotateToInterfaceOrientation:interfaceOrientation];
+    if (interfaceOrientation == UIInterfaceOrientationPortrait) {
+        return YES;
+    }
+
+    return self.model.screenRotationEnabled && postersViewController == nil;
+}
+
+
+- (void) hidePostersView {
+    [self popViewControllerAnimated:YES];
+    self.postersViewController = nil;
+}
+
+
+- (void) showPostersView:(Movie*) movie posterCount:(NSInteger) posterCount {
+    if (postersViewController != nil) {
+        [self hidePostersView];
+    }
+
+    self.postersViewController =
+    [[[PostersViewController alloc] initWithMovie:movie
+                                      posterCount:posterCount] autorelease];
+
+    [self pushViewController:postersViewController animated:YES];
+}
+
+
+- (void) pushBrowser:(NSString*) address showSafariButton:(BOOL) showSafariButton animated:(BOOL) animated {
+    WebViewController* controller = [[[WebViewController alloc] initWithAddress:address
+                                                               showSafariButton:showSafariButton] autorelease];
+    [self pushViewController:controller animated:animated];
+}
+
+
+- (void) pushBrowser:(NSString*) address animated:(BOOL) animated {
+    [self pushBrowser:address showSafariButton:YES animated:animated];
+}
+
+
+- (void) pushInfoControllerAnimated:(BOOL) animated {
+    UIViewController* controller = [[[SettingsViewController alloc] init] autorelease];
+
+#if IPHONE_OS_VERSION_3
+    UINavigationController* navigationController = [[[AbstractNavigationController alloc] initWithRootViewController:controller] autorelease];
+    navigationController.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
+    [self presentModalViewController:navigationController animated:YES];
+#else
+    [self pushViewController:controller animated:YES];
+#endif
+}
+
+#ifndef IPHONE_OS_VERSION_3
+- (void) showSearchView {
+    if (searchViewController == nil) {
+        self.searchViewController = [[[SearchViewController alloc] init] autorelease];
+    }
+
+    [self pushViewController:searchViewController animated:YES];
+}
+#endif
+
+
+- (void) onTabBarItemSelected {
+    for (id controller in self.viewControllers) {
+        if ([controller respondsToSelector:@selector(onTabBarItemSelected)]) {
+            [controller onTabBarItemSelected];
+        }
+    }
+}
+
+@end
---
+++ iPhone/3.7/Classes/LocalizableStringsCache.m
@@ -0,0 +1,175 @@
+//
+//  StringsCache.m
+//  NowPlaying
+//
+//  Created by Cyrus Najmabadi on 4/29/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "LocalizableStringsCache.h"
+
+#import "Application.h"
+#import "FileUtilities.h"
+#import "LocaleUtilities.h"
+#import "NetworkUtilities.h"
+#import "NotificationCenter.h"
+#import "OperationQueue.h"
+
+@interface LocalizableStringsCache()
+@property (retain) NSLock* gate;
+@property (retain) NSDictionary* indexData;
+@end
+
+@implementation LocalizableStringsCache
+
+@synthesize gate;
+@synthesize indexData;
+
+static LocalizableStringsCache* instance = nil;
+
+- (void) dealloc {
+    self.gate = nil;
+    self.indexData = nil;
+    [super dealloc];
+}
+
+
+- (id) init {
+    if (self = [super init]) {
+        self.gate = [[[NSLock alloc] init] autorelease];
+    }
+
+    return self;
+}
+
+
++ (LocalizableStringsCache*) cache {
+    if (instance == nil) {
+        instance = [[LocalizableStringsCache alloc] init];
+    }
+
+    return instance;
+}
+
+
+- (NSString*) hashFile {
+    NSString* name = [NSString stringWithFormat:@"%@-hash.plist", [LocaleUtilities preferredLanguage]];
+    return [[Application localizableStringsDirectory] stringByAppendingPathComponent:name];
+}
+
+
+- (NSString*) indexFile {
+    NSString* name = [NSString stringWithFormat:@"%@.plist", [LocaleUtilities preferredLanguage]];
+    return [[Application localizableStringsDirectory] stringByAppendingPathComponent:name];
+}
+
+
+- (void) update {
+    if (updated) {
+        return;
+    }
+    updated = YES;
+    
+    [[OperationQueue operationQueue] performSelector:@selector(updateBackgroundEntryPoint)
+                                            onTarget:self
+                                                gate:nil
+                                            priority:Priority];
+}
+
+
++ (void) update {
+    [[LocalizableStringsCache cache] update];
+}
+
+
+- (void) updateBackgroundEntryPointWorker {
+    NSString* address = [NSString stringWithFormat:@"http://%@.appspot.com/LookupLocalizableStrings?id=%@&language=%@",
+                         [Application host],
+                         [[NSBundle mainBundle] bundleIdentifier],
+                         [LocaleUtilities preferredLanguage]];
+    NSString* hashAddress = [address stringByAppendingString:@"&hash=true"];
+
+    NSString* localHash = [FileUtilities readObject:self.hashFile];
+    NSString* serverHash = [NetworkUtilities stringWithContentsOfAddress:hashAddress];
+    if (serverHash.length > 0 && [serverHash isEqual:localHash]) {
+        return;
+    }
+
+    NSDictionary* dict = [NSDictionary dictionaryWithContentsOfURL:[NSURL URLWithString:address]];
+    if (dict.count <= 0) {
+        return;
+    }
+
+    [FileUtilities writeObject:dict toFile:self.indexFile];
+    [FileUtilities writeObject:serverHash toFile:self.hashFile];
+    [gate lock];
+    {
+        self.indexData = dict;
+    }
+    [gate unlock];
+}
+
+
+- (void) updateBackgroundEntryPoint {
+    NSDate* modificationDate = [FileUtilities modificationDate:self.hashFile];
+    if (modificationDate != nil) {
+        if (ABS(modificationDate.timeIntervalSinceNow) < ONE_WEEK) {
+            return;
+        }
+    }
+
+    NSString* notification = [LocalizedString(@"Translations", nil) lowercaseString];
+    [NotificationCenter addNotification:notification];
+    {
+        [self updateBackgroundEntryPointWorker];
+    }
+    [NotificationCenter removeNotification:notification];
+}
+
+
+- (NSDictionary*) loadIndex {
+    NSDictionary* result = [FileUtilities readObject:self.indexFile];
+    if (result.count == 0) {
+        return [NSDictionary dictionary];
+    }
+
+    return result;
+}
+
+
+- (NSDictionary*) indexNoLock {
+    if (indexData == nil) {
+        self.indexData = [self loadIndex];
+    }
+
+    // access through pointer so we always get value value back.
+    return self.indexData;
+}
+
+
+- (NSDictionary*) index {
+    NSDictionary* result;
+    [gate lock];
+    {
+        result = [self indexNoLock];
+    }
+    [gate unlock];
+    return result;
+}
+
+
+- (NSString*) localizedString:(NSString*) key {
+    NSString* result = [self.index objectForKey:key];
+    if (result.length > 0) {
+        return result;
+    }
+
+    return [[NSBundle mainBundle] localizedStringForKey:key value:key table:nil];
+}
+
+
++ (NSString*) localizedString:(NSString*) key {
+    return [[LocalizableStringsCache cache] localizedString:key];
+}
+
+@end
