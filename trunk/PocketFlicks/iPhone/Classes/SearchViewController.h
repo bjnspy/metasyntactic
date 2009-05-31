67d88f14b10f5bf4c3f3d0b778144aaf2f33176c
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-05-31 07:56:13 +0000

    porting over changes to pocketflicks

--- trunk/PocketFlicks/iPhone/Classes/SearchViewController.h
+++ trunk/PocketFlicks/iPhone/Classes/SearchViewController.h
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 #ifndef IPHONE_OS_VERSION_3
-#import "SearchEngineDelegate.h"
 
 @interface SearchViewController : AbstractTableViewController<SearchEngineDelegate,UISearchBarDelegate> {
 @private
