f945e96521bb5cb0acba734ef243c1412c15032d
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-05-31 03:42:07 +0000

    cleaning up code

--- trunk/MetasyntacticShared/Classes/MetasyntacticShared.h
+++ trunk/MetasyntacticShared/Classes/MetasyntacticShared.h
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#import "../ForwardDeclarations.h"
+
 #import "../External/OAuth/OAuthConsumer.h"
 
 #import "AbstractApplication.h"
