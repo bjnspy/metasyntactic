6776ec66c94d33b3ea364ace393f76bdc22d7590
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-06-07 20:25:32 +0000

    removing protocol buffers lib

--- trunk/MetasyntacticShared/Classes/AbstractApplication.m
+++ trunk/MetasyntacticShared/Classes/AbstractApplication.m
@@ -31,299 +31,299 @@ static NSString* trashDirectory = nil;
 
 
 + (NSString*) name {
-    return [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleDisplayName"];
+  return [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleDisplayName"];
 }
 
 
 + (NSString*) version {
-    return [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
+  return [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
 }
 
 
 + (NSString*) nameAndVersion {
-    NSString* appName = [self name];
-    NSString* appVersion = [self version];
-
-    return [NSString stringWithFormat:@"%@ v%@", appName, appVersion];
+  NSString* appName = [self name];
+  NSString* appVersion = [self version];
+  
+  return [NSString stringWithFormat:@"%@ v%@", appName, appVersion];
 }
 
 
 + (void) emptyTrash {
-    [ThreadingUtilities backgroundSelector:@selector(emptyTrashBackgroundEntryPoint)
-                                  onTarget:self
-                                      gate:nil
-                                   visible:NO];
+  [ThreadingUtilities backgroundSelector:@selector(emptyTrashBackgroundEntryPoint)
+                                onTarget:self
+                                    gate:nil
+                                 visible:NO];
 }
 
 
 + (void) initializeDirectories {
-    tempDirectory = [NSTemporaryDirectory() retain];
-
-    {
-        NSArray* paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, /*expandTilde:*/YES);
-        NSString* executableName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleExecutable"];
-        NSString* directory = [[paths objectAtIndex:0] stringByAppendingPathComponent:executableName];
-        [FileUtilities createDirectory:directory];
-        cacheDirectory = [directory retain];
-    }
-
-    {
-        NSArray* paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, /*expandTilde:*/YES);
-        NSString* executableName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleExecutable"];
-        NSString* directory = [[paths objectAtIndex:0] stringByAppendingPathComponent:executableName];
-        [FileUtilities createDirectory:directory];
-        supportDirectory = [directory retain];
-    }
-
-    {
-        NSString* directory = [cacheDirectory stringByAppendingPathComponent:@"Trash"];
-        [FileUtilities createDirectory:directory];
-        trashDirectory = [directory retain];
-    }
+  tempDirectory = [NSTemporaryDirectory() retain];
+  
+  {
+    NSArray* paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, /*expandTilde:*/YES);
+    NSString* executableName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleExecutable"];
+    NSString* directory = [[paths objectAtIndex:0] stringByAppendingPathComponent:executableName];
+    [FileUtilities createDirectory:directory];
+    cacheDirectory = [directory retain];
+  }
+  
+  {
+    NSArray* paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, /*expandTilde:*/YES);
+    NSString* executableName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleExecutable"];
+    NSString* directory = [[paths objectAtIndex:0] stringByAppendingPathComponent:executableName];
+    [FileUtilities createDirectory:directory];
+    supportDirectory = [directory retain];
+  }
+  
+  {
+    NSString* directory = [cacheDirectory stringByAppendingPathComponent:@"Trash"];
+    [FileUtilities createDirectory:directory];
+    trashDirectory = [directory retain];
+  }
 }
 
 
 + (void) initialize {
-    if (self == [AbstractApplication class]) {
-        gate = [[NSRecursiveLock alloc] init];
-
-        [self initializeDirectories];
-        [self emptyTrash];
-    }
+  if (self == [AbstractApplication class]) {
+    gate = [[NSRecursiveLock alloc] init];
+    
+    [self initializeDirectories];
+    [self emptyTrash];
+  }
 }
 
 
 + (NSLock*) gate {
-    return gate;
+  return gate;
 }
 
 
 + (NSString*) cacheDirectory {
-    return cacheDirectory;
+  return cacheDirectory;
 }
 
 
 + (NSString*) supportDirectory {
-    return supportDirectory;
+  return supportDirectory;
 }
 
 
 + (NSString*) trashDirectory {
-    return trashDirectory;
+  return trashDirectory;
 }
 
 
 + (NSString*) tempDirectory {
-    return tempDirectory;
+  return tempDirectory;
 }
 
 
 + (void) emptyTrashBackgroundEntryPoint {
-    NSLog(@"Application:emptyTrashBackgroundEntryPoint - start");
-    NSFileManager* manager = [NSFileManager defaultManager];
-    NSDirectoryEnumerator* enumerator = [manager enumeratorAtPath:trashDirectory];
-
+  NSLog(@"Application:emptyTrashBackgroundEntryPoint - start");
+  NSFileManager* manager = [NSFileManager defaultManager];
+  NSDirectoryEnumerator* enumerator = [manager enumeratorAtPath:trashDirectory];
+  
+  {
+    NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+    NSString* fileName;
+    while ((fileName = [enumerator nextObject]) != nil) {
+      NSString* fullPath = [trashDirectory stringByAppendingPathComponent:fileName];
+      NSDictionary* attributes = [enumerator fileAttributes];
+      
+      // don't delete folders yet
+      if (![[attributes objectForKey:NSFileType] isEqual:NSFileTypeDirectory]) {
+        NSLog(@"Application:emptyTrashBackgroundEntryPoint - %@", fullPath.lastPathComponent);
+        [manager removeItemAtPath:fullPath error:NULL];
+      }
+      
+      [NSThread sleepForTimeInterval:1];
+      
+      [pool release];
+      pool = [[NSAutoreleasePool alloc] init];
+    }
+    
+    [pool release];
+  }
+  
+  // Now remove the directories.
+  for (NSString* fileName in [FileUtilities directoryContentsNames:trashDirectory]) {
+    NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
     {
-        NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
-        NSString* fileName;
-        while ((fileName = [enumerator nextObject]) != nil) {
-            NSString* fullPath = [trashDirectory stringByAppendingPathComponent:fileName];
-            NSDictionary* attributes = [enumerator fileAttributes];
-
-            // don't delete folders yet
-            if (![[attributes objectForKey:NSFileType] isEqual:NSFileTypeDirectory]) {
-                NSLog(@"Application:emptyTrashBackgroundEntryPoint - %@", fullPath.lastPathComponent);
-                [manager removeItemAtPath:fullPath error:NULL];
-            }
-
-            [NSThread sleepForTimeInterval:1];
-
-            [pool release];
-            pool = [[NSAutoreleasePool alloc] init];
-        }
-
-        [pool release];
+      NSString* fullPath = [trashDirectory stringByAppendingPathComponent:fileName];
+      
+      [manager removeItemAtPath:fullPath error:NULL];
+      [NSThread sleepForTimeInterval:1];
     }
-
-    // Now remove the directories.
-    for (NSString* fileName in [FileUtilities directoryContentsNames:trashDirectory]) {
-        NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
-        {
-            NSString* fullPath = [trashDirectory stringByAppendingPathComponent:fileName];
-
-            [manager removeItemAtPath:fullPath error:NULL];
-            [NSThread sleepForTimeInterval:1];
-        }
-        [pool release];
-    }
-
-    NSLog(@"Application:emptyTrashBackgroundEntryPoint - stop");
+    [pool release];
+  }
+  
+  NSLog(@"Application:emptyTrashBackgroundEntryPoint - stop");
 }
 
 
 + (void) clearStaleItem:(NSString*) fullPath
            inEnumerator:(NSDirectoryEnumerator*) enumerator
             withManager:(NSFileManager*) manager {
-    if ((rand() % 1000) < 50) {
-        NSDictionary* attributes = [enumerator fileAttributes];
-
-        // don't delete folders
-        if (![[attributes objectForKey:NSFileType] isEqual:NSFileTypeDirectory]) {
-            NSDate* lastModifiedDate = [attributes objectForKey:NSFileModificationDate];
-            if (lastModifiedDate != nil) {
-                if (ABS(lastModifiedDate.timeIntervalSinceNow) > CACHE_LIMIT) {
-                    NSLog(@"Application:clearStaleDataBackgroundEntryPoint - %@", fullPath.lastPathComponent);
-                    [manager removeItemAtPath:fullPath error:NULL];
-                }
-            }
+  if ((rand() % 1000) < 50) {
+    NSDictionary* attributes = [enumerator fileAttributes];
+    
+    // don't delete folders
+    if (![[attributes objectForKey:NSFileType] isEqual:NSFileTypeDirectory]) {
+      NSDate* lastModifiedDate = [attributes objectForKey:NSFileModificationDate];
+      if (lastModifiedDate != nil) {
+        if (ABS(lastModifiedDate.timeIntervalSinceNow) > CACHE_LIMIT) {
+          NSLog(@"Application:clearStaleDataBackgroundEntryPoint - %@", fullPath.lastPathComponent);
+          [manager removeItemAtPath:fullPath error:NULL];
         }
+      }
     }
+  }
 }
 
 
 + (NSArray*) directoriesToKeep {
-    @throw [NSException exceptionWithName:@"ImproperSubclassing" reason:@"" userInfo:nil];
+  @throw [NSException exceptionWithName:@"ImproperSubclassing" reason:@"" userInfo:nil];
 }
 
 
 + (void) clearStaleDataBackgroundEntryPoint {
-    NSLog(@"Application:clearStaleDataBackgroundEntryPoint - start");
-    NSFileManager* manager = [NSFileManager defaultManager];
-    NSDirectoryEnumerator* enumerator = [manager enumeratorAtPath:cacheDirectory];
-    NSArray* directoriesToKeep = [self directoriesToKeep];
-
-    NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
-    NSString* fileName;
-    while ((fileName = [enumerator nextObject]) != nil) {
-        NSString* fullPath = [cacheDirectory stringByAppendingPathComponent:fileName];
-        if ([directoriesToKeep containsObject:fullPath]) {
-            [enumerator skipDescendents];
-        } else {
-            [self clearStaleItem:fullPath inEnumerator:enumerator withManager:manager];
-        }
-
-        [pool release];
-        pool = [[NSAutoreleasePool alloc] init];
+  NSLog(@"Application:clearStaleDataBackgroundEntryPoint - start");
+  NSFileManager* manager = [NSFileManager defaultManager];
+  NSDirectoryEnumerator* enumerator = [manager enumeratorAtPath:cacheDirectory];
+  NSArray* directoriesToKeep = [self directoriesToKeep];
+  
+  NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+  NSString* fileName;
+  while ((fileName = [enumerator nextObject]) != nil) {
+    NSString* fullPath = [cacheDirectory stringByAppendingPathComponent:fileName];
+    if ([directoriesToKeep containsObject:fullPath]) {
+      [enumerator skipDescendents];
+    } else {
+      [self clearStaleItem:fullPath inEnumerator:enumerator withManager:manager];
     }
+    
     [pool release];
-    NSLog(@"Application:clearStaleDataBackgroundEntryPoint - stop");
+    pool = [[NSAutoreleasePool alloc] init];
+  }
+  [pool release];
+  NSLog(@"Application:clearStaleDataBackgroundEntryPoint - stop");
 }
 
 
 + (void) clearStaleData {
-    [ThreadingUtilities backgroundSelector:@selector(clearStaleDataBackgroundEntryPoint)
-                                  onTarget:self
-                                      gate:nil
-                                   visible:NO];
+  [ThreadingUtilities backgroundSelector:@selector(clearStaleDataBackgroundEntryPoint)
+                                onTarget:self
+                                    gate:nil
+                                 visible:NO];
 }
 
 
 + (void) moveItemToTrash:(NSString*) path {
-    [gate lock];
-    {
-        NSString* trashPath = [self uniqueTrashDirectory];
-        [[NSFileManager defaultManager] moveItemAtPath:path toPath:trashPath error:NULL];
-
-        // safeguard, just in case.
-        [[NSFileManager defaultManager] removeItemAtPath:path error:NULL];
-    }
-    [gate unlock];
+  [gate lock];
+  {
+    NSString* trashPath = [self uniqueTrashDirectory];
+    [[NSFileManager defaultManager] moveItemAtPath:path toPath:trashPath error:NULL];
+    
+    // safeguard, just in case.
+    [[NSFileManager defaultManager] removeItemAtPath:path error:NULL];
+  }
+  [gate unlock];
 }
 
 
 + (NSString*) randomString {
-    NSMutableString* string = [NSMutableString string];
-    for (int i = 0; i < 8; i++) {
-        [string appendFormat:@"%c", ((rand() % 26) + 'a')];
-    }
-    return string;
+  NSMutableString* string = [NSMutableString string];
+  for (int i = 0; i < 8; i++) {
+    [string appendFormat:@"%c", ((rand() % 26) + 'a')];
+  }
+  return string;
 }
 
 
 + (NSString*) uniqueDirectory:(NSString*) parentDirectory
                        create:(BOOL) create {
-    NSString* finalDir;
-
-    [gate lock];
-    {
-        do {
-            NSString* random = [self randomString];
-            finalDir = [parentDirectory stringByAppendingPathComponent:random];
-        } while ([[NSFileManager defaultManager] fileExistsAtPath:finalDir]);
-
-        if (create) {
-            [FileUtilities createDirectory:finalDir];
-        }
+  NSString* finalDir;
+  
+  [gate lock];
+  {
+    do {
+      NSString* random = [self randomString];
+      finalDir = [parentDirectory stringByAppendingPathComponent:random];
+    } while ([[NSFileManager defaultManager] fileExistsAtPath:finalDir]);
+    
+    if (create) {
+      [FileUtilities createDirectory:finalDir];
     }
-    [gate unlock];
-
-    return finalDir;
+  }
+  [gate unlock];
+  
+  return finalDir;
 }
 
 
 + (NSString*) uniqueTemporaryDirectory {
-    return [self uniqueDirectory:[self tempDirectory] create:YES];
+  return [self uniqueDirectory:[self tempDirectory] create:YES];
 }
 
 
 + (NSString*) uniqueTrashDirectory {
-    return [self uniqueDirectory:[self trashDirectory] create:NO];
+  return [self uniqueDirectory:[self trashDirectory] create:NO];
 }
 
 
 + (void) openBrowser:(NSString*) address {
-    if (address.length == 0) {
-        return;
-    }
-
-    NSURL* url = [NSURL URLWithString:address];
-    [[UIApplication sharedApplication] openURL:url];
+  if (address.length == 0) {
+    return;
+  }
+  
+  NSURL* url = [NSURL URLWithString:address];
+  [[UIApplication sharedApplication] openURL:url];
 }
 
 
 + (void) openMap:(NSString*) address {
-    [self openBrowser:address];
+  [self openBrowser:address];
 }
 
 
 + (BOOL) isIPhone {
-    return [[[UIDevice currentDevice] model] isEqual:@"iPhone"];
+  return [[[UIDevice currentDevice] model] isEqual:@"iPhone"];
 }
 
 
 + (void) makeCall:(NSString*) phoneNumber {
-    if (![self isIPhone]) {
-        return;
-    }
-
-    NSRange xRange = [phoneNumber rangeOfString:@"x"];
-    if (xRange.length > 0 && xRange.location >= 12) {
-        // 222-222-2222 x222
-        // remove extension
-        phoneNumber = [phoneNumber substringToIndex:xRange.location];
-    }
-
-    NSString* urlString = [NSString stringWithFormat:@"tel:%@", [StringUtilities stringByAddingPercentEscapes:phoneNumber]];
-
-    [self openBrowser:urlString];
+  if (![self isIPhone]) {
+    return;
+  }
+  
+  NSRange xRange = [phoneNumber rangeOfString:@"x"];
+  if (xRange.length > 0 && xRange.location >= 12) {
+    // 222-222-2222 x222
+    // remove extension
+    phoneNumber = [phoneNumber substringToIndex:xRange.location];
+  }
+  
+  NSString* urlString = [NSString stringWithFormat:@"tel:%@", [StringUtilities stringByAddingPercentEscapes:phoneNumber]];
+  
+  [self openBrowser:urlString];
 }
 
 
 + (BOOL) useKilometers {
-    // yeah... so the UK supposedly uses metric...
-    // except they don't. so we special case them to stick with 'miles' in the UI.
-    BOOL isMetric = [[[NSLocale currentLocale] objectForKey:NSLocaleUsesMetricSystem] boolValue];
-    BOOL isUK = [@"GB" isEqual:[LocaleUtilities isoCountry]];
-
-    return isMetric && !isUK;
+  // yeah... so the UK supposedly uses metric...
+  // except they don't. so we special case them to stick with 'miles' in the UI.
+  BOOL isMetric = [[[NSLocale currentLocale] objectForKey:NSLocaleUsesMetricSystem] boolValue];
+  BOOL isUK = [@"GB" isEqual:[LocaleUtilities isoCountry]];
+  
+  return isMetric && !isUK;
 }
 
 
 + (BOOL) canSendMail {
 #ifdef IPHONE_OS_VERSION_3
-    return [MFMailComposeViewController canSendMail];
+  return [MFMailComposeViewController canSendMail];
 #else
-    return YES;
+  return YES;
 #endif
 }
 //
