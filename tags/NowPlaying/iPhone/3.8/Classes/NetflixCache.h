92fee94666382f66c911b6d9d5cd086e6ff80796
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-06-17 08:26:03 +0000

    tagging NowPlaying

---
+++ tags/NowPlaying/iPhone/3.8/Classes/NetflixCache.h
@@ -0,0 +1,74 @@
+// Copyright 2008 Cyrus Najmabadi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "AbstractNetflixCache.h"
+
+@interface NetflixCache : AbstractNetflixCache {
+@private
+  // accessed from multiple threads.  needs lock
+  ThreadsafeValue*/*NSArray*/ feedsData;
+  NSDictionary* queuesData;
+  NSDate* lastQuotaErrorDate;
+}
+
+@property (readonly, retain) NSDate* lastQuotaErrorDate;
+
++ (NSArray*) mostPopularTitles;
+
+- (NSArray*) movieSearch:(NSString*) query error:(NSString**) error;
+- (NSArray*) peopleSearch:(NSString*) query;
+
+- (BOOL) isEnqueued:(Movie*) movie;
+- (NSArray*) statusesForMovie:(Movie*) movie;
+
+- (void) update;
+
+- (NSArray*) feeds;
+- (Queue*) queueForFeed:(Feed*) feed;
+- (Feed*) feedForKey:(NSString*) key;
+- (Queue*) queueForKey:(NSString*) key;
+- (NSString*) titleForKey:(NSString*) key;
+- (NSString*) titleForKey:(NSString*) key includeCount:(BOOL) includeCount;
+- (NSArray*) moviesForRSSTitle:(NSString*) title;
+- (NSInteger) movieCountForRSSTitle:(NSString*) title;
+
+- (NSArray*) castForMovie:(Movie*) movie;
+- (NSArray*) directorsForMovie:(Movie*) movie;
+- (NSString*) synopsisForMovie:(Movie*) movie;
+- (NSString*) netflixRatingForMovie:(Movie*) movie;
+- (NSString*) userRatingForMovie:(Movie*) movie;
+- (NSArray*) formatsForMovie:(Movie*) movie;
+- (NSArray*) similarMoviesForMovie:(Movie*) movie;
+- (NSString*) netflixAddressForMovie:(Movie*) movie;
+- (NSString*) availabilityForMovie:(Movie*) movie;
+
+- (NSString*) noInformationFound;
+
+- (void) lookupNetflixMovieForLocalMovieBackgroundEntryPoint:(Movie*) movie;
+
+- (Movie*) netflixMovieForMovie:(Movie*) movie;
+
+- (void) saveQueue:(Queue*) queue;
+- (Movie*) promoteDiscToSeries:(Movie*) disc;
+- (NSString*) userRatingsFile:(Movie*) movie;
+- (NSString*) downloadEtag:(Feed*) feed;
+
+- (void) checkApiResult:(XmlElement*) result;
+- (NSString*) extractErrorMessage:(XmlElement*) element;
+
++ (void) processMovieItemList:(XmlElement*) element
+                       movies:(NSMutableArray*) movies
+                        saved:(NSMutableArray*) saved;
+
+@end
---
+++ tags/NowPlaying/iPhone/3.8/Classes/SettingCell.m
@@ -0,0 +1,98 @@
+// Copyright 2008 Cyrus Najmabadi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "SettingCell.h"
+
+@interface SettingCell()
+@property (retain) UILabel* separatorLine;
+@property (copy) NSString* value;
+@end
+
+
+@implementation SettingCell
+
+@synthesize separatorLine;
+@synthesize value;
+@synthesize placeholder;
+
+- (void) dealloc {
+    self.separatorLine = nil;
+    self.placeholder = nil;
+    self.value = nil;
+
+    [super dealloc];
+}
+
+
+- (id) initWithReuseIdentifier:(NSString*) reuseIdentifier {
+    if ((self = [super initWithStyle:UITableViewCellStyleValue1
+                    reuseIdentifier:reuseIdentifier])) {
+        self.separatorLine = [[[UILabel alloc] init] autorelease];
+    }
+
+    return self;
+}
+
+
+- (void) layoutSubviews {
+    [super layoutSubviews];
+
+    CGRect separatorFrame = CGRectMake(0, -1, self.contentView.frame.size.width, 1);
+    separatorLine.frame = separatorFrame;
+}
+
+
+- (void) setValueColor {
+    if (value.length > 0) {
+        self.detailTextLabel.textColor = [ColorCache commandColor];
+    } else {
+        self.detailTextLabel.textColor = [UIColor lightGrayColor];
+    }
+}
+
+
+- (void) setCellValue:(NSString*) text {
+    self.value = text;
+
+    if (value.length > 0) {
+        self.detailTextLabel.text = value;
+    } else {
+        self.detailTextLabel.text = placeholder;
+    }
+
+    [self setValueColor];
+}
+
+
+- (void) setHidesSeparator:(BOOL) hideSeparator {
+    [separatorLine removeFromSuperview];
+    if (hideSeparator) {
+        [self.contentView addSubview:separatorLine];
+    }
+}
+
+
+- (void) setSelected:(BOOL) selected
+            animated:(BOOL) animated {
+    [super setSelected:selected animated:animated];
+    if (selected) {
+        self.textLabel.textColor = [UIColor whiteColor];
+        self.detailTextLabel.textColor = [UIColor whiteColor];
+    } else {
+        self.textLabel.textColor = [UIColor blackColor];
+        [self setValueColor];
+    }
+}
+
+@end
