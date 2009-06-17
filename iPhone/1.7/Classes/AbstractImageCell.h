734c4ff866988c2a9966fac6308afb49fc70a534
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-06-17 08:27:08 +0000

    Tagging pocketflix 1.7

---
+++ iPhone/1.7/Classes/AbstractImageCell.h
@@ -0,0 +1,41 @@
+// Copyright 2008 Cyrus Najmabadi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+@interface AbstractImageCell : UITableViewCell {
+@private
+  UIImageView* imageLoadingView;
+  UIActivityIndicatorView* activityView;
+  BOOL imageLoaded;
+
+@protected
+  UIImageView* imageView;
+
+  UILabel* titleLabel;
+  CGFloat titleWidth;
+}
+
+- (id) initWithReuseIdentifier:(NSString*) reuseIdentifier;
+
+- (void) loadImage;
+- (void) clearImage;
+
+// @protected
+- (NSArray*) allLabels;
+
+- (UILabel*) createTitleLabel:(NSString*) title yPosition:(NSInteger) yPosition;
+- (UILabel*) createValueLabel:(NSInteger) yPosition forTitle:(UILabel*) titleLabel;
+
+- (void) setCellImage:(UIImage*) image;
+
+@end
---
+++ iPhone/1.7/Classes/MetacriticScoreProvider.m
@@ -0,0 +1,76 @@
+// Copyright 2008 Cyrus Najmabadi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "MetacriticScoreProvider.h"
+
+#import "Application.h"
+#import "Score.h"
+
+@implementation MetacriticScoreProvider
+
+- (void) dealloc {
+    [super dealloc];
+}
+
+
++ (MetacriticScoreProvider*) provider {
+    return [[[MetacriticScoreProvider alloc] init] autorelease];
+}
+
+
+- (NSString*) providerName {
+    return @"Metacritic";
+}
+
+
+- (NSString*) lookupServerHash {
+    NSString* address = [NSString stringWithFormat:@"http://%@.appspot.com/LookupMovieRatings?q=metacritic&format=xml&hash=true", [Application host]];
+    NSString* value = [NetworkUtilities stringWithContentsOfAddress:address];
+    return value;
+}
+
+
+- (NSDictionary*) lookupServerScores {
+    NSString* address = [NSString stringWithFormat:@"http://%@.appspot.com/LookupMovieRatings?q=metacritic&format=xml", [Application host]];
+    XmlElement* resultElement = [NetworkUtilities xmlWithContentsOfAddress:address];
+
+    if (resultElement != nil) {
+        NSMutableDictionary* ratings = [NSMutableDictionary dictionary];
+
+        for (XmlElement* movieElement in resultElement.children) {
+            NSString* title =    [[movieElement attributeValue:@"title"] stringByReplacingOccurrencesOfString:@"&amp;" withString:@"&"];
+            NSString* link =     [movieElement attributeValue:@"link"];
+            NSString* synopsis = [movieElement attributeValue:@"synopsis"];
+            NSString* score =    [movieElement attributeValue:@"score"];
+
+            if ([score isEqual:@"xx"]) {
+                score = @"-1";
+            }
+
+            Score* extraInfo = [Score scoreWithTitle:title
+                                            synopsis:synopsis
+                                               score:score
+                                            provider:@"metacritic"
+                                          identifier:link];
+
+            [ratings setObject:extraInfo forKey:extraInfo.canonicalTitle];
+        }
+
+        return ratings;
+    }
+
+    return nil;
+}
+
+@end
