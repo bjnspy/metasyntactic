5f45e23ad3ee1f5df91db15782af0bb633cfe4ff
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-06-18 04:44:27 +0000

    cleaner API.

--- trunk/MetasyntacticShared/Classes/AbstractTableViewController.m
+++ trunk/MetasyntacticShared/Classes/AbstractTableViewController.m
@@ -29,50 +29,50 @@
 @synthesize visible;
 
 - (void) dealloc {
-    self.visibleIndexPaths = nil;
-    self.visible = NO;
-
-    [super dealloc];
+  self.visibleIndexPaths = nil;
+  self.visible = NO;
+  
+  [super dealloc];
 }
 
 
 - (AbstractNavigationController*) abstractNavigationController {
-    return (id)self.navigationController;
+  return (id)self.navigationController;
 }
 
 
 - (void) viewDidAppear:(BOOL) animated {
-    [super viewDidAppear:animated];
-    [MetasyntacticSharedApplication saveNavigationStack:self.navigationController];
+  [super viewDidAppear:animated];
+  [MetasyntacticSharedApplication saveNavigationStack:self.navigationController];
 }
 
 
 - (void) majorRefreshWorker {
-    @throw [NSException exceptionWithName:@"ImproperSubclassing" reason:@"" userInfo:nil];
+  @throw [NSException exceptionWithName:@"ImproperSubclassing" reason:@"" userInfo:nil];
 }
 
 
 - (void) minorRefreshWorker {
-    @throw [NSException exceptionWithName:@"ImproperSubclassing" reason:@"" userInfo:nil];
+  @throw [NSException exceptionWithName:@"ImproperSubclassing" reason:@"" userInfo:nil];
 }
 
 
 - (void) viewWillAppear:(BOOL) animated {
-    [super viewWillAppear:animated];
-
+  [super viewWillAppear:animated];
+  
   self.visible = YES;
-    [self majorRefreshWorker];
+  [self majorRefreshWorker];
 }
 
 
 - (void) viewWillDisappear:(BOOL) animated {
-    [super viewWillDisappear:animated];
-    self.visible = NO;
+  [super viewWillDisappear:animated];
+  self.visible = NO;
 }
 
 
 - (void) viewDidDisappear:(BOOL) animated {
-    [super viewDidDisappear:animated];
+  [super viewDidDisappear:animated];
 }
 
 
@@ -81,36 +81,36 @@
 
 
 - (void) didReceiveMemoryWarning {
-    if (visible) {
-        return;
-    }
-
-    // Store the currently visible cells so we can scroll back to them when
-    // we're reloaded.
-    self.visibleIndexPaths = [self.tableView indexPathsForVisibleRows];
-
-    [self didReceiveMemoryWarningWorker];
-
-    [super didReceiveMemoryWarning];
+  if (visible) {
+    return;
+  }
+  
+  // Store the currently visible cells so we can scroll back to them when
+  // we're reloaded.
+  self.visibleIndexPaths = [self.tableView indexPathsForVisibleRows];
+  
+  [self didReceiveMemoryWarningWorker];
+  
+  [super didReceiveMemoryWarning];
 }
 
 
 - (void) reloadTableViewData {
-    if (!visible) {
-        return;
+  if (!visible) {
+    return;
+  }
+  
+  [self.tableView reloadData];
+  
+  if (visibleIndexPaths.count > 0) {
+    NSIndexPath* path = [visibleIndexPaths objectAtIndex:0];
+    if (path.section >= 0 && path.section < self.tableView.numberOfSections &&
+        path.row >= 0 && path.row < [self.tableView numberOfRowsInSection:path.section]) {
+      [self.tableView scrollToRowAtIndexPath:[visibleIndexPaths objectAtIndex:0] atScrollPosition:UITableViewScrollPositionNone animated:NO];
     }
-
-    [self.tableView reloadData];
-
-    if (visibleIndexPaths.count > 0) {
-        NSIndexPath* path = [visibleIndexPaths objectAtIndex:0];
-        if (path.section >= 0 && path.section < self.tableView.numberOfSections &&
-            path.row >= 0 && path.row < [self.tableView numberOfRowsInSection:path.section]) {
-            [self.tableView scrollToRowAtIndexPath:[visibleIndexPaths objectAtIndex:0] atScrollPosition:UITableViewScrollPositionNone animated:NO];
-        }
-
-        self.visibleIndexPaths = nil;
-    }
+    
+    self.visibleIndexPaths = nil;
+  }
 }
 
 
@@ -122,44 +122,44 @@
   if (!visible) {
     return;
   }
-
+  
   [self.tableView reloadRowsAtIndexPaths:self.tableView.indexPathsForVisibleRows withRowAnimation:UITableViewRowAnimationNone];
 }
 
 
 - (void)scrollViewDidEndDragging:(UIScrollView*) scrollView
                   willDecelerate:(BOOL) willDecelerate {
-    if (willDecelerate) {
-        [[OperationQueue operationQueue] temporarilySuspend];
-    }
+  if (willDecelerate) {
+    [[OperationQueue operationQueue] temporarilySuspend];
+  }
 }
 
 
 - (void) refreshWithSelector:(SEL) selector subclassSelector:(SEL) subclassSelector {
-    [NSObject cancelPreviousPerformRequestsWithTarget:self
-                                             selector:selector
-                                               object:nil];
-
-    if (!visible) {
-        return;
-    }
-
-    if (self.tableView.dragging || self.tableView.decelerating) {
-        [self performSelector:selector withObject:nil afterDelay:1];
-        return;
-    }
-
-    [self performSelector:subclassSelector];
+  [NSObject cancelPreviousPerformRequestsWithTarget:self
+                                           selector:selector
+                                             object:nil];
+  
+  if (!visible) {
+    return;
+  }
+  
+  if (self.tableView.dragging || self.tableView.decelerating) {
+    [self performSelector:selector withObject:nil afterDelay:1];
+    return;
+  }
+  
+  [self performSelector:subclassSelector];
 }
 
 
 - (void) majorRefresh {
-    [self refreshWithSelector:@selector(majorRefresh) subclassSelector:@selector(majorRefreshWorker)];
+  [self refreshWithSelector:@selector(majorRefresh) subclassSelector:@selector(majorRefreshWorker)];
 }
 
 
 - (void) minorRefresh {
-    [self refreshWithSelector:@selector(minorRefresh) subclassSelector:@selector(minorRefreshWorker)];
+  [self refreshWithSelector:@selector(minorRefresh) subclassSelector:@selector(minorRefreshWorker)];
 }
 
 @end
