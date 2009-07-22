fc40a8512a93d124a3c3678e121a07e4bbe5ae9c
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-07-22 09:09:56 +0000

    fixing up the splash screen.

--- trunk/YourRights/iPhone/Classes/AppDelegate.h
+++ trunk/YourRights/iPhone/Classes/AppDelegate.h
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-@interface AppDelegate : NSObject <UIApplicationDelegate,MetasyntacticSharedApplicationDelegate> {
+@interface AppDelegate : NSObject <UIApplicationDelegate,MetasyntacticSharedApplicationDelegate,SplashScreenDelegate> {
 @private
   UIWindow *window;
   UIViewController* viewController;
