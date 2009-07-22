1c487529fce2942915d11b696702081052ec011c
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-07-22 19:38:30 +0000

    copying project.

---
+++ trunk/BoxOfficeShared/iPhone/Classes/AbstractWebsiteCache.m
@@ -0,0 +1,86 @@
+// Copyright 2008 Cyrus Najmabadi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "AbstractWebsiteCache.h"
+
+#import "AppDelegate.h"
+#import "Movie.h"
+
+@interface AbstractWebsiteCache()
+@end
+
+
+@implementation AbstractWebsiteCache
+
+- (void) dealloc {
+  [super dealloc];
+}
+
+
+- (NSString*) cacheDirectory {
+  @throw [NSException exceptionWithName:@"ImproperSubclassing" reason:@"" userInfo:nil];
+}
+
+
+- (NSString*) serverUrl:(Movie*) movie {
+  @throw [NSException exceptionWithName:@"ImproperSubclassing" reason:@"" userInfo:nil];
+}
+
+
+- (NSString*) addressFile:(Movie*) movie {
+  NSString* name = [[FileUtilities sanitizeFileName:movie.canonicalTitle] stringByAppendingPathExtension:@"plist"];
+  return [self.cacheDirectory stringByAppendingPathComponent:name];
+}
+
+
+- (void) updateMovieDetails:(Movie*) movie force:(BOOL) force {
+  NSString* path = [self addressFile:movie];
+
+  NSDate* lastLookupDate = [FileUtilities modificationDate:path];
+  if (lastLookupDate != nil) {
+    NSString* value = [FileUtilities readObject:path];
+    if (value.length > 0) {
+      // we have a real imdb value for this movie
+      return;
+    }
+
+    if (!force) {
+      // we have a sentinel.  only update if it's been long enough
+      if (ABS(lastLookupDate.timeIntervalSinceNow) < THREE_DAYS) {
+        return;
+      }
+    }
+  }
+
+
+  NSString* url = [self serverUrl:movie];
+  NSString* addressValue = [NetworkUtilities stringWithContentsOfAddress:url pause:NO];
+  if (addressValue == nil) {
+    return;
+  }
+
+  // write down the response (even if it is empty).  An empty value will
+  // ensure that we don't update this entry too often.
+  [FileUtilities writeObject:addressValue toFile:path];
+  if (addressValue.length > 0) {
+    [MetasyntacticSharedApplication minorRefresh];
+  }
+}
+
+
+- (NSString*) addressForMovie:(Movie*) movie {
+  return [FileUtilities readObject:[self addressFile:movie]];
+}
+
+@end
---
+++ trunk/BoxOfficeShared/iPhone/Classes/AppDelegate.m
@@ -0,0 +1,128 @@
+// Copyright 2008 Cyrus Najmabadi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "AppDelegate.h"
+
+#import "ApplicationTabBarController.h"
+#import "CacheUpdater.h"
+#import "Controller.h"
+#import "Model.h"
+#import "NetflixNavigationController.h"
+
+@interface AppDelegate()
+@property (nonatomic, retain) UIWindow* window;
+@property (retain) UIViewController* viewController;
+@end
+
+
+@implementation AppDelegate
+
+static AppDelegate* appDelegate = nil;
+
+@synthesize window;
+@synthesize viewController;
+
+- (void) dealloc {
+  self.window = nil;
+  self.viewController = nil;
+
+  [super dealloc];
+}
+
+
+- (void) applicationDidFinishLaunching:(UIApplication*) app {
+  if (getenv("NSZombieEnabled") || getenv("NSAutoreleaseFreedObjectCheckEnabled")) {
+    [AlertUtilities showOkAlert:@"Zombies enabled!"];
+  }
+
+  [Beacon initAndStartBeaconWithApplicationCode:[[[NSBundle mainBundle] infoDictionary] objectForKey:@"PinchMediaApplicationCode"]
+                                useCoreLocation:NO
+                                    useOnlyWiFi:NO];
+
+  [MetasyntacticSharedApplication setSharedApplicationDelegate:self];
+
+  appDelegate = self;
+
+  Class rootViewControllerClass = NSClassFromString([[[NSBundle mainBundle] infoDictionary] objectForKey:@"RootViewControllerClass"]);
+  self.viewController = [[[rootViewControllerClass alloc] init] autorelease];
+
+  [SplashScreen presentSplashScreen:self];
+}
+
+
+- (void) onSplashScreenFinished {
+  [NotificationCenter attachToViewController:viewController];
+
+  // Ok.  We've set up all our global state.  Now get the ball rolling.
+  [[Controller controller] start];
+}
+
+
+- (void) applicationWillTerminate:(UIApplication*) application {
+  [[NSUserDefaults standardUserDefaults] synchronize];
+  [[Beacon shared] endBeacon];
+}
+
+
+- (void) majorRefresh {
+  if ([viewController respondsToSelector:@selector(majorRefresh)]) {
+    [(id)viewController majorRefresh];
+  }
+}
+
+
+- (void) minorRefresh {
+  if ([viewController respondsToSelector:@selector(minorRefresh)]) {
+    [(id)viewController minorRefresh];
+  }
+}
+
+
+- (void) resetTabs {
+  if ([viewController isKindOfClass:[ApplicationTabBarController class]]) {
+    [(id) viewController resetTabs];
+  }
+}
+
+
++ (void) resetTabs {
+  [appDelegate resetTabs];
+}
+
+
++ (UIWindow*) window {
+  return appDelegate.window;
+}
+
+
+- (NSString*) localizedString:(NSString*) key {
+  return [LocalizableStringsCache localizedString:key];
+}
+
+
+- (void) saveNavigationStack:(UINavigationController*) controller {
+  [[Model model] saveNavigationStack:controller];
+}
+
+
+- (BOOL) notificationsEnabled {
+  return [[Model model] notificationsEnabled];
+}
+
+
+- (BOOL) screenRotationEnabled {
+  return [[Model model] screenRotationEnabled];
+}
+
+@end
