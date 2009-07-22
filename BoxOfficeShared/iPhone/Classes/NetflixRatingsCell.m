023dbce1334817c6a1f6d7e00fb78359d91d6bc6
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-07-22 19:38:30 +0000

    copying project.

---
+++ BoxOfficeShared/iPhone/Classes/NetflixRatingsCell.m
@@ -0,0 +1,205 @@
+// Copyright 2008 Cyrus Najmabadi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "NetflixRatingsCell.h"
+
+#import "Model.h"
+#import "MutableNetflixCache.h"
+#import "StockImages.h"
+
+@interface NetflixRatingsCell()
+@property (retain) NSArray* imageViews;
+@end
+
+@implementation NetflixRatingsCell
+
+@synthesize imageViews;
+
+- (void) dealloc {
+  self.imageViews = nil;
+
+  [super dealloc];
+}
+
+
+- (Model*) model {
+  return [Model model];
+}
+
+
+- (NSInteger) halfWayPoint {
+  if ([MetasyntacticSharedApplication screenRotationEnabled] &&
+      UIDeviceOrientationIsLandscape([[UIDevice currentDevice] orientation])) {
+    return 230;
+  } else {
+    return 150;
+  }
+}
+
+
+- (void) setupNetflixRating {
+  CGFloat rating = [[self.model.netflixCache netflixRatingForMovie:movie] floatValue];
+
+  NSMutableArray* array = [NSMutableArray array];
+  for (NSInteger i = 0; i < 5; i++) {
+    UIImage* image;
+
+    CGFloat value = rating - i;
+    if (value < 0.2) {
+      image = [StockImages redStar_0_5_Image];
+    } else if (value < 0.4) {
+      image = [StockImages redStar_1_5_Image];
+    } else if (value < 0.6) {
+      image = [StockImages redStar_2_5_Image];
+    } else if (value < 0.8) {
+      image = [StockImages redStar_3_5_Image];
+    } else if (value < 1) {
+      image = [StockImages redStar_4_5_Image];
+    } else {
+      image = [StockImages redStar_5_5_Image];
+    }
+
+    TappableImageView* imageView = [[[TappableImageView alloc] initWithImage:image] autorelease];
+    imageView.delegate = self;
+    imageView.tag = i + 1;
+    imageView.contentMode = UIViewContentModeCenter;
+
+    CGRect rect = imageView.frame;
+    rect.origin.y = 5;
+    rect.size.width += 10;
+    rect.size.height += 10;
+    NSInteger halfWayPoint = [self halfWayPoint];
+
+    rect.origin.x = (halfWayPoint - 135) + (40 * (i + 1));
+    imageView.frame = rect;
+
+    [self.contentView addSubview:imageView];
+    [array addObject:imageView];
+  }
+
+  self.imageViews = array;
+}
+
+
+- (void) setupUserRating:(NSString*) userRating {
+  CGFloat rating = [userRating floatValue];
+
+  NSMutableArray* array = [NSMutableArray array];
+  for (NSInteger i = -1; i < 5; i++) {
+    UIImage* image;
+    if (i == -1) {
+      image = [UIImage imageNamed:@"ClearRating.png"];
+    } else {
+      CGFloat value = rating - i;
+      if (value < 1) {
+        image = [StockImages emptyStarImage];
+      } else {
+        image = [StockImages filledYellowStarImage];
+      }
+    }
+
+    TappableImageView* imageView = [[[TappableImageView alloc] initWithImage:image] autorelease];
+    imageView.delegate = self;
+    imageView.tag = i + 1;
+    imageView.contentMode = UIViewContentModeCenter;
+
+    CGRect rect = imageView.frame;
+    rect.origin.y = 5;
+    rect.size.width += 10;
+    rect.size.height += 10;
+    NSInteger halfWayPoint = [self halfWayPoint];
+
+    rect.origin.x = (halfWayPoint - 115) + (40 * (i + 1));
+    imageView.frame = rect;
+
+    [self.contentView addSubview:imageView];
+    [array addObject:imageView];
+  }
+  self.imageViews = array;
+}
+
+
+- (void) clearRating {
+  for (UIView* view in self.contentView.subviews) {
+    [view removeFromSuperview];
+  }
+
+  self.imageViews = [NSArray array];
+}
+
+
+- (void) setupRating {
+  [self clearRating];
+
+  NSString* userRating = [self.model.netflixCache userRatingForMovie:movie];
+  if (userRating.length > 0) {
+    [self setupUserRating:userRating];
+  } else {
+    [self setupNetflixRating];
+  }
+}
+
+
+- (id) initWithMovie:(Movie*) movie_ {
+  if ((self = [super initWithMovie:movie_])) {
+    self.imageViews = [NSMutableArray array];
+    [self setupRating];
+  }
+
+  return self;
+}
+
+
+- (void) imageView:(TappableImageView*) imageView
+        wasTouched:(UITouch*) touch
+          tapCount:(NSInteger) tapCount {
+  NSInteger value = imageView.tag;
+  NSInteger currentUserRating = (NSInteger)[[self.model.netflixCache userRatingForMovie:movie] floatValue];
+
+  if (value == currentUserRating) {
+    return;
+  }
+
+  // change the UI:
+  [self clearRating];
+  if (value == 0) {
+    [self setupNetflixRating];
+  } else {
+    [self setupUserRating:[NSString stringWithFormat:@"%d", value]];
+  }
+
+  // now, update in the background.
+  NSString* rating = value == 0 ? @"" : [NSString stringWithFormat:@"%d", value];
+  [self.model.netflixCache changeRatingTo:rating forMovie:movie delegate:self];
+}
+
+
+- (void) changeSucceeded {
+
+}
+
+
+- (void) changeFailedWithError:(NSString*) error {
+  NSString* message = [NSString stringWithFormat:LocalizedString(@"Could not change rating:\n\n%@", @"%@ is the underlying error.  i.e. 'Could not connect to server'"), error];
+  [AlertUtilities showOkAlert:message];
+
+  [self setupRating];
+}
+
+
+- (void) refresh {
+  [self setupRating];
+}
+
+@end
---
+++ BoxOfficeShared/iPhone/Classes/NetflixSettingsViewController.m
@@ -0,0 +1,108 @@
+// Copyright 2008 Cyrus Najmabadi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "NetflixSettingsViewController.h"
+
+#import "Controller.h"
+#import "Model.h"
+
+@interface NetflixSettingsViewController()
+@end
+
+
+@implementation NetflixSettingsViewController
+
+- (void) dealloc {
+    [super dealloc];
+}
+
+
+- (id) init {
+    if ((self = [super initWithStyle:UITableViewStyleGrouped])) {
+    }
+
+    return self;
+}
+
+
+- (Model*) model {
+    return [Model model];
+}
+
+
+- (Controller*) controller {
+    return [Controller controller];
+}
+
+
+- (BOOL) shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation) interfaceOrientation {
+    return YES;
+}
+
+
+- (NSInteger) numberOfSectionsInTableView:(UITableView*) tableView {
+    return 2;
+}
+
+
+- (NSInteger) tableView:(UITableView*) tableView numberOfRowsInSection:(NSInteger) section {
+    if (section == 0) {
+        return 1;
+    } else if (section == 1) {
+        return 0;
+        return self.model.netflixPreferredFormats.count;
+    }
+
+    return 0;
+}
+
+
+- (UITableViewCell*) tableView:(UITableView*) tableView cellForRowAtIndexPath:(NSIndexPath*) indexPath {
+    UITableViewCell* cell = [[[UITableViewCell alloc] initWithFrame:CGRectZero] autorelease];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+
+    if (indexPath.section == 0) {
+        cell.textLabel.text = LocalizedString(@"Instant Watch", nil);
+        UISwitch* switch_ = [[[UISwitch alloc] init] autorelease];
+        switch_.enabled = NO;
+        switch_.on = self.model.netflixCanInstantWatch;
+        cell.accessoryView = switch_;
+    } else {
+        cell.textLabel.text = [self.model.netflixPreferredFormats objectAtIndex:indexPath.row];
+    }
+
+    return cell;
+}
+
+
+- (NSString*)       tableView:(UITableView*) tableView
+      titleForHeaderInSection:(NSInteger) section {
+    if (section == 1) {
+        return LocalizedString(@"Preferred Formats", @"The preferred movie format that the user has.  i.e. DVD or Bluray");
+    }
+
+    return nil;
+}
+
+
+- (NSString*)       tableView:(UITableView*) tableView
+      titleForFooterInSection:(NSInteger) section {
+    if (section == 1) {
+        return LocalizedString(@"Currently, settings can only be modified from Netflix's website", nil);
+    }
+
+    return nil;
+}
+
+@end
