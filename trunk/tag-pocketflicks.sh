79325f2ddb934aeaa3aa19b578648a315d3cd951
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-10-18 01:43:04 +0000

    adding tag files.

---
+++ trunk/tag-pocketflicks.sh
@@ -0,0 +1,4 @@
+svn cp PocketFlicks/iPhone/ ../tags/PocketFlicks/iPhone/1.10/PocketFlicks
+svn cp BoxOfficeShared/ ../tags/PocketFlicks/iPhone/1.10/BoxOfficeShared
+svn cp MetasyntacticShared/ ../tags/PocketFlicks/iPhone/1.10/MetasyntacticShared
+svn cp NetflixShared/ ../tags/PocketFlicks/iPhone/1.10/NetflixShared
