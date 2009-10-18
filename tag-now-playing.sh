4a5592c2acfab2a211a186ea8ccec3cc009f1ee2
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-10-18 01:43:04 +0000

    adding tag files.

---
+++ tag-now-playing.sh
@@ -0,0 +1,4 @@
+svn cp NowPlaying/iPhone/ ../tags/NowPlaying/iPhone/3.11/NowPlaying
+svn cp BoxOfficeShared/ ../tags/NowPlaying/iPhone/3.11/BoxOfficeShared
+svn cp MetasyntacticShared/ ../tags/NowPlaying/iPhone/3.11/MetasyntacticShared
+svn cp NetflixShared/ ../tags/NowPlaying/iPhone/3.11/NetflixShared
