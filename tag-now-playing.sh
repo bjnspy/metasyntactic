88c925622d18ccfcaf433b64de7e9d75cc967971
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-10-18 01:49:08 +0000

    improving tagging scripts.

--- tag-now-playing.sh
+++ tag-now-playing.sh
@@ -1,4 +1,6 @@
-svn cp NowPlaying/iPhone/ ../tags/NowPlaying/iPhone/3.11/NowPlaying
-svn cp BoxOfficeShared/ ../tags/NowPlaying/iPhone/3.11/BoxOfficeShared
-svn cp MetasyntacticShared/ ../tags/NowPlaying/iPhone/3.11/MetasyntacticShared
-svn cp NetflixShared/ ../tags/NowPlaying/iPhone/3.11/NetflixShared
+mkdir ../tags/NowPlaying/iPhone/$1
+svn add ../tags/NowPlaying/iPhone/$1
+svn cp NowPlaying/iPhone/ ../tags/NowPlaying/iPhone/$1/NowPlaying
+svn cp BoxOfficeShared/ ../tags/NowPlaying/iPhone/$1/BoxOfficeShared
+svn cp MetasyntacticShared/ ../tags/NowPlaying/iPhone/$1/MetasyntacticShared
+svn cp NetflixShared/ ../tags/NowPlaying/iPhone/$1/NetflixShared
--- tag-pocketflicks.sh
+++ tag-pocketflicks.sh
@@ -1,4 +1,6 @@
-svn cp PocketFlicks/iPhone/ ../tags/PocketFlicks/iPhone/1.10/PocketFlicks
-svn cp BoxOfficeShared/ ../tags/PocketFlicks/iPhone/1.10/BoxOfficeShared
-svn cp MetasyntacticShared/ ../tags/PocketFlicks/iPhone/1.10/MetasyntacticShared
-svn cp NetflixShared/ ../tags/PocketFlicks/iPhone/1.10/NetflixShared
+mkdir ../tags/PocketFlicks/iPhone/$1
+svn add ../tags/PocketFlicks/iPhone/$1
+svn cp PocketFlicks/iPhone/ ../tags/PocketFlicks/iPhone/$1/PocketFlicks
+svn cp BoxOfficeShared/ ../tags/PocketFlicks/iPhone/$1/BoxOfficeShared
+svn cp MetasyntacticShared/ ../tags/PocketFlicks/iPhone/$1/MetasyntacticShared
+svn cp NetflixShared/ ../tags/PocketFlicks/iPhone/$1/NetflixShared
