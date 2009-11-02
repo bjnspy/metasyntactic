8f34653af96e7e1a398d0858231373b6b11b2c46
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-11-02 02:59:56 +0000

    use the abstract root classes

--- YourRights/iPhone/Classes/Model.h
+++ YourRights/iPhone/Classes/Model.h
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-@interface Model : NSObject {
+@interface Model : AbstractModel {
 @private
   RSSCache* rssCache;
 }
