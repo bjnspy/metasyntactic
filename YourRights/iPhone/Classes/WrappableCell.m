38b754549fff3bf2469b996872ad2188ea565560
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-11-02 05:49:46 +0000

    whitespace

--- YourRights/iPhone/Classes/WrappableCell.m
+++ YourRights/iPhone/Classes/WrappableCell.m
@@ -24,90 +24,90 @@
 static UIFont* defaultFont;
 
 + (void) initialize {
-    if (self == [WrappableCell class]) {
-        defaultFont = [[UIFont boldSystemFontOfSize:18] retain];
-    }
+  if (self == [WrappableCell class]) {
+    defaultFont = [[UIFont boldSystemFontOfSize:18] retain];
+  }
 }
 
 @synthesize title;
 @synthesize label;
 
 - (void) dealloc {
-    self.title = nil;
-    self.label = nil;
-
-    [super dealloc];
+  self.title = nil;
+  self.label = nil;
+  
+  [super dealloc];
 }
 
 
 - (id) initWithTitle:(NSString*) title_ {
-    if (self = [super initWithFrame:[[UIScreen mainScreen] bounds] reuseIdentifier:nil]) {
-        self.title = title_;
-
-        self.label = [[[UILabel alloc] init] autorelease];
-        label.text = title;
-        label.numberOfLines = 0;
-        label.lineBreakMode = UILineBreakModeWordWrap;
-        label.font = defaultFont;
-
-        [self.contentView addSubview:label];
-    }
-
-    return self;
+  if (self = [super initWithFrame:[[UIScreen mainScreen] bounds] reuseIdentifier:nil]) {
+    self.title = title_;
+    
+    self.label = [[[UILabel alloc] init] autorelease];
+    label.text = title;
+    label.numberOfLines = 0;
+    label.lineBreakMode = UILineBreakModeWordWrap;
+    label.font = defaultFont;
+    
+    [self.contentView addSubview:label];
+  }
+  
+  return self;
 }
 
 
 - (void) setFont:(UIFont*) font {
-    label.font = font;
+  label.font = font;
 }
 
 
 - (void) layoutSubviews {
-    [super layoutSubviews];
-    CGRect frame = self.contentView.frame;
-
-    CGFloat height =  [WrappableCell height:title accessoryType:self.accessoryType font:label.font] - 20;
-    label.frame = CGRectMake(10, 10, frame.size.width - 20, height);
+  [super layoutSubviews];
+  CGRect frame = self.contentView.frame;
+  
+  CGFloat height =  [WrappableCell height:title accessoryType:self.accessoryType font:label.font] - 20;
+  label.frame = CGRectMake(10, 10, frame.size.width - 20, height);
 }
 
 
 - (void) setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    if (selected && self.selectionStyle != UITableViewCellSelectionStyleNone) {
-        label.textColor = [UIColor whiteColor];
-    } else {
-        label.textColor = [UIColor blackColor];
-    }
+  [super setSelected:selected animated:animated];
+  
+  if (selected && self.selectionStyle != UITableViewCellSelectionStyleNone) {
+    label.textColor = [UIColor whiteColor];
+  } else {
+    label.textColor = [UIColor blackColor];
+  }
 }
 
 
 + (CGFloat)  height:(NSString*) text
       accessoryType:(UITableViewCellAccessoryType) accessoryType
                font:(UIFont*) font {
-    double width;
-    if (UIDeviceOrientationIsLandscape([UIDevice currentDevice].orientation)) {
-        width = [UIScreen mainScreen].bounds.size.height;
-    } else {
-        width = [UIScreen mainScreen].bounds.size.width;
-    }
-    width -= 20; // normal content view
-
-    if (accessoryType == UITableViewCellAccessoryDisclosureIndicator) {
-        width -= 10;
-    }
-
-    CGSize size = CGSizeMake(width, 50000);
-    size = [text sizeWithFont:font
-            constrainedToSize:size
-                lineBreakMode:UILineBreakModeWordWrap];
-
-    return size.height + 20;
+  double width;
+  if (UIDeviceOrientationIsLandscape([UIDevice currentDevice].orientation)) {
+    width = [UIScreen mainScreen].bounds.size.height;
+  } else {
+    width = [UIScreen mainScreen].bounds.size.width;
+  }
+  width -= 20; // normal content view
+  
+  if (accessoryType == UITableViewCellAccessoryDisclosureIndicator) {
+    width -= 10;
+  }
+  
+  CGSize size = CGSizeMake(width, 50000);
+  size = [text sizeWithFont:font
+          constrainedToSize:size
+              lineBreakMode:UILineBreakModeWordWrap];
+  
+  return size.height + 20;
 }
 
 
 + (CGFloat) height:(NSString*) text accessoryType:(UITableViewCellAccessoryType) accessoryType {
-    return [self height:text accessoryType:accessoryType font:defaultFont];
+  return [self height:text accessoryType:accessoryType font:defaultFont];
 }
 
 
