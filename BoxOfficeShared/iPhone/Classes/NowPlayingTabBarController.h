e0ebce82e16222c13adfd71b3adf38e4840f6ea7
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-11-04 04:27:36 +0000

    renaming file.

--- BoxOfficeShared/iPhone/Classes/NowPlayingTabBarController.h
+++ BoxOfficeShared/iPhone/Classes/NowPlayingTabBarController.h
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-@interface ApplicationTabBarController : UITabBarController<UITabBarControllerDelegate> {
+@interface NowPlayingTabBarController : UITabBarController<UITabBarControllerDelegate> {
 @private
   MoviesNavigationController* moviesNavigationControllerData;
   TheatersNavigationController* theatersNavigationControllerData;
