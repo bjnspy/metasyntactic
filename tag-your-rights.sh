1b36b62f56559606f7655cf597dfba5be4b4a697
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-11-04 08:16:05 +0000

    adding a tag lib

---
+++ tag-your-rights.sh
@@ -0,0 +1,4 @@
+mkdir ../tags/YourRights/iPhone/$1
+svn add ../tags/YourRights/iPhone/$1
+svn cp MetasyntacticShared/ ../tags/YourRights/iPhone/$1/MetasyntacticShared
+svn cp YourRights/ ../tags/YourRights/iPhone/$1/YourRights
