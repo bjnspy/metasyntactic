ef98373e2ee02344601f32da81ae033f045fe35e
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-11-06 18:33:40 +0000

    add the intercepting view.

---
+++ MetasyntacticShared/Classes/InterceptingView.h
@@ -0,0 +1,13 @@
+//
+//  InterceptingView.h
+//  MetasyntacticShared
+//
+//  Created by Cyrus Najmabadi on 11/6/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+@interface InterceptingView : UIView {
+
+}
+
+@end
