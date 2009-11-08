ad490b38d987f910a025581593945e759c1542e6
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-11-08 02:30:36 +0000

    removing directory

--- Ropes/Macros.h
+++
@@ -1,9 +0,0 @@
-//
-//  Macros.h
-//  Ropes
-//
-//  Created by Cyrus Najmabadi on 11/7/09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
-//
-
-#define AbstractMethod { @throw [NSException exceptionWithName:@"ImproperSubclassing" reason:@"" userInfo:nil]; }
