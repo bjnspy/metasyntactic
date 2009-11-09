99e6545dfef14582f2f93c3f645df4acb9479dea
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-11-09 02:56:42 +0000

    helper method.

---
+++ trunk/Ropes/Classes/NSString+Utilities.h
@@ -0,0 +1,11 @@
+//
+//  NSString+Utilities.h
+//  Ropes
+//
+//  Created by Cyrus Najmabadi on 11/8/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+@interface NSString(Utilities)
++ (NSString*) stringWithCharacter:(unichar) character;
+@end
