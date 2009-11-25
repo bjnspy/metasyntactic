f502124667f0158170c5e4af21856c5a4bbed968
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-11-25 01:17:39 +0000

    pt loc.

--- trunk/BoxOfficeShared/iPhone/Classes/NowPlayingCreditsViewController.m
+++ trunk/BoxOfficeShared/iPhone/Classes/NowPlayingCreditsViewController.m
@@ -83,7 +83,7 @@ static NSComparisonResult compareLanguageCodes(id code1, id code2, void* context
     [dictionary setObject:@"André van Haren"     forKey:@"nl"];
     [dictionary setObject:@"Eivind Samseth"      forKey:@"no"];
     [dictionary setObject:@"Marek Wieczorek"     forKey:@"pl"];
-    [dictionary setObject:@"Pedro Pinhão"        forKey:@"pt"];
+    [dictionary setObject:@"João Sampaio"        forKey:@"pt"];
     [dictionary setObject:@"Marius Andrei"       forKey:@"ro"];
     [dictionary setObject:@"Aleksey Surkov"      forKey:@"ru"];
     [dictionary setObject:@"Ján Senko"           forKey:@"sk"];
