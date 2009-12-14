dffd3273cb9d38ac9bd5cd948004f81303a3a6a4
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-12-14 01:20:08 +0000

    rename type.

--- BoxOfficeShared/iPhone/Classes/AbstractLocationCache.h
+++ BoxOfficeShared/iPhone/Classes/AbstractLocationCache.h
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-@interface LocationCache : AbstractCache {
+@interface AbstractLocationCache : AbstractCache {
 @private
   // Shared amongst threads
   AutoreleasingMutableDictionary* addressToLocation;
