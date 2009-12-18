85ecb34d675a9550cf905c4d0633f250a4d9574c
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-12-18 06:40:55 +0000

    problem 20.

---
+++ trunk/Euler1/Euler1/Problems/Problem20.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace Euler.Problems
+{
+    class Problem20 : Problem
+    {
+        public bool run()
+        {
+            BigInteger b = 100;
+            b = b.factorial();
+
+            var result = b.ToString().Sum(c => c - '0');
+            return result == 648;
+        } 
+    } 
+} 
