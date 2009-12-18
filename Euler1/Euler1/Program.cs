9088e9b6c43966552f6cdd4086d4191a45fb5ee6
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-12-18 04:44:03 +0000

    adding problem 13

--- Euler1/Euler1/Program.cs
+++ Euler1/Euler1/Program.cs
@@ -12,8 +12,8 @@ namespace Euler
     {
         static void Main()
         {
-            //new Problem12().run();
-            //Console.ReadLine();
+            new Problem13().run();
+            Console.ReadLine();
             int current = 1;
             while (true)
             {
