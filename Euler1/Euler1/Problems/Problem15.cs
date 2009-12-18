04c72b4efa996e9f2369f892abc3596487ee105c
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-12-18 05:16:56 +0000

    problem 15.

---
+++ Euler1/Euler1/Problems/Problem15.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace Euler.Problems
+{
+    class Problem15 : Problem
+    {
+        public void run()
+        {
+            var result = factorial(20 + 20) / (factorial(20) * factorial(20));
+            Console.WriteLine(result);
+        }
+
+        private BigInteger factorial(BigInteger v)
+        {
+            BigInteger result = 1;
+            while (v > 1)
+            {
+                result *= v;
+                v--;
+            }
+            return result;
+        }
+    }
+}
