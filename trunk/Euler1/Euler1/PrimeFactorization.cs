0e994283661bf4c03d728bd66cd71e137e468970
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-12-18 04:39:54 +0000

    problem 12.

---
+++ trunk/Euler1/Euler1/PrimeFactorization.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Euler
+{
+    class PrimeFactorization
+    {
+        private List<long> variables;
+        private List<long> exponents;
+
+        public PrimeFactorization(List<long> variables, List<long> exponents)
+        {
+            this.variables = variables;
+            this.exponents = exponents;
+        }
+
+        public List<long> Variables
+        {
+            get
+            {
+                return variables;
+            }
+        }
+
+        public List<long> Exponents
+        {
+            get
+            {
+                return exponents;
+            }
+        }
+    }
+}
