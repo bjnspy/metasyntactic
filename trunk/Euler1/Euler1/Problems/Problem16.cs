47b8cf2279a9e6dba78da2c8f013f39830834373
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-12-18 05:21:05 +0000

    problem 16

---
+++ trunk/Euler1/Euler1/Problems/Problem16.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace Euler.Problems
+{
+    class Problem16 : Problem
+    {
+        public void run()
+        {
+            BigInteger value = 1;
+            for (int i = 0; i < 1000; i++)
+            {
+                value *= 2;
+            }
+            int sum = value.ToString().Sum(c => c - '0');
+            if (sum != 1366)
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
--- trunk/Euler1/Euler1/Program.cs
+++ trunk/Euler1/Euler1/Program.cs
@@ -12,7 +12,7 @@ namespace Euler
     {
         static void Main()
         {
-            new Problem15().run();
+            new Problem16().run();
             Console.ReadLine();
             int current = 1;
             while (true)
