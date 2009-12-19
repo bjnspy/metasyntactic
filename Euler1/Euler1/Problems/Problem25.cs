ddf207dd0e567e493951165d727e4b2454aded82
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-12-19 01:48:22 +0000

    24 and 25

---
+++ Euler1/Euler1/Problems/Problem25.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Euler.Problems
+{
+    class Problem25 : Problem
+    {
+        public bool run()
+        {
+            int i = 1;
+            foreach (var f in Fibonacci.Generator)
+            {
+                if (f.ToString().Length >= 1000)
+                {
+                    break;
+                }
+                i++;
+            }
+
+            return i == 4782;
+        }
+    }
+}
