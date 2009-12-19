0764381117da704d2f06d2efdaff1fde2119ff38
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2009-12-19 04:26:14 +0000

    problem 56

---
+++ trunk/Euler1/Euler1/Problems/Problem56.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Euler.Problems
+{
+    class Problem56 : Problem
+    {
+        public bool run()
+        {
+            var q = from a in Enumerable.Range(1, 99)
+                    from b in Enumerable.Range(1, 99)
+                    let ab = a.nthPower(b)
+                    let sum = ab.ToString().Sum(c => c.digitToInt())
+                    select sum;
+
+            var max = q.Max();
+            return max == 972;
+        }
+    }
+}
