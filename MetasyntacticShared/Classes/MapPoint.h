5f37f9d117dbbf782fd7936485f0ac9860c821a2
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2010-01-24 21:30:05 +0000

    Minor improvements.

--- MetasyntacticShared/Classes/MapPoint.h
+++ MetasyntacticShared/Classes/MapPoint.h
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-@protocol MapPoint<MKAnnotation>
+@protocol MapPoint<MKAnnotation,NSObject>
 - (Location*) location;
 - (NSString*) mapUrl;
 @end
