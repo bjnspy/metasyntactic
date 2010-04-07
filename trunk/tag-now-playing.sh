4719e3b4b959675af131f61bb9266771b3913ece
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2010-04-07 03:59:22 +0000

    Improving the movie playback on the iPad.

--- trunk/tag-now-playing.sh
+++ trunk/tag-now-playing.sh
@@ -4,3 +4,5 @@ svn cp NowPlaying/iPhone/ ../tags/NowPlaying/iPhone/$1/NowPlaying
 svn cp BoxOfficeShared/ ../tags/NowPlaying/iPhone/$1/BoxOfficeShared
 svn cp MetasyntacticShared/ ../tags/NowPlaying/iPhone/$1/MetasyntacticShared
 svn cp NetflixShared/ ../tags/NowPlaying/iPhone/$1/NetflixShared
+tar -zcvf ../tags/NowPlaying/iPhone/$1/Distribution-iphoneos.tar.gz  NowPlaying/iPhone/build/Distribution-iphoneos
+svn add ../tags/NowPlaying/iPhone/$1/Distribution-iphoneos.tar.gz
