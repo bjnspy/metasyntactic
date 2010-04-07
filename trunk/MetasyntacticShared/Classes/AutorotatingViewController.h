680f317cd2b179ef4b854232a2b531ff73b18627
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2010-04-07 03:40:14 +0000

    Support movie playback on the iPad.

---
+++ trunk/MetasyntacticShared/Classes/AutorotatingViewController.h
@@ -0,0 +1,13 @@
+//
+//  AutorotatingViewController.h
+//  MetasyntacticShared
+//
+//  Created by Cyrus Najmabadi on 4/6/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+@interface AutorotatingViewController : UIViewController {
+
+}
+
+@end
