20063751c402a8412614368477ed1ee62da37b38
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2010-04-08 10:37:51 +0000

    tagging 1.18

---
+++ iPhone/1.18/BoxOfficeShared/iPhone/ForwardDeclarations.h
@@ -0,0 +1,96 @@
+// Copyright 2008 Cyrus Najmabadi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+@protocol BoxOfficeSharedApplicationDelegate;
+@protocol DataProvider;
+@protocol DataProviderUpdateDelegate;
+@protocol InfoViewControllerDelegate;
+@protocol ScoreProvider;
+@protocol SearchEngineDelegate;
+
+@class AbstractDataProvider;
+@class AllMoviesViewController;
+@class AllTheatersViewController;
+@class AmazonCache;
+@class AppleMoviePosterDownloader;
+@class Application;
+@class BlurayCache;
+@class MovieCacheUpdater;
+@class CollapsedMovieDetailsCell;
+@class CommonNavigationController;
+@class CreditsViewController;
+@class DVD;
+@class DVDCache;
+@class DVDNavigationController;
+@class DVDViewController;
+@class ExpandedMovieDetailsCell;
+@class FandangoMoviePosterDownloader;
+@class FavoriteTheater;
+@class HelpCache;
+@class IMDbCache;
+@class ImdbMoviePosterDownloader;
+@class InternationalDataCache;
+@class LargeMoviePosterCache;
+@class LocationManager;
+@class LookupResult;
+@class MetacriticScoreProvider;
+@class MovieDetailsViewController;
+@class Score;
+@class MovieShowtimesCell;
+@class MoviesNavigationController;
+@class MovieTitleCell;
+@class NetflixNavigationController;
+@class NetflixRatingsCell;
+@class NetflixSearchEngine;
+@class NetflixUIController;
+@class NetflixViewController;
+@class NoneScoreProvider;
+@class Controller;
+@class Model;
+@class PersonPosterCache;
+@class Performance;
+@class PocketFlicksViewController;
+@class MoviePosterCache;
+@class PostersViewController;
+@class PreviewNetworksMoviePosterDownloader;
+@class Review;
+@class ReviewBodyCell;
+@class ReviewsViewController;
+@class ReviewTitleCell;
+@class RottenTomatoesScoreProvider;
+@class ScoreCache;
+@class ScoreProviderViewController;
+@class SearchDatePickerViewController;
+@class LocalizableStringsCache;
+@class LocalSearchEngine;
+@class SearchRequest;
+@class SearchResult;
+@class NowPlayingSettingsViewController;
+@class PocketFlicksSettingsViewController;
+@class Theater;
+@class TheaterDetailsViewController;
+@class TheaterNameCell;
+@class TheatersNavigationController;
+@class TicketsViewController;
+@class TrailerCache;
+@class UpcomingCache;
+@class UpcomingMovieCell;
+@class UpcomingMoviesNavigationController;
+@class UpcomingMoviesViewController;
+@class UserLocationCache;
+@class WikipediaCache;
+@class WikipediaApiPersonPosterDownloader;
+@class WikipediaScrapePersonPosterDownloader;
+@class LocalSearchDisplayController;
+@class NetflixSearchDisplayController;
