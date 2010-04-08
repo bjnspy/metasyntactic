4e423bc0af807d2b87db28d855bc068c1198ed45
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2010-04-08 10:32:49 +0000

    updating the tag file.

--- tag-pocketflicks.sh
+++ tag-pocketflicks.sh
@@ -4,3 +4,5 @@ svn cp PocketFlicks/iPhone/ ../tags/PocketFlicks/iPhone/$1/PocketFlicks
 svn cp BoxOfficeShared/ ../tags/PocketFlicks/iPhone/$1/BoxOfficeShared
 svn cp MetasyntacticShared/ ../tags/PocketFlicks/iPhone/$1/MetasyntacticShared
 svn cp NetflixShared/ ../tags/PocketFlicks/iPhone/$1/NetflixShared
+tar -zcvf ../tags/PocketFlicks/iPhone/$1/Distribution-iphoneos.tar.gz PocketFlicks/iPhone/build/Distribution-iphoneos
+svn add ../tags/PocketFlicks/iPhone/$1/Distribution-iphoneos.tar.gz
