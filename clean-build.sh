13719b3454f9f4ced307cf58dba3208f5a50a40b
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2010-04-08 21:31:43 +0000

    flattenning pocketflcks dependencies

---
+++ clean-build.sh
@@ -0,0 +1,6 @@
+rm -rf BoxOfficeShared/iPhone/build
+rm -rf MetasyntacticShared/build
+rm -rf NetflixShared/build
+rm -rf NowPlaying/iPhone/build
+rm -rf PocketFlicks/iPhone/build
+rm -rf protobuf-2.2.0/objectivec/build
