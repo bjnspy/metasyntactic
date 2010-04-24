5076a6923644daa36d93d556e8dac3a7bba3c437
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2010-04-24 05:59:49 +0000

    your rights script.

--- trunk/tag-your-rights.sh
+++ trunk/tag-your-rights.sh
@@ -2,3 +2,5 @@ mkdir ../tags/YourRights/iPhone/$1
 svn add ../tags/YourRights/iPhone/$1
 svn cp MetasyntacticShared/ ../tags/YourRights/iPhone/$1/MetasyntacticShared
 svn cp YourRights/ ../tags/YourRights/iPhone/$1/YourRights
+tar -zcvf ../tags/YourRights/iPhone/$1/Distribution-iphoneos.tar.gz  YourRights/iPhone/build/Distribution-iphoneos
+svn add ../tags/YourRights/iPhone/$1/Distribution-iphoneos.tar.gz
