a97aa380fab854d46b40fca734baf87128c63ca1
cyrus.najmabadi <cyrus.najmabadi@d0982328-d147-0410-9e64-db1d7fe6b3aa>
2010-05-13 07:01:59 +0000

    tagging 3.21

---
+++ iPhone/3.21/MetasyntacticShared/Classes/StringUtilities.h
@@ -0,0 +1,42 @@
+// Copyright 2008 Cyrus Najmabadi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+NSComparisonResult compareStringsNaturally(NSString* i1, NSString* i2);
+NSComparisonResult compareStringsFunction(id i1, id i2, void* context);
+
+@interface StringUtilities : NSObject {
+}
+
++ (NSString*) makeCanonical:(NSString*) title;
++ (NSString*) makeDisplay:(NSString*) title;
+
++ (NSString*) nonNilString:(NSString*) string;
+
++ (NSString*) stringByAddingPercentEscapes:(NSString*) string;
+
++ (NSString*) asciiString:(NSString*) string;
++ (NSString*) stringFromUnichar:(unichar) c;
+
++ (NSArray*) splitIntoChunks:(NSString*) string;
+
++ (NSUInteger) hashString:(NSString*) string;
+
++ (unichar) starCharacter;
++ (NSString*) emptyStarString;
++ (NSString*) halfStarString;
++ (NSString*) starString;
+
++ (NSString*) randomString:(NSInteger) length;
+
+@end
